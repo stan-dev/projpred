93ff562f6069cef29e1a7a06057dbc5e91658647
Juho Piironen <juho.piironen@aalto.fi>
2016-12-07 12:45:53 +0200

    moved projection functions to projfun.R.

--- R/misc.R
+++ R/misc.R
@@ -162,54 +162,7 @@ kfold <- function (x, K = 10, save_fits = FALSE)
 
 
 
-# function handle for the projection over samples. Gaussian case
-# uses analytical solution to do the projection over samples.
-.get_proj_handle <- function(family_kl) {
 
-  # Use analytical solution for gaussian as it is a lot faster
-  if(family_kl$family == 'gaussian' && family_kl$link == 'identity') {
-    function(chosen, p_full, d_train, intercept, regul, coef_init) {
-
-      if(intercept) {
-        d_train$x <- cbind(1, d_train$x)
-        chosen <- c(1, chosen + 1)
-      }
-
-      regulvec <- c((1-intercept)*regul, rep(regul, length(chosen) - 1))
-      regulmat <- diag(regulvec, length(regulvec), length(regulvec))
-
-      w <- sqrt(d_train$weights)
-      # Solution for the gaussian case (with l2-regularization)
-      b <- solve(crossprod(w*d_train$x[, chosen, drop = F]) + regulmat,
-                 crossprod(w*d_train$x[, chosen, drop = F], w*p_full$mu))
-      dis <- sqrt(colMeans(d_train$weights*(
-        p_full$mu - d_train$x[, chosen, drop = F]%*%b)^2) + p_full$dis^2)
-      p_sub <- list(kl = weighted.mean(log(dis) - log(p_full$dis) +
-                                        colSums(b^2*regulvec), p_full$cluster_w),
-                    dis = dis)
-      # split b to alpha and beta, add it to p_sub and return the result
-      c(p_sub, .split_coef(b, intercept))
-    }
-
-  } else {
-    function(chosen, p_full, d_train, intercept, regul, coef_init) {
-
-      # perform the projection over samples
-      res <- sapply(1:ncol(p_full$mu), function(s_ind) {
-        IRLS(list(mu = p_full$mu[, s_ind, drop = F], dis = p_full$dis[s_ind]),
-             list(x = d_train$x[, chosen, drop = F], weights = d_train$weights,
-                  offset = d_train$offset), family_kl, intercept, regul,
-             within(coef_init, beta <- coef_init$beta[chosen]))
-      })
-
-      # weight the results by sample/cluster weights
-      list(kl = weighted.mean(unlist(res['kl',]), p_full$cluster_w),
-           alpha = unlist(res['alpha',]),
-           beta = do.call(cbind, res['beta',]),
-           dis = unlist(res['dis',]))
-    }
-  }
-}
 
 # calculate everything that needs to be saved from the submodel
 .summary_stats <- function(chosen, d_train, d_test, p_full, family_kl,
--- R/projfun.R
+++ R/projfun.R
@@ -1,8 +1,11 @@
 
 
-project_gaussian <- function(ind, f, sigma2, x, wobs=rep(1.0,dim(as.matrix(x)[1]),
-                            wsample=rep(1.0,dim(as.matrix(x)[2]),
-                            type='one-to-one', intercept=TRUE, regul=1e-12)
+
+
+# THIS FUNCTION IS CURRENTLY UNUSED
+project_gaussian_new <- function(ind, f, sigma2, x, wobs=rep(1.0,dim(as.matrix(x)[1])),
+                            wsample=rep(1.0,dim(as.matrix(x)[2])),
+                            type='one-to-one', intercept=TRUE, regul=1e-12) 
 {
     
     if(intercept) {
@@ -28,5 +31,63 @@ project_gaussian <- function(ind, f, sigma2, x, wobs=rep(1.0,dim(as.matrix(x)[1]
     
     # split b to alpha and beta, add it to p_sub and return the result
     c(p_sub, .split_coef(b, intercept))
-    
 }
+
+
+
+
+project_gaussian <- function(chosen, p_full, d_train, intercept, regul, coef_init) {
+    
+    if(intercept) {
+        d_train$x <- cbind(1, d_train$x)
+        chosen <- c(1, chosen + 1)
+    }
+    
+    regulvec <- c((1-intercept)*regul, rep(regul, length(chosen) - 1))
+    regulmat <- diag(regulvec, length(regulvec), length(regulvec))
+    
+    w <- sqrt(d_train$weights)
+    # Solution for the gaussian case (with l2-regularization)
+    b <- solve(crossprod(w*d_train$x[, chosen, drop = F]) + regulmat,
+               crossprod(w*d_train$x[, chosen, drop = F], w*p_full$mu))
+    dis <- sqrt(colMeans(d_train$weights*(
+        p_full$mu - d_train$x[, chosen, drop = F]%*%b)^2) + p_full$dis^2)
+    p_sub <- list(kl = weighted.mean(log(dis) - log(p_full$dis) +
+                                         colSums(b^2*regulvec), p_full$cluster_w),
+                  dis = dis)
+    # split b to alpha and beta, add it to p_sub and return the result
+    c(p_sub, .split_coef(b, intercept))
+}
+
+
+project_nongaussian <- function(chosen, p_full, d_train, intercept, regul, coef_init) {
+    
+    # perform the projection over samples
+    res <- sapply(1:ncol(p_full$mu), function(s_ind) {
+        IRLS(list(mu = p_full$mu[, s_ind, drop = F], dis = p_full$dis[s_ind]),
+             list(x = d_train$x[, chosen, drop = F], weights = d_train$weights,
+                  offset = d_train$offset), family_kl, intercept, regul,
+             within(coef_init, beta <- coef_init$beta[chosen]))
+    })
+    
+    # weight the results by sample/cluster weights
+    list(kl = weighted.mean(unlist(res['kl',]), p_full$cluster_w),
+         alpha = unlist(res['alpha',]),
+         beta = do.call(cbind, res['beta',]),
+         dis = unlist(res['dis',]))
+}
+
+
+# function handle for the projection over samples. Gaussian case
+# uses analytical solution to do the projection over samples.
+.get_proj_handle <- function(family_kl) {
+    
+    # Use analytical solution for gaussian as it is a lot faster
+    if(family_kl$family == 'gaussian' && family_kl$link == 'identity') {
+        return(project_gaussian)
+    } else {
+        return(project_nongaussian)
+    }
+}
+
+
