c6e37928d7be25f90c6b89bf0073d5ab55349c81
Juho Piironen <juho.piironen@aalto.fi>
2016-12-19 10:56:14 +0200

    modified gaussian projection for easier use. finished get_submodels-function.

--- R/projfun.R
+++ R/projfun.R
@@ -15,6 +15,10 @@ project_gaussian <- function(ind, p_full, d_train, intercept=TRUE, regul=1e-12, 
         wsample <- p_full$weights
     else
         wsample <- rep(1.0, dim(as.matrix(mu))[2])
+    
+    # ensure the weights are normalized
+    wobs <- wobs/sum(wobs)
+    wsample <- wsample/sum(wsample)
 
     if(intercept) {
         # add vector of ones to x and transform the variable indices
@@ -23,16 +27,16 @@ project_gaussian <- function(ind, p_full, d_train, intercept=TRUE, regul=1e-12, 
     } else if (length(ind) == 0) {
         # no intercept used and ind is empty, so projecting to the completely 
         # null model with eta=0 always
-        # TODO FILL THIS IN
+        beta_sub <- matrix(integer(length=0), ncol=NCOL(mu))
+        dis_sub <- sqrt( colSums(wobs*mu^2) + dis^2 )
+        kl <- weighted.mean(log(dis_sub) - log(dis), wsample)
+        p_sub <- list(kl = kl, weights = wsample, dis = dis_sub)
+        return(c(p_sub, .split_coef(beta_sub, intercept)))
     }
 
     xp <- x[, ind, drop = F]
     Dp <- dim(xp)[2]
     regulmat <- diag(regul*rep(1.0, Dp), Dp, Dp)
-
-    # normalize the weights
-    wobs <- wobs/sum(wobs)
-    wsample <- wsample/sum(wsample)
 
     # Solve the projection equations (with l2-regularization)
     w <- sqrt(wobs)
@@ -101,10 +105,10 @@ project_nongaussian <- function(chosen, p_full, d_train, family_kl, intercept=TR
     #
     projfun <- .get_proj_handle(family_kl)
     
-    p_sub <- sapply(nv,
+    p_sub <- lapply(nv,
         function(nv) {
             if (nv == 0)
-                ind <- 0
+                ind <- integer(length=0) # empty 
             else
                 ind <- chosen[1:nv]
             return(projfun(ind, p_full, d_train, intercept))
