47010a7bf4f22820fc7a4388bdf528bf0fa85777
Juho Piironen <juho.piironen@aalto.fi>
2017-01-11 11:20:11 +0200

    robustified L1-search and fixed a small bug.

--- R/search.R
+++ R/search.R
@@ -34,31 +34,33 @@ search_forward <- function(p_full, d_train, family_kl, intercept, nv_max,
 
 
 
-search_L1 <- function(p_full, d_train, family, intercept, nv_max) {
+search_L1 <- function(p_full, d_train, family, intercept, nv_max, lambda_min_ratio=1e-6, nlam=150) {
 
     # prediction of full model (integrate over the uncertainty about the model parameters)
     mu <- p_full$mu %*% p_full$weights
 
     # create a grid of lambda values
-    lambda_min_ratio <- 1e-3 # this is the relative value above which we use fairly dense grid
-    nlam <- 100 # how many values in the grid before lambda_min_ratio
     lambda <- lambda_grid(d_train$x, mu, family, alpha=1.0, eps=lambda_min_ratio, nlam=nlam)
 
     # add a sparser grid of very small lambda values to proceed the variable selection almost up to the full model
-    lambda_min <- 1e-15 # ultimate minimum for lambda
-    lambda <- c(lambda, rev(seq(log(lambda_min), log(min(lambda)),  by=log(10))))
-
+    # lambda_min <- 1e-7*max(lambda) # ultimate minimum for lambda
+    # lambda_tail <- exp(rev(seq(log(lambda_min), log(min(lambda)),  by=log(1.2)))) # a few small lambda values
+    # lambda <- c(lambda, lambda_tail)
 
     # L1-penalized projection (projection path)
     search <- glm_elnet(d_train$x, mu, family, lambda=lambda, pmax=nv_max, pmax_strict=FALSE,
                         offset=d_train$offset, weights=d_train$weights, intercept=intercept)
 
     # sort the variables according to the order in which they enter the model in the L1-path
-    entering_indices <- apply(search$beta!=0, 1, function(num) which(num)[1])
-    entered_variables <- c(1:NCOL(d_train$x))[!is.na(entering_indices)] # variables that entered at some point
-    order_of_entered <- sort(entering_indices, index.return=TRUE)$ix[1:nv_max]
-
-    return(entered_variables[order_of_entered])
+    entering_indices <- apply(search$beta!=0, 1, function(num) which(num)[1]) # na for those that did not enter
+    # entered_variables <- c(1:NCOL(d_train$x))[!is.na(entering_indices)] # variables that entered at some point
+    notentered_variables <- c(1:NCOL(d_train$x))[is.na(entering_indices)] # variables that did not enter at any point
+    order_of_entered <- sort(entering_indices, index.return=TRUE)$ix
+	order <- c(order_of_entered, notentered_variables)
+    
+	if (length(order_of_entered) < nv_max)
+		warning('Less than nv_max variables entered L1-path. Try reducing lambda_min_ratio. ')
+	return(order[1:nv_max])
 }
 
 
