1d581e0c464b054d45ad0a7422be66f456f08b8c
Juho Piironen <juho.piironen@aalto.fi>
2017-01-23 10:29:44 +0200

    weighted_summary_means takes weights as an input (instead of p_full).

--- R/summary_funs.R
+++ R/summary_funs.R
@@ -16,18 +16,18 @@
     }
 
     mu <- family_kl$mu_fun(xt, p_sub$alpha, p_sub$beta, d_test$offset, intercept)
-    .weighted_summary_means(d_test, family_kl, p_full, mu, p_sub$dis)
+    .weighted_summary_means(d_test, family_kl, p_full$weights, mu, p_sub$dis)
   })
 }
 
 .get_full_summaries <- function(p_full, d_test, coef_full, family_kl, intercept) {
   mu <- family_kl$mu_fun(d_test$x, coef_full$alpha, coef_full$beta, d_test$offset, intercept)
-  .weighted_summary_means(d_test, family_kl, p_full, mu, p_full$dis)
+  .weighted_summary_means(d_test, family_kl, p_full$weights, mu, p_full$dis)
 }
 
 # Calculates weighted means of mu, dis, kl and lppd given samples of
 # mu and dis, the full model and the data.
-.weighted_summary_means <- function(d_test, family_kl, p_full, mu, dis) {
+.weighted_summary_means <- function(d_test, family_kl, wsample, mu, dis) {
 
   loglik <- family_kl$ll_fun(mu, dis, matrix(d_test$y,nrow=NROW(mu)))
   if (length(loglik) == 1) {
@@ -38,8 +38,8 @@
       stop('loglik is a vector, but should be a scalar or matrix')
   } else {
       # mu is a matrix, so apply weighted sum over the samples
-      list(mu = c(mu%*%p_full$weights),
-           lppd = apply(loglik, 1, log_weighted_mean_exp, p_full$weights))
+      list(mu = c(mu %*% wsample),
+           lppd = apply(loglik, 1, log_weighted_mean_exp, wsample))
   }
   
 }
