cceeefa48f26c655548c81c465862255414d9fcb
Juho Piironen <juho.piironen@aalto.fi>
2017-01-27 14:19:13 +0200

    do the clustering in the latent space instead of mu-space.

--- R/clust.R
+++ R/clust.R
@@ -3,42 +3,47 @@
 
 get_p_clust <- function(family_kl, mu, dis, nc=10, wsample=rep(1,dim(mu)[2]), cl = NULL) {
 
-  # TODO, add comments
-  # THIS FUNCTION WORKS CURRENTLY ONLY FOR GAUSSIAN FAMILY.
-  # SHOULD TAKE FAMILY AS AN INPUT AND ACT ACCORDINGLY
-
-  # cluster the mu-samples if no clustering provided
-  cl <- cl %ORifNULL% kmeans(t(mu), nc, iter.max = 50)
-
-  if (typeof(cl)=='double' || typeof(cl)=='integer') {
-    # only cluster-indices provided, so create the list and put them there
-    cl <- list(cluster=cl)
-  }
-
-  # (re)compute the cluster centers, because they may be different from the ones
-  # returned by kmeans if the samples have differing weights
-  nc <- max(cl$cluster, na.rm=T) # number of clusters (assumes labeling 1,...,nc)
-  centers <- matrix(0, nrow=nc, ncol=dim(mu)[1])
-  wcluster <- rep(0,nc) # cluster weights
-  eps <- 1e-10
-  for (j in 1:nc) {
-  	# compute normalized weights within the cluster, 1-eps is for numerical stability
-    ind <- which(cl$cluster==j)
-    ws <- wsample[ind]/sum(wsample[ind])*(1-eps)
+    # TODO, add comments
+    # THIS FUNCTION WORKS CURRENTLY ONLY FOR GAUSSIAN FAMILY.
+    # SHOULD TAKE FAMILY AS AN INPUT AND ACT ACCORDINGLY
     
-    # cluster centers and their weights
-    centers[j,] <- mu[,ind,drop=F] %*% ws
-    wcluster[j] <- sum(wsample[ind]) # unnormalized weight for the jth cluster
-  }
-  cl$centers <- centers
-  wcluster <- wcluster/sum(wcluster)
+    # cluster the samples in the latent space if no clustering provided
+    if (is.null(cl)) {
+        f <- family_kl$linkfun(mu)
+        out <- kmeans(t(f), nc, iter.max = 50)
+        cl <- out$cluster # cluster indices for each sample
+    } else if (typeof(cl)=='list') {
+        # old clustering solution provided, so fetch the cluster indices
+        if (is.null(cl$cluster))
+            stop('argument cl must be a vector of cluster indices or a clustering object returned by k-means.')
+        cl <- cl$cluster
+    }
+    
 
-  # compute the dispersion parameters for each cluster
-  disps <- sapply(1:nc,
+    # (re)compute the cluster centers, because they may be different from the ones
+    # returned by kmeans if the samples have differing weights
+    nc <- max(cl, na.rm=T) # number of clusters (assumes labeling 1,...,nc)
+    centers <- matrix(0, nrow=nc, ncol=dim(mu)[1])
+    wcluster <- rep(0,nc) # cluster weights
+    eps <- 1e-10
+    for (j in 1:nc) {
+  	    # compute normalized weights within the cluster, 1-eps is for numerical stability
+        ind <- which(cl==j)
+        ws <- wsample[ind]/sum(wsample[ind])*(1-eps)
+    
+        # cluster centers and their weights
+        centers[j,] <- mu[,ind,drop=F] %*% ws
+        wcluster[j] <- sum(wsample[ind]) # unnormalized weight for the jth cluster
+    }
+    # cl$centers <- centers
+    wcluster <- wcluster/sum(wcluster)
+
+    # compute the dispersion parameters for each cluster
+    disps <- sapply(1:nc,
                   function(j) {
                   	
                   	# compute normalized weights within the cluster, 1-eps is for numerical stability
-                    ind <- which(cl$cluster == j)
+                    ind <- which(cl == j)
                     ws <- wsample[ind]/sum(wsample[ind])*(1-eps)
                     
                     if (length(ind) > 1) {
@@ -49,10 +54,10 @@ get_p_clust <- function(family_kl, mu, dis, nc=10, wsample=rep(1,dim(mu)[2]), cl
                       sqrt(sum(ws*dis[ind]^2))
                   })
 
-  # combine the results
-  p <- list(mu = unname(t(cl$centers)),
-            dis = disps,
-            weights = wcluster,
-  		  	cl = cl$cluster)
-  return(p)
+    # combine the results
+    p <- list(  mu = unname(t(centers)),
+                dis = disps,
+                weights = wcluster,
+                cl = cl)
+    return(p)
 }
