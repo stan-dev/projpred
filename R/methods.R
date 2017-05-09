beefee0b92a215186300ba3122aa0a4755b815d0
Markus Paasiniemi <markus.paasiniemi@cs.helsinki.fi>
2017-05-09 22:01:22 +0300

    documentation, tests and some bugs fixed for changes in proj_linpred/proj_predict

--- R/methods.R
+++ R/methods.R
@@ -10,11 +10,13 @@
 #'
 #' @param object The object returned by \link{varsel},
 #' \link{cv_varsel} or \link{project}.
-#' @param xnew The predictor values used in the prediction. The number and 
-#' order of the columns should be the same as in the original full data if
-#' argument \code{nv} is specified (see below). However, if argument 
-#' \code{vind} is specified, then the number and order of columns should
-#' correspond to \code{vind}.
+#' @param xnew The predictor values used in the prediction. If \code{vind} is
+#' specified, then the number and order of columns should correspond to
+#' contain the variables at same positons (columns) as in the original data. If
+#' \code{vind} is \code{NULL} and \code{xnew} is a matrix, it must contain the
+#' same columns at same positions as in the original data. If \code{vind} is
+#' \code{NULL} and \code{xnew} is a data frame, it must contain columns with
+#' same names as in the original data.
 #' @param ynew New (test) target variables.
 #' @param offsetnew Offsets for the new observations. Defaults to a vector of
 #' 0s.
@@ -30,33 +32,39 @@
 #' taken from the \code{varsel} information). If a list, then results for all
 #' specified model sizes are returned. Ignored if \code{vind} is specified.
 #' @param draws Number of draws to return from the predictive distribution of
-#' the projection. The default is the number of draws in the projection. 
+#' the projection. The default is the number of draws in the projection.
 #' \code{proj_predict} only.
 #' @param seed_samp An optional seed to use for drawing from the projection.
 #' \code{proj_predict} only.
-#' @param ... Additional argument passed to \link{project} if object
-#' was returned by \link{varsel} or\link{cv_varsel}.
-#' 
+#' @param ... Additional argument passed to \link{project} if \code{object}
+#' is an object returned by \link{varsel} or\link{cv_varsel}.
+#'
 #' @return If nv has length one and ynew is unspecified, a matrix of
-#' predictions. Otherwise, a list of length nv with the corresponding 
+#' predictions. Otherwise, a list of length nv with the corresponding
 #' predictions. If ynew is specified (proj_linpred only), a list with
 #' elements pred (predictions) and lpd (corresponding log-densities)
 #' for each submodel size.
-#' 
+#'
 NULL
 
-# document this
-proj_helper <- function(object, xnew, offsetnew, weightsnew, nv, fun, ...) {
-  
+# The 'helper' for proj_linpred and proj_predict, ie. does all the
+# functionality that is common to them. It essentially checks all the arguments
+# and sets them to their respective defaults and then loops over the
+# projections. For each projection, it evaluates the fun-function, which
+# calculates the linear predictor if called from proj_linpred and samples from
+# the predictive distribution if called from proj_predict.
+proj_helper <- function(object, xnew, offsetnew, weightsnew, nv, seed_samp,
+                        fun, ...) {
+
   if (is.null(offsetnew)) offsetnew <- rep(0, nrow(xnew))
   if (is.null(weightsnew)) weightsnew <- rep(1, nrow(xnew))
 
-  if ('projection' %in% class(object) || 
-     (length(object)>0 && 'projection' %in% class(object[[1]]))) {
-      proj <- object
+  if ('projection' %in% class(object) ||
+      (length(object)>0 && 'projection' %in% class(object[[1]]))) {
+    proj <- object
   } else {
-      # reference model obtained, so run the projection
-      proj <- project(object, nv = nv, ...)
+    # reference model obtained, so run the projection
+    proj <- project(object = object, nv = nv, ...)
   }
 
   if (!.is_proj_list(proj)) {
@@ -69,7 +77,7 @@ proj_helper <- function(object, xnew, offsetnew, weightsnew, nv, fun, ...) {
   }
 
   projected_sizes <- sapply(proj, function(x) NROW(x$beta))
-  if (is.null(nv)) nv <- projected_sizes
+  nv <- list(...)$nv %ORifNULL% projected_sizes
 
   if (!all(nv %in% projected_sizes))
     stop(paste0('Linear prediction requested for nv = ',
@@ -79,20 +87,24 @@ proj_helper <- function(object, xnew, offsetnew, weightsnew, nv, fun, ...) {
 
   projs <- Filter(function(x) NROW(x$beta) %in% nv, proj)
   names(projs) <- nv
-    
+
   xnew_df <- is.data.frame(xnew)
   if (xnew_df) {
     terms <- unique(unlist(lapply(projs, function(x) x$ind_names)))
     xnew <- .df_to_model_mat(xnew, terms)
   }
-  
+
   if (!is.matrix(xnew))
     stop('xnew not provided in the correct format. See ?proj-pred.')
-  
+
   vind <- list(...)$vind
   if (!is.null(vind) && NCOL(xnew) != length(vind))
     stop(paste('The number of columns in xnew does not match with the given',
                'number of variable indices (vind).'))
+
+  # save the old seed and initialize with the new one
+  seed_old <- .Random.seed
+  set.seed(seed_samp)
 
   preds <- lapply(projs, function(proj) {
     if (xnew_df) {
@@ -104,10 +116,13 @@ proj_helper <- function(object, xnew, offsetnew, weightsnew, nv, fun, ...) {
       xtemp <- xnew[, proj$ind, drop = F]
     }
     mu <- proj$family_kl$mu_fun(xtemp, proj$alpha, proj$beta, offsetnew)
-    
+
     fun(proj, mu, offsetnew, weightsnew)
   })
-  
+
+  # restore the old seed
+  .Random.seed <- seed_old
+
   .unlist_proj(preds)
 }
 
@@ -116,7 +131,8 @@ proj_helper <- function(object, xnew, offsetnew, weightsnew, nv, fun, ...) {
 proj_linpred <- function(object, xnew, ynew = NULL, offsetnew = NULL,
                          weightsnew = NULL, nv = NULL, transform = FALSE,
                          integrated = FALSE, ...) {
-  
+
+  # function to perform to each projected submodel
   fun <- function(proj, mu, offset, weights) {
     pred <- t(mu)
     if (!transform) pred <- proj$family_kl$linkfun(pred)
@@ -127,7 +143,7 @@ proj_linpred <- function(object, xnew, ynew = NULL, offsetnew = NULL,
       # return a vector if pred contains only one row
       pred <- as.vector(pred)
     }
-    
+
     if (!is.null(ynew)) {
       # compute also the log-density
       temp <- .get_standard_y(ynew, weights, proj$family_kl)
@@ -144,27 +160,33 @@ proj_linpred <- function(object, xnew, ynew = NULL, offsetnew = NULL,
       pred
     }
   }
-  
-  proj_helper(object, xnew, offsetnew, weightsnew, nv, fun, ...)
+
+  # proj_helper lapplies fun to each projection in object
+  proj_helper(object = object, xnew = xnew, offsetnew = offsetnew,
+              weightsnew = weightsnew, nv = nv, seed_samp = NULL, fun = fun,
+              ...)
 }
 
 #' @rdname proj-pred
 #' @export
-proj_predict <- function(object, xnew, offsetnew = NULL, weightsnew = NULL, 
+proj_predict <- function(object, xnew, offsetnew = NULL, weightsnew = NULL,
                          nv = NULL, draws = NULL, seed_samp = NULL, ...) {
-  if (!is.null(seed_samp)) set.seed(seed_samp)
-  
+
+  # function to perform to each projected submodel
   fun <- function(proj, mu, offset, weights) {
     if(is.null(draws)) draws <- length(proj$weights)
     draw_inds <- sample(x = seq_along(proj$weights), size = draws,
                         replace = TRUE, prob = proj$weights)
-    
+
     t(sapply(draw_inds, function(i) {
       proj$family_kl$ppd_fun(mu[,i], proj$dis[i], weights)
     }))
   }
-  
-  proj_helper(object, xnew, offsetnew, weightsnew, nv, fun, ...)
+
+  # proj_helper lapplies fun to each projection in object
+  proj_helper(object = object, xnew = xnew, offsetnew = offsetnew,
+              weightsnew = weightsnew, nv = nv, seed_samp = seed_samp,
+              fun = fun, ...)
 }
 
 #' Plotting or printing summary statistics related to variable selection
@@ -202,7 +224,7 @@ varsel_plot <- function(object, ..., nv_max = NULL, statistics = NULL, deltas = 
                    'variable selection. Run the variable selection first.'))
 
     boot_stats <- .bootstrap_stats(object$varsel, n_boot, alpha)
-    
+
     #
     if(is.null(statistics)) statistics <- 'mlpd' #as.character(unique(stats$statistic))
     if(deltas) {
@@ -221,10 +243,10 @@ varsel_plot <- function(object, ..., nv_max = NULL, statistics = NULL, deltas = 
                              statistic %in% statistics, 'statistic'),
         value = boot_vals - boot_deltas)
     }
-     
+
     stats <- subset(boot_stats, delta == deltas | statistic == 'kl')
     arr <- subset(stats, statistic %in% statistics)
-    
+
 
     if(NROW(arr) == 0) {
         stop(paste0(ifelse(length(statistics)==1, 'Statistics ', 'Statistic '),
@@ -285,13 +307,13 @@ varsel_statistics <- function(object, ..., nv_max = NULL, deltas = F) {
 }
 
 #' Generic reference model initialization
-#' 
-#' Initializes a structure that can be used as a reference fit for the 
+#'
+#' Initializes a structure that can be used as a reference fit for the
 #' projective variable selection.
 #' This function is provided to allow construction of the reference fit
 #' using also other tools than \code{rstanarm}, because only a limited amount
 #' of information is needed for the actual projection and the variable selection.
-#' 
+#'
 #' @param x Predictor matrix of dimension \code{n}-by-\code{D} containing the candidate
 #'  variables for selection
 #' (i.e. variables from which to select the submodel). Rows denote the observations
@@ -304,17 +326,17 @@ varsel_statistics <- function(object, ..., nv_max = NULL, deltas = F) {
 #' @param dis Vector of length \code{S} giving the posterior draws for the dispersion parameter
 #' in the reference model if there is such a parameter in the model family. For Gaussian
 #' observation model this is the noise std \code{sigma}.
-#' @param offset Offset to be added to the linear predictor in the projection. (Same as in 
+#' @param offset Offset to be added to the linear predictor in the projection. (Same as in
 #' function \code{glm})
-#' @param wobs Observation weights. The weights should sum to \code{n}. 
-#' If omitted, equal weights are assumed. 
+#' @param wobs Observation weights. The weights should sum to \code{n}.
+#' If omitted, equal weights are assumed.
 #' @param wsample vector of length \code{S} giving the weights for the posterior draws.
 #' The weights should sum to one. If omitted, equal weights are assumed.
 #' @param intercept Whether to use intercept. Default is \code{TRUE}.
 #' @param loglik \code{S}-by-\code{n} matrix giving the log-likelihood values
 #' for the reference model for each pair of \code{S} posterior draws and \code{n} observations.
 #' Can be omitted but is mandatory for performing the LOO validation.
-#' 
+#'
 #' @return An object that can be passed to all the functions that
 #' take the reference fit as the first argument, such as \link{varsel}, \link{cv_varsel},
 #' \link[=proj-pred]{proj_predict} and \link[=proj-pred]{proj_linpred}.
@@ -322,12 +344,12 @@ varsel_statistics <- function(object, ..., nv_max = NULL, deltas = F) {
 #' @export
 init_refmodel <- function(x, y, family, mu=NULL, dis=NULL, offset=NULL, wobs=NULL, wsample=NULL,
                           intercept=TRUE, loglik=NULL) {
-    
+
     # fill in the missing values with their defaults
     if (is.null(mu))
         mu <- y
     mu <- unname(as.matrix(mu))
-    
+
     S <- NCOL(mu) # number of samples in the reference model
     n <- length(y)
     if (is.null(dis))
@@ -340,7 +362,7 @@ init_refmodel <- function(x, y, family, mu=NULL, dis=NULL, offset=NULL, wobs=NUL
         wsample <- rep(1/S, S)
     if (is.null(intercept))
         intercept <- TRUE
-    
+
     # figure out column names for the variables
     if (!is.null(colnames(x)))
         coefnames <- colnames(x)
@@ -349,10 +371,10 @@ init_refmodel <- function(x, y, family, mu=NULL, dis=NULL, offset=NULL, wobs=NUL
 
     # y and the observation weights in a standard form
     temp <- .get_standard_y(y, wobs, family)
-    
+
     fit <- list(x=x, y=temp$y, fam=kl_helpers(family), mu=mu, dis=dis, coefnames=coefnames,
                 offset=offset, wobs=temp$weights, wsample=wsample, intercept=intercept, loglik=loglik)
-    
+
     # define the class of the retuned object to be 'refmodel'
     class(fit) <- 'refmodel'
     return(fit)
--- tests/testthat/test_proj_pred.R
+++ tests/testthat/test_proj_pred.R
@@ -43,8 +43,8 @@ fit_poiss <- stan_glm(y ~ x, family = f_poiss, data = df_poiss, QR = T,
 
 fit_list <- list(gauss = fit_gauss, binom = fit_binom, poiss = fit_poiss)
 vs_list <- lapply(fit_list, varsel, nv_max = nv, verbose = FALSE)
-proj_vind_list <- lapply(vs_list, project, vind = c(2,3))
-proj_all_list <- lapply(vs_list, project, intercept = FALSE)
+proj_vind_list <- lapply(vs_list, project, vind = c(2,3), seed = seed)
+proj_all_list <- lapply(vs_list, project, intercept = FALSE, seed = seed)
 
 context('proj_linpred')
 test_that("output of proj_linpred is sensible with fit-object as input", {
@@ -81,10 +81,12 @@ test_that("proj_linpred: specifying ynew has an expected effect", {
   for(i in 1:length(vs_list)) {
     i_inf <- names(vs_list)[i]
     pl <- proj_linpred(vs_list[[i]], xnew = x, ynew = ys[[i]], weightsnew=weights)
+    pl2 <- proj_linpred(vs_list[[i]], xnew = x, weightsnew=weights)
     for(j in 1:length(pl)) {
       expect_equal(names(pl[[j]]), c('pred', 'lpd'))
       expect_equal(ncol(pl[[j]]$pred), n, info = i_inf)
       expect_equal(ncol(pl[[j]]$lpd), n, info = i_inf)
+      expect_equal(ncol(pl2[[j]]), n, info = i_inf)
     }
   }
 })
@@ -100,7 +102,7 @@ test_that("proj_linpred: specifying weights has an expected effect", {
       expect_equal(names(plw), c('pred', 'lpd'), info = i_inf)
       expect_equal(ncol(plw$pred), n, info = i_inf)
       expect_equal(ncol(plw$lpd), n, info = i_inf)
-      expect_true(sum(plw$lpd != pl$lpd) > 0, info = i_inf) 
+      expect_true(sum(plw$lpd != pl$lpd) > 0, info = i_inf)
     }
   }
 })
@@ -136,20 +138,34 @@ test_that("proj_linpred: specifying integrated has an expected effect", {
   }
 })
 
-test_that("project: adding more regularization has an expected effect", {
-    regul <- c(1e-6, 1e-1, 1e2)
-    for(i in 1:length(vs_list)) {
-        # i_inf <- names(vs_list)[i]
-        norms <- rep(0, length(regul))
-        for (j in 1:length(regul)) {
-            pred <- proj_linpred(vs_list[[i]], xnew = x, nv = 2, transform = F,
-                                 integrated = T, regul=regul[j])
-            norms[j] <- sum(pred^2)
-        }
-        for (j in 1:(length(regul)-1))
-            expect_gt(norms[j],norms[j+1])
+test_that("proj_linpred: adding more regularization has an expected effect", {
+  regul <- c(1e-6, 1e-1, 1e2)
+  for(i in 1:length(vs_list)) {
+    i_inf <- names(vs_list)[i]
+    norms <- rep(0, length(regul))
+    for (j in 1:length(regul)) {
+      pred <- proj_linpred(vs_list[[i]], xnew = x, nv = 2, transform = FALSE,
+                           integrated = TRUE, regul=regul[j])
+      norms[j] <- sum(pred^2)
     }
+    for (j in 1:(length(regul)-1))
+      expect_true(all(norms[j] >= norms[j+1]), info = i_inf)
+  }
 })
+
+
+test_that("proj_linpred: arguments passed to project work accordingly", {
+  for(i in 1:length(vs_list)) {
+    i_inf <- names(vs_list)[i]
+    pr <- project(vs_list[[i]], nv = c(2, 4), nc = 2, ns = 20,
+                  intercept = FALSE, regul = 1e-8, seed = 12)
+    prl1 <- proj_linpred(pr, xnew = x)
+    prl2 <- proj_linpred(vs_list[[i]], xnew = x, nv = c(2, 4), nc = 2, ns = 20,
+                         intercept = FALSE, regul = 1e-8, seed = 12)
+    expect_equal(prl1, prl2, info = i_inf)
+  }
+})
+
 
 context('proj_predict')
 test_that("output of proj_predict is sensible with fit-object as input", {
@@ -208,12 +224,25 @@ test_that("proj_predict: specifying draws has an expected effect", {
   }
 })
 
-test_that("proj_predict: specifying seed has an expected effect", {
+test_that("proj_predict: specifying seed_sam has an expected effect", {
   for(i in 1:length(proj_vind_list)) {
     i_inf <- names(proj_vind_list)[i]
-    pl1 <- proj_predict(proj_vind_list[[i]], xnew = x, seed = seed)
-    pl2 <- proj_predict(proj_vind_list[[i]], xnew = x, seed = seed)
+    pl1 <- proj_predict(proj_vind_list[[i]], xnew = x, seed_samp = seed)
+    pl2 <- proj_predict(proj_vind_list[[i]], xnew = x, seed_samp = seed)
     expect_equal(pl1, pl2, info = i_inf)
+  }
+})
+
+test_that("proj_predict: arguments passed to project work accordingly", {
+  for(i in 1:length(vs_list)) {
+    i_inf <- names(vs_list)[i]
+    pr1 <- project(vs_list[[i]], nv = c(2, 4), nc = 2, ns = 20,
+                  intercept = FALSE, regul = 1e-8, seed = 12)
+    prp1 <- proj_predict(pr1, xnew = x, draws = 100, seed_samp = 11)
+    prp2 <- proj_predict(vs_list[[i]], xnew = x, draws = 100, seed_samp = 11,
+                         nv = c(2, 4), nc = 2, ns = 20, intercept = FALSE,
+                         regul = 1e-8, seed = 12)
+    expect_equal(prp1, prp2, info = i_inf)
   }
 })
 
