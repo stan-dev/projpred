5a494c04e3ef7236012f4dd7957a2bd7428682d7
Juho Piironen <juho.piironen@aalto.fi>
2017-11-16 17:24:35 +0200

    added loss-functions to the family objects, moved Student-t to separate file and added doc.

---
+++ R/families.R
@@ -0,0 +1,56 @@
+#' Extra family objects.
+#'
+#' Family objects not in the set of default \link[=family]{family}-objects.
+#'
+#' @name extra-families
+#'
+#' @param link Specification of the link function, as for the default \link[=family]{family}-objects.
+#' @param nu Degrees of freedom for the Student-t distribution.
+#' @param ... Further arguments, currently ignored.
+#'
+#' @return A family object analogous to those described in \link[=family]{family}
+#'
+NULL
+
+
+# define a student-t family object. Dispersion is defined to be the scale parameter
+# of the distribution
+
+#' @rdname extra-families
+#' @export
+Student_t <- function(link='identity', nu=3) {
+	
+	if (!(link %in% c('identity','log','inverse')))
+		stop(paste0('Non-supported link: ', link))
+	if (!is.character(link))
+		stop('Link must be a string.')
+	
+	# fetch the link statistics
+	stats <- make.link(link)
+	
+	# variance function
+	varfun <- function(mu) {
+		if (nu > 2)
+			rep(nu/(nu-2), length(mu))
+		else
+			rep(Inf, length(mu))
+	}
+	
+	# create the object and append the relevant fields
+	fam <- list(
+		family = 'Student_t',
+		nu = nu,
+		link = link,
+		linkfun = stats$linkfun,
+		linkinv = stats$linkinv,
+		variance = varfun, 
+		dev.resids = function(y, mu, wt, dis=1) wt*(nu+1) * log(1 + 1/nu*((y-mu)/dis)^2), 
+		aic = function(y, n, mu, wt, dev) stop('aic not implemented for Student-t.'),
+		mu.eta = stats$mu.eta,
+		initialize = expression({ stop('initialization for Student-t not implemented.')	}),
+		validmu = function(mu) TRUE,
+		valideta = stats$valideta
+	)
+	
+	structure(fam, class = 'family')
+}
--- R/kl_helpers.R
+++ R/kl_helpers.R
@@ -13,32 +13,41 @@ kl_helpers <- function(fam) {
 
   # kl-divergences
   # for binomial and poisson it is the mean of the dev.resids divided by 2
-  kl_dev <- function(p_full, data, p_sub) {
-    if(NCOL(p_full$mu)>1) {
-      w <- rep(data$weights, NCOL(p_full$mu))
-      colMeans(fam$dev.resids(p_full$mu, p_sub$mu, w))/2
+  kl_dev <- function(pfull, data, psub) {
+    if(NCOL(pfull$mu)>1) {
+      w <- rep(data$weights, NCOL(pfull$mu))
+      colMeans(fam$dev.resids(pfull$mu, psub$mu, w))/2
     } else {
-      mean(fam$dev.resids(p_full$mu, p_sub$mu, data$weights))/2
+      mean(fam$dev.resids(pfull$mu, psub$mu, data$weights))/2
     }
   }
-  kl_gauss <- function(p_full, data, p_sub) log(p_sub$dis) - log(p_full$dis)
-  kl_gamma <- function(p_full, data, p_sub) {
-    mean(data$weights*(
-      p_sub$dis*(log(p_full$dis)-log(p_sub$dis)+log(p_sub$mu)-log(p_full$mu)) +
-        digamma(p_full$dis)*(p_full$dis - p_sub$dis) - lgamma(p_full$dis) +
-        lgamma(p_sub$dis) + p_full$mu*p_sub$dis/p_sub$mu - p_full$dis))
+  kl_gauss <- function(pfull, data, psub) log(psub$dis) - 0.5*log(pfull$var)
+  kl_student_t <- function(pfull, data, psub) log(psub$dis) #- 0.5*log(pfull$var) # FIX THIS, NOT CORRECT
+  kl_gamma <- function(pfull, data, psub) {
+  	stop('KL-divergence for gamma not implemented yet.')
+    # mean(data$weights*(
+    #   p_sub$dis*(log(p_full$dis)-log(p_sub$dis)+log(p_sub$mu)-log(p_full$mu)) +
+    #     digamma(p_full$dis)*(p_full$dis - p_sub$dis) - lgamma(p_full$dis) +
+    #     lgamma(p_sub$dis) + p_full$mu*p_sub$dis/p_sub$mu - p_full$dis))
   }
 
-  # dispersion parameters in one-to-one projection.
-  # for gaussian dispersion is sigma and for gamma it is the shape parameter.
-  # NOTE: these could be functions of p_full = list(mu, var), p_sub = list(mu), wobs
-  dis_na <- function(mu,v,musub,wobs) rep(0, ncol(mu)) 
-  dis_gauss <- function(mu,v,musub,wobs) {
-  	wobs <- wobs / sum(wobs)
-  	sqrt(colSums(wobs*(v + (mu-musub)^2)))
+  # dispersion parameters in one-to-one or clustered projection.
+  # for gaussian and student-t dispersion is the noise scale, and for gamma it is the shape parameter.
+  # in both cases pfull is a list with field mu and var giving the mean and predictive
+  # variance for each sample/cluster (columns) and each observation (rows).
+  # psub is a list containing mu (analogous to pfull$mu) and w, which give the weights
+  # of thee pseudo-observations at optimal coefficients (needed for student-t projection).
+  # wobs denote the observation weights. 
+  dis_na <- function(pfull, psub, wobs) rep(0, ncol(pfull$mu)) 
+  dis_gauss <- function(pfull, psub, wobs) {
+  	sqrt(colSums(wobs/sum(wobs)*(pfull$var + (pfull$mu-psub$mu)^2)))
   }
-  dis_student_t <- function(mu,v,musub,wobs) { stop('Projection of dispersion not yet implemented for student-t') }
-  dis_gamma <- function(mu,v,musub,wobs) {
+  dis_student_t <- function(pfull, psub, wobs) { 
+  	s2 <- colSums( psub$w/sum(wobs)*(pfull$var+(pfull$mu-psub$mu)^2) ) # CHECK THIS
+  	sqrt(s2)
+  	# stop('Projection of dispersion not yet implemented for student-t')
+  }
+  dis_gamma <- function(pfull, psub, wobs) {
       # TODO, IMPLEMENT THIS
       stop('Projection of dispersion parameter not yet implemented for family Gamma.')
       #mean(data$weights*((p_full$mu - p_sub$mu)/
@@ -66,7 +75,11 @@ kl_helpers <- function(fam) {
   	as.vector( sum(wsample*dis^2) + mu_var )
   }
   predvar_student_t <- function(mu, dis, wsample=1) { 
-  	stop('not implemented for student_t yet.')
+  	wsample <- wsample/sum(wsample)
+  	mu_mean <- mu %*% wsample
+  	mu_var <- mu^2 %*% wsample - mu_mean^2
+  	as.vector( fam$nu/(fam$nu-2)*sum(wsample*dis^2) + mu_var )
+  	# stop('not implemented for student_t yet.')
   	# mu_mean <- mu %*% wsample
   	# mu_var <- mu^2 %*% wsample - mu_mean^2
   	# sum(wsample*dis^2) + mu_var
@@ -74,33 +87,67 @@ kl_helpers <- function(fam) {
   predvar_gamma <- function(mu, dis, wsample) { stop('Family Gamma not implemented yet.')}
 
   # log likelihoods
-  ll_binom <- function(mu, dis, y, weights=1) { 
-    # if (!is.integer(y) || !is.integer(weights))
-      # stop('Internal error: obtained non-integer weights or success counts in binomial model.')
-    # dbinom(y*weights, weights, mu, log=T)
-    if (NCOL(y) < NCOL(mu))
-      y <- matrix(y, nrow=length(y), ncol=NCOL(mu))
-    weights*(y*log(mu) + (1-y)*log(1-mu))
-  }
+  ll_binom <- function(mu, dis, y, weights=1) dbinom(y*weights, weights, mu, log=T)
   ll_poiss <- function(mu, dis, y, weights=1) weights*dpois(y, mu, log=T)
   ll_gauss <- function(mu, dis, y, weights=1) {
     dis <- matrix(rep(dis, each=length(y)), ncol=NCOL(mu))
     weights*dnorm(y, mu, dis, log=T)
   }
   ll_student_t <- function(mu, dis, y, weights=1) {
+  	# print(dim(mu))
+  	# print(dim(dis))
+  	# print(dim(y))
     dis <- matrix(rep(dis, each=length(y)), ncol=NCOL(mu))
     weights*(dt((y-mu)/dis, fam$nu, log=T) - log(dis))
-    # weights*dnorm(y, mu, dis, log=T)
   }
   ll_gamma <- function(mu, dis, y, weights=1) {
     dis <- matrix(rep(dis, each=length(y)), ncol=NCOL(mu))
     weights*dgamma(y, dis, dis/matrix(mu), log=T)
+  }
+  
+  # loss functions for projection. these are defined to be -2*log-likelihood, ignoring any additional constants.
+  # we need to define these separately from the log-likelihoods because some of the  log-likelihoods
+  # or deviance functions do not work when given the fit of the full model (float) in place of y (integer),
+  # for instance binomial and poisson models.
+  loss_binom <- function(mu, y, weights=1, dis=NULL) {
+  	if (NCOL(y) < NCOL(mu))
+  		y <- matrix(y, nrow=length(y), ncol=NCOL(mu))
+  	-2*weights*(y*log(mu) + (1-y)*log(1-mu))
+  }
+  loss_poiss <- function(mu, y, weights=1, dis=NULL) {
+  	if (NCOL(y) < NCOL(mu))
+  		y <- matrix(y, nrow=length(y), ncol=NCOL(mu))
+  	-2*weights*(y*log(mu) - mu)
+  } 
+  loss_gauss <- function(mu, y, weights=1, dis=NULL) {
+  	if (is.null(dis))
+  		dis <- 1
+  	else
+  		dis <- matrix(rep(dis, each=length(y)), ncol=NCOL(mu))
+  	if (NCOL(y) < NCOL(mu))
+  		y <- matrix(y, nrow=length(y), ncol=NCOL(mu))
+  	-2*weights*(-0.5/dis*(y-mu)^2 - log(dis))
+  }
+  loss_student_t <- function(mu, y, weights=1, dis=NULL) {
+  	if (is.null(dis))
+  		dis <- 1
+  	else
+  		dis <- matrix(rep(dis, each=length(y)), ncol=NCOL(mu))
+  	if (NCOL(y) < NCOL(mu))
+  		y <- matrix(y, nrow=length(y), ncol=NCOL(mu))
+  	-2*weights*(-0.5*(fam$nu+1)*log(1 + 1/fam$nu*((y-mu)/dis)^2) - log(dis))
+  }
+  loss_gamma <- function(mu, dis, y, weights=1) {
+  	# dis <- matrix(rep(dis, each=length(y)), ncol=NCOL(mu))
+  	# weights*dgamma(y, dis, dis/matrix(mu), log=T)
+  	stop('Loss function not implemented for Gamma-family yet.')
   }
 
   # functions to sample from posterior predictive distribution
   ppd_gauss <- function(mu, dis, weights = 1) rnorm(length(mu), mu, dis)
   ppd_binom <- function(mu, dis, weights = 1) rbinom(length(mu), weights, mu)
   ppd_poiss <- function(mu, dis, weights = 1) rpois(length(mu), mu)
+  ppd_student_t <- function(mu, dis, weights = 1) rt(length(mu), fam$nu)*dis + mu
   ppd_gamma <- function(mu, dis, weights = 1) rgamma(length(mu), dis, dis/mu)
 
 
@@ -113,16 +160,16 @@ kl_helpers <- function(fam) {
 
   # return the family object with the correct function handles
   c(switch(fam$family,
-           'binomial' = list(kl = kl_dev, ll_fun = ll_binom, dis_fun = dis_na, #discl_fun = discl_na,
+           'binomial' = list(kl = kl_dev, ll_fun = ll_binom, loss_fun = loss_binom, dis_fun = dis_na,
                              predvar = predvar_na, ppd_fun = ppd_binom),
-           'poisson' = list(kl = kl_dev, ll_fun = ll_poiss, dis_fun = dis_na, #discl_fun = discl_na,
+           'poisson' = list(kl = kl_dev, ll_fun = ll_poiss, loss_fun = loss_poiss, dis_fun = dis_na, 
                             predvar = predvar_na, ppd_fun = ppd_poiss),
-           'gaussian' = list(kl = kl_gauss, ll_fun = ll_gauss, dis_fun = dis_gauss, #discl_fun = discl_gauss,
+           'gaussian' = list(kl = kl_gauss, ll_fun = ll_gauss, loss_fun = loss_gauss, dis_fun = dis_gauss,
                              predvar = predvar_gauss, ppd_fun = ppd_gauss),
-           'Gamma' = list(kl = kl_gamma, ll_fun = ll_gamma, dis_fun = dis_gamma, #discl_fun = discl_gamma,
+           'Gamma' = list(kl = kl_gamma, ll_fun = ll_gamma, loss_fun = loss_gamma, dis_fun = dis_gamma, 
                           predvar_gamma, ppd_fun = ppd_gamma),
-  				 'Student_t' = list(kl = NULL, ll_fun = ll_student_t, dis_fun = dis_student_t, #discl_fun = discl_gauss,
-  				 									predvar = predvar_student_t, ppd_fun = NULL)
+  				 'Student_t' = list(kl = kl_student_t, ll_fun = ll_student_t, loss_fun = loss_student_t, dis_fun = dis_student_t,
+  				 									predvar = predvar_student_t, ppd_fun = ppd_student_t)
   				 ),
     list(mu_fun = mu_fun), fam)
 
@@ -133,44 +180,8 @@ kl_helpers <- function(fam) {
 
 
 
-# define a student-t family object. Dispersion is defined to be the scale parameter
-# of the distribution
-Student_t <- function(link='identity', nu=1) {
-	
-	if (!(link %in% c('identity','log','inverse')))
-		stop(paste0('Non-supported link: ', link))
-	if (!is.character(link))
-		stop('Link must be a string.')
-	
-	# fetch the link statistics
-	stats <- make.link(link)
-	
-	# variance function # CHECK THIS!!!! 
-	varfun <- function(mu) {
-		if (nu > 2)
-			rep(nu/(nu-2), length(mu))
-		else
-			rep(Inf, length(mu))
-	}
-	
-	# create the object and append the relevant fields
-	fam <- list(
-		family = 'Student_t',
-		nu = nu,
-		link = link,
-		linkfun = stats$linkfun,
-		linkinv = stats$linkinv,
-		variance = varfun, 
-		dev.resids = function(y, mu, wt, dis=1) wt*(nu+1) * log(1 + 1/nu*((y-mu)/dis)^2), 
-		aic = function(y, n, mu, wt, dev) stop('aic not implemented yet.'),
-		mu.eta = stats$mu.eta,
-		initialize = expression({ stop('initialization not implemented yet.')	}),
-		validmu = function(mu) TRUE,
-		valideta = stats$valideta
-	)
-	
-	structure(fam, class = 'family')
-}
+
+
 
 
 
