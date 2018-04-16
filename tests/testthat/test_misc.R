0249282259191f711a978494750ea54ebe72edab
Juho Piironen <juho.piironen@aalto.fi>
2018-04-16 13:01:15 +0300

    modified the setting and restoring of RNG states, and added some tests.

--- tests/testthat/test_misc.R
+++ tests/testthat/test_misc.R
@@ -55,14 +55,14 @@ test_that("check that the main function calls do not return the same RNG state e
       r1 <- rnorm(s)
       foo <- varsel(fit, seed=seed)
       r2 <- rnorm(s)
-      expect_true(all(r1!=r2))
+      expect_true(any(r1!=r2))
       
       # cv_varsel
       SW(foo <- cv_varsel(fit, seed=seed))
       r1 <- rnorm(s)
       SW(foo <- cv_varsel(fit, seed=seed))
       r2 <- rnorm(s)
-      expect_true(all(r1!=r2))
+      expect_true(any(r1!=r2))
       
       # project
       vind <- c(1,2)
@@ -70,7 +70,7 @@ test_that("check that the main function calls do not return the same RNG state e
       r1 <- rnorm(s)
       foo <- project(fit, vind=vind, ns = 100, seed=seed)
       r2 <- rnorm(s)
-      expect_true(all(r1!=r2))
+      expect_true(any(r1!=r2))
       
       # proj_linpred
       vind <- c(1,3)
@@ -78,7 +78,7 @@ test_that("check that the main function calls do not return the same RNG state e
       r1 <- rnorm(s)
       foo <- proj_linpred(fit, x[,vind], vind=vind, seed=seed)
       r2 <- rnorm(s)
-      expect_true(all(r1!=r2))
+      expect_true(any(r1!=r2))
       
       # proj_predict
       vind <- c(1,3)
@@ -86,7 +86,49 @@ test_that("check that the main function calls do not return the same RNG state e
       r1 <- rnorm(s)
       foo <- proj_predict(fit, x[,vind], vind=vind, seed=seed)
       r2 <- rnorm(s)
-      expect_true(all(r1!=r2))
+      expect_true(any(r1!=r2))
+    }
+  }
+  
+})
+
+
+
+test_that("check that providing seed has the expected effect", {
+  
+  for (seed in c(130927, 1524542)) {
+    for (i in seq_along(fit_list)) {
+      
+      fit <- fit_list[[i]]
+      
+      # varsel
+      foo <- varsel(fit, seed=seed)
+      bar <- varsel(fit, seed=seed)
+      expect_equal(foo, bar)
+      
+      # cv_varsel
+      SW(foo <- cv_varsel(fit, seed=seed))
+      SW(bar <- cv_varsel(fit, seed=seed))
+      expect_equal(foo, bar)
+      
+      # project
+      vind <- c(1,2)
+      foo <- project(fit, vind=vind, nc = 10, seed=seed)
+      bar <- project(fit, vind=vind, nc = 10, seed=seed)
+      expect_equal(foo, bar)
+      
+      
+      # proj_linpred
+      vind <- c(1,3)
+      foo <- proj_linpred(fit, x[,vind], vind=vind, seed=seed)
+      bar <- proj_linpred(fit, x[,vind], vind=vind, seed=seed)
+      expect_equal(foo, bar)
+      
+      # proj_predict
+      vind <- c(1,3)
+      foo <- proj_predict(fit, x[,vind], vind=vind, seed=seed)
+      bar <- proj_predict(fit, x[,vind], vind=vind, seed=seed)
+      expect_equal(foo, bar)
     }
   }
   
