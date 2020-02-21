eef15e3475a0dd11ccac26fe6a4dbf68cdbdd936
Alejandro Catalina <AlejandroCatalina@users.noreply.github.com>
2020-02-21 10:26:42 +0200

    delete penalized methods

--- R/mle_helpers.R
+++ R/mle_helpers.R
@@ -13,30 +13,6 @@ fetch_data <- function(data, obs=NULL, newdata=NULL) {
 linear_mle <- function(formula, data, regul=NULL)
   lm(formula, data = data)
 
-penalized_linear_mle <- function(formula, data, regul=1e-2) {
-  formula <- validate_response_formula(formula)
-  fit_penalized <- function(f) {
-    all <- extract_terms_response(f)
-    response <- data[, as.character(all$response)]
-    ## no group terms allowed for penalized MLE
-    covariates <- c(all$individual_terms, all$interaction_terms)
-    if (length(covariates) == 0)
-      linear_mle(f, data)
-    else {
-      covariates_formula <- as.formula(paste0("~ ", paste(covariates, collapse=" + ")))
-      penalized::penalized(response=response, penalized=covariates_formula,
-                           lambda2=regul, data=data, model="linear",
-                           standardize=TRUE) # standard l2 regularisation
-    }
-  }
-  if (inherits(formula, "formula"))
-    return(fit_penalized(formula))
-  else if (inherits(formula, "list"))
-    return(lapply(formula, function(f) fit_penalized(f)))
-  else
-    stop("The provided formula is neither a formula nor a list")
-}
-
 #' Use lmer to fit the projection to the posterior draws for multilevel models.
 #' Note that we don't use #' glmer because the target is a pseudo-Gaussian
 #' transformation.
@@ -76,19 +52,4 @@ linear_proj_predfun <- function(fit, newdata=NULL) {
     return(predict(fit, newdata=newdata))
   else
     return(predict(fit))
-}
-
-penalized_linear_proj_predfun <- function(fit, newdata=NULL) {
-  if (inherits(fit, "list")) {
-    return(do.call(cbind, lapply(fit, function(fit)
-      penalized_linear_proj_predfun(fit, newdata=newdata))))
-  } else if (inherits(fit, "penfit"))
-    if (is.null(newdata))
-      return(fit@fitted)
-    else {
-      covariates <- as.formula(paste0("~ ", paste(names(fit@penalized), collapse=" + ")))
-      return(matrix(penalized::predict(fit, penalized=covariates, data=newdata), nrow(newdata), 2)[, 1])
-    }
-  else
-    return(linear_proj_predfun(fit, newdata))
 }
