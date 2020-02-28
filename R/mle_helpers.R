c10c5c60462bc4322d9aa0acb10f1a17e5e71aa8
Alejandro Catalina <AlejandroCatalina@users.noreply.github.com>
2020-02-29 01:21:03 +0200

    add offset to MLE solvers

--- R/mle_helpers.R
+++ R/mle_helpers.R
@@ -10,21 +10,22 @@ fetch_data <- function(data, obs=NULL, newdata=NULL) {
     return(newdata[obs,, drop = FALSE])
 }
 
-linear_mle <- function(formula, data, weights=NULL, regul=NULL)
-  lm(formula, data = data, weights = weights)
+linear_mle <- function(formula, data, weights=NULL, offset = NULL, regul=NULL)
+  lm(formula, data = data, weights = weights, offset = offset)
 
 #' Use lmer to fit the projection to the posterior draws for multilevel models.
 #' Note that we don't use #' glmer because the target is a pseudo-Gaussian
 #' transformation.
-linear_multilevel_mle <- function(formula, data, weights = NULL, regul=NULL) {
+linear_multilevel_mle <- function(formula, data, weights = NULL, offset = NULL,
+                                  regul=NULL) {
   formula <- validate_response_formula(formula)
   fit_lmer_callback <- function(f) {
-    tryCatch(lme4::lmer(f, data = data, weigts = weights),
+    tryCatch(lme4::lmer(f, data = data, weigts = weights, offset = offset),
              error=function(e) {
                if (grepl("No random effects", as.character(e)))
-                 lm(f, data = data, weights = weights)
+                 lm(f, data = data, weights = weights, offset = offset)
                else if (grepl("not positive definite", as.character(e)))
-                 lme4::lmer(f, data = data, weights = weights,
+                 lme4::lmer(f, data = data, weights = weights, offset = offset,
                             control = lmerControl(optimizer="optimx",
                                                   optCtrl = list(method="nlminb")))
                else
