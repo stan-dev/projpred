efc2d8ea6dfdc9dab0df5ed19246318c2c4f16ac
Alejandro Catalina Feliú <alex@AlejandrosMBP2.home>
2020-05-01 11:42:45 +0300

    refactor divergence_minimizers

--- R/divergence_minimizers.R
+++ R/divergence_minimizers.R
@@ -15,20 +15,23 @@ fetch_data <- function(data, obs = NULL, newdata = NULL) {
 linear_mle <- function(formula, data, family, weights = NULL, regul = NULL) {
   formula <- validate_response_formula(formula)
   if (inherits(formula, "formula")) {
-    return(fit_lm_ridge_callback(formula, data, family, weights))
+    return(fit_glm_callback(formula, data, family, weights))
   } else if (inherits(formula, "list")) {
-    return(lapply(formula, fit_lm_ridge_callback, data, family, weights))
+    return(lapply(formula, fit_glm_callback, data, family, weights))
   } else {
     stop("The provided formula is neither a formula object nor a list")
   }
 }
 
 # helper function of 'linear_mle'
-fit_lm_ridge_callback <- function(formula, data, family, weights) {
+fit_glm_callback <- function(formula, data, family, weights) {
   # make sure correct 'weights' can be found
   environment(formula) <- environment()
-  # TODO: is it correct to use glm for the ref predicted mean?
-  glm(formula, data = data, family = family, weights = weights)
+  if (family$family == "gaussian" && family$link == "identity") {
+    return(lm(formula, data = data, weights = weights))
+  } else {
+    return(glm(formula, data = data, family = family, weights = weights))
+  }
 }
 
 #' Use lmer to fit the projection to the posterior draws for multilevel models.
@@ -38,54 +41,86 @@ linear_multilevel_mle <- function(formula, data, family, weights = NULL,
                                   regul = NULL) {
   formula <- validate_response_formula(formula)
   if (inherits(formula, "formula")) {
-    return(fit_lmer_callback(formula, data, family, weights))
+    return(fit_glmer_callback(formula, data, family, weights))
   } else if (inherits(formula, "list")) {
-    return(lapply(formula, fit_lmer_callback, data, family, weights))
+    return(lapply(formula, fit_glmer_callback, data, family, weights))
   } else {
     stop("The provided formula is neither a formula object nor a list")
   }
 }
 
 # helper function of 'linear_multilevel_mle'
-fit_lmer_callback <- function(formula, data, family, weights) {
-  # make sure correct 'weights' can be found
+fit_glmer_callback <- function(formula, data, family, weights, control = NULL) {
+  ## make sure correct 'weights' can be found
   environment(formula) <- environment()
   tryCatch({
-    lme4::glmer(formula, data = data, family = family, weights = weights)
-  },
-  error = function(e) {
-    if (grepl("No random effects", as.character(e))) {
-      glm(formula, data = data, family = family, weights = weights)
-    } else if (grepl("not positive definite", as.character(e))) {
-      lme4::glmer(formula,
-        data = data, weights = weights, family = family,
-        control = lme4::glmerControl(
-          optimizer = "optimx",
-          optCtrl = list(method = "nlminb")
-        )
-      )
-    } else {
-      e
+      if (family$family == "gaussian" && family$link == "identity") {
+        return(lme4::lmer(formula,
+          data = data, weights = weights,
+          control = control
+        ))
+      } else {
+        return(lme4::glmer(formula,
+          data = data, family = family, weights = weights,
+          control = control
+        ))
+      }
+    },
+    error = function(e) {
+      if (grepl("No random effects", as.character(e))) {
+        return(fit_glm_callback(formula,
+          data = data, family = family,
+          weights = weights
+        ))
+      } else if (grepl("not positive definite", as.character(e))) {
+        return(fit_glmer_callback(formula,
+          data = data, weights = weights, family = family,
+          control = control_callback(family,
+            optimizer = "optimx",
+            optCtrl = list(method = "nlminb")
+          )
+        ))
+      } else {
+        return(e)
+      }
     }
-  }
   )
 }
 
-linear_multilevel_proj_predfun <- function(fit, newdata = NULL, 
+# helper function of fit_glmer_callback to pass the proper kind of control
+# options depending on the family
+control_callback <- function(family, ...) {
+  if (family$family == "gaussian" && family$link == "identity") {
+    return(lme4::lmerControl(...))
+  } else {
+    return(lme4::glmerControl(...))
+  }
+}
+
+# helper function for linear_multilevel_proj_predfun to only pass
+# allow.new.levels if the fit is multilevel
+predict_multilevel_callback <- function(fit, newdata = NULL, weights = NULL) {
+  if (inherits(fit, "lmerMod")) {
+    return(predict(fit,
+      newdata = newdata, allow.new.levels = TRUE,
+      weights = weights
+    ))
+  } else {
+    return(predict(fit, newdata = newdata, weights = weights))
+  }
+}
+
+linear_multilevel_proj_predfun <- function(fit, newdata = NULL,
                                            weights = NULL) {
   if (is.null(weights)) {
     weights <- 1
   }
   if (inherits(fit, "list")) {
     return(do.call(cbind, lapply(fit, function(fit) {
-      predict(fit, newdata = newdata, allow.new.levels = TRUE,
-              weights = weights)
+      predict_multilevel_callback(fit, newdata, weights)
     })))
   } else {
-    return(predict(fit,
-      newdata = newdata, allow.new.levels = TRUE,
-      weights = weights
-    ))
+    return(predict_multilevel_callback(fit, newdata, weights))
   }
 }
 
@@ -111,92 +146,4 @@ linear_proj_predfun <- function(fit, newdata = NULL, weights = NULL) {
       return(predict(fit))
     }
   }
-}
-
-# importing MASS solves a weird error caused by coef(fit) sometimes
-# returning NULL for some reason that seems platform dependent
-#' @import MASS
-predict.ridgelm <- function(fit, newdata = NULL, weights = NULL) {
-  b <- coef(fit)
-  center <- fit$xm
-  scales <- fit$scales
-  if (!is.null(newdata)) {
-    if (is.null(weights)) {
-      weights <- 1
-    }
-    x <- model.matrix(delete.response(terms(fit$formula)), newdata)
-  } else {
-    if (is.null(fit$weights)) {
-      weights <- 1
-    } else {
-      weights <- fit$weights
-    }
-    x <- model.matrix(delete.response(terms(fit$formula)), fit$data)
-  }
-  if (NCOL(x) > 1) {
-    x <- cbind(
-      x[, 1, drop = FALSE],
-      scale(x[, -1, drop = FALSE], center = center, scale = scales)
-    )
-  }
-  x <- weights * x
-  return(x %*% b)
-}
-
-## taken from MASS, but fix a small bug when removing the intercept
-lm.ridge <- function(formula, data, subset, na.action, lambda = 0,
-                     model = FALSE, x = FALSE, y = FALSE, contrasts = NULL,
-                     ...) {
-  m <- match.call(expand.dots = FALSE)
-  m$model <- m$x <- m$y <- m$contrasts <- m$... <- m$lambda <- NULL
-  m[[1L]] <- quote(stats::model.frame)
-  m <- eval.parent(m)
-  Terms <- attr(m, "terms")
-  Y <- model.response(m)
-  X <- model.matrix(Terms, m, contrasts)
-  n <- nrow(X)
-  p <- ncol(X)
-  offset <- model.offset(m)
-  if (!is.null(offset)) {
-    Y <- Y - offset
-  }
-  if (Inter <- attr(Terms, "intercept")) {
-    Xm <- colMeans(X[, -Inter, drop = FALSE])
-    Ym <- mean(Y)
-    p <- p - 1
-    X <- X[, -Inter, drop = FALSE] - rep(Xm, rep(n, p))
-    Y <- Y - Ym
-  }
-  else {
-    Ym <- Xm <- NA
-  }
-  Xscale <- drop(rep(1 / n, n) %*% X^2)^0.5
-  X <- X / rep(Xscale, rep(n, p))
-  Xs <- svd(X)
-  rhs <- t(Xs$u) %*% Y
-  d <- Xs$d
-  lscoef <- Xs$v %*% (rhs / d)
-  lsfit <- X %*% lscoef
-  resid <- Y - lsfit
-  s2 <- sum(resid^2) / (n - p - Inter)
-  HKB <- (p - 2) * s2 / sum(lscoef^2)
-  LW <- (p - 2) * s2 * n / sum(lsfit^2)
-  k <- length(lambda)
-  dx <- length(d)
-  div <- d^2 + rep(lambda, rep(dx, k))
-  a <- drop(d * rhs) / div
-  dim(a) <- c(dx, k)
-  coef <- Xs$v %*% a
-  dimnames(coef) <- list(names(Xscale), format(lambda))
-  GCV <- colSums((Y - X %*% coef)^2) / (n - colSums(matrix(
-    d^2 / div,
-    dx
-  )))^2
-  res <- list(
-    coef = drop(coef), scales = Xscale, Inter = Inter,
-    lambda = lambda, ym = Ym, xm = Xm, GCV = GCV, kHKB = HKB,
-    kLW = LW
-  )
-  class(res) <- "ridgelm"
-  res
 }
