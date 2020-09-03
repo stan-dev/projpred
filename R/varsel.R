024763cf8fbe179a4b77104012e3d907ba23f7ee
Alejandro Catalina Feliú <alejandro.catalina@aalto.fi>
2020-09-03 07:17:16 +0300

    export cv_search

--- R/varsel.R
+++ R/varsel.R
@@ -114,6 +114,7 @@ varsel.refmodel <- function(object, d_test = NULL, method = NULL,
     snow::clusterEvalQ(cl, library(MASS))
     snow::clusterEvalQ(cl, library(optimx))
     snow::clusterExport(cl, ls())
+    snow::clusterExport(cl, list("cv_search"))
   }
   ## fetch the default arguments or replace them by the user defined values
   args <- parse_args_varsel(
