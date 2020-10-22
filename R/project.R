db0219aaa8e295f3ec64ff88e327807fbda14561
Alejandro Catalina Feliú <alejandro.catalina@aalto.fi>
2020-10-22 15:19:11 +0200

    fix doc

--- R/project.R
+++ R/project.R
@@ -63,7 +63,8 @@
 #'   y <- x[, 1] + 0.5 * rnorm(n)
 #'   data <- data.frame(x, y)
 #'
-#'   fit <- rstanarm::stan_glm(y ~ X1 + X2 + X3 + X4 + X5, gaussian(), data = data, chains = 2, iter = 500)
+#'   fit <- rstanarm::stan_glm(y ~ X1 + X2 + X3 + X4 + X5, gaussian(),
+#'     data = data, chains = 2, iter = 500)
 #'   vs <- varsel(fit)
 #'
 #'   # project onto the best model with 4 variables
