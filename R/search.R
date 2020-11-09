cb14ca53dccd4487e1a12f20abbca098a4cf2794
Alejandro Catalina Feliú <alejandro.catalina@aalto.fi>
2020-11-09 13:36:16 +0100

    set total_terms to consider only terms we are searching through

--- R/search.R
+++ R/search.R
@@ -14,7 +14,7 @@ search_forward <- function(p_ref, refmodel, family, intercept, nterms_max,
   }
 
   chosen <- NULL
-  total_terms <- count_terms_in_formula(formula)
+  total_terms <- count_terms_chosen(allterms)
   stop_search <- min(total_terms, nterms_max)
   submodels <- c()
 
