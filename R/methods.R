498b72a48b4665a9decb2ee29ba48b2e791901de
Alejandro Catalina <alecatfel@gmail.com>
2021-04-07 14:58:30 +0300

    drop dimension if vector

--- R/methods.R
+++ R/methods.R
@@ -202,10 +202,10 @@ proj_linpred <- function(object, newdata = NULL, offsetnew = NULL,
       ynew <- NULL
     }
 
-    return(nlist(pred, lpd = t(compute_lpd(
+    return(nlist(pred, lpd = drop(t(compute_lpd(
       ynew = ynew, pred = pred, proj = proj, weights = weights,
       integrated = integrated, transform = transform
-    ))))
+    )))))
   }
 
   ## proj_helper lapplies fun to each projection in object
