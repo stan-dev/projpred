stan-dev/projpred

commit c51b86cc2dfc69a8b5f424534cc83f49f31524b4

Fix `ndraws` remnants (#128)
* Fix a bug caused by the fact that `ndraws` must be non-NULL now (since commit 16d5afd40908207ebdc6da32e3b6671bf6c581b5).

* Fix a warning message.

* Fix output of print.vselsummary() (either `ndraws` or `nclusters` is used, but not both) and take into account that `ndraws` must be non-NULL now (since commit 16d5afd40908207ebdc6da32e3b6671bf6c581b5).

R/methods.R
@@ -578,14 +578,24 @@ print.vselsummary <- function(x, digits = 1, ...) {
   nterms_max <- max(x$selection$size)
   cat(paste0("Search method: ", x$method, ", maximum number of terms ",
              nterms_max, "\n"))
-  cat(paste0(
-    "Draws used for selection: ", x$ndraws, ", in ",
-    x$nclusters, " clusters\n"
-  ))
-  cat(paste0(
-    "Draws used for prediction: ", x$ndraws_pred, ", in ",
-    x$nclusters_pred, " clusters\n"
-  ))
+  if (!is.null(nclusters)) {
+    cat(paste0(
+      "Number of clusters used for selection: ", x$nclusters, "\n"
+    ))
+  } else {
+    cat(paste0(
+      "Number of draws used for selection: ", x$ndraws, "\n"
+    ))
+  }
+  if (!is.null(nclusters_pred)) {
+    cat(paste0(
+      "Number of clusters used for prediction: ", x$nclusters_pred, "\n"
+    ))
+  } else {
+    cat(paste0(
+      "Number of draws used for prediction: ", x$ndraws_pred, "\n"
+    ))
+  }
   cat(paste0("Suggested Projection Size: ", x$suggested_size, "\n"))
   cat("\n")
   cat("Selection Summary:\n")
@@ -955,8 +965,8 @@ t.list <- function(x, ...) {
 #' @export
 as.matrix.projection <- function(x, ...) {
   if (x$p_type) {
-    warning(paste0(
-      "Note, that projection was performed using",
+    warning(paste(
+      "Note that projection was performed using",
       "clustering and the clusters might have different weights."
     ))
   }

R/project.R
@@ -203,7 +203,7 @@ project <- function(object, nterms = NULL, solution_terms = NULL,
   ## add family
   proj <- lapply(subm, function(model) {
     model <- c(model, nlist(family))
-    model$p_type <- is.null(ndraws)
+    model$p_type <- !is.null(nclusters)
     model$intercept <- intercept
     model$extract_model_data <- refmodel$extract_model_data
     model$refmodel <- refmodel

commit 5a5628f1ae1ca28c11fd15946174da5f001c0dde

Fix a bug introduced by PR #128.

R/methods.R
@@ -578,7 +578,7 @@ print.vselsummary <- function(x, digits = 1, ...) {
   nterms_max <- max(x$selection$size)
   cat(paste0("Search method: ", x$method, ", maximum number of terms ",
              nterms_max, "\n"))
-  if (!is.null(nclusters)) {
+  if (!is.null(x$nclusters)) {
     cat(paste0(
       "Number of clusters used for selection: ", x$nclusters, "\n"
     ))
@@ -587,7 +587,7 @@ print.vselsummary <- function(x, digits = 1, ...) {
       "Number of draws used for selection: ", x$ndraws, "\n"
     ))
   }
-  if (!is.null(nclusters_pred)) {
+  if (!is.null(x$nclusters_pred)) {
     cat(paste0(
       "Number of clusters used for prediction: ", x$nclusters_pred, "\n"
     ))