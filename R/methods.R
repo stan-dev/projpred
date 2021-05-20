stan-dev/projpred

commit c51b86cc2dfc69a8b5f424534cc83f49f31524b4

Fix `ndraws` remnants (#128)
* Fix a bug caused by the fact that `ndraws` must be non-NULL now (since commit 16d5afd40908207ebdc6da32e3b6671bf6c581b5).

* Fix a warning message.

* Fix output of print.vselsummary() (either `ndraws` or `nclusters` is used, but not both) and take into account that `ndraws` must be non-NULL now (since commit 16d5afd40908207ebdc6da32e3b6671bf6c581b5).

R/methods.R
@@ -578,14 +578,24 @@ print.vselsummary <- function(x, digits = 1, ...) {
   nterms_max <- max(x$selection$size)
   cat(paste0("Search method: ", x$method, ", maximum number of terms ",
              nterms_max, "\n"))
-  cat(paste0(
-    "Draws used for selection: ", x$ndraws, ", in ",
-    x$nclusters, " clusters\n"
-  ))
-  cat(paste0(
-    "Draws used for prediction: ", x$ndraws_pred, ", in ",
-    x$nclusters_pred, " clusters\n"
-  ))
+  if (!is.null(nclusters)) {
+    cat(paste0(
+      "Number of clusters used for selection: ", x$nclusters, "\n"
+    ))
+  } else {
+    cat(paste0(
+      "Number of draws used for selection: ", x$ndraws, "\n"
+    ))
+  }
+  if (!is.null(nclusters_pred)) {
+    cat(paste0(
+      "Number of clusters used for prediction: ", x$nclusters_pred, "\n"
+    ))
+  } else {
+    cat(paste0(
+      "Number of draws used for prediction: ", x$ndraws_pred, "\n"
+    ))
+  }
   cat(paste0("Suggested Projection Size: ", x$suggested_size, "\n"))
   cat("\n")
   cat("Selection Summary:\n")
@@ -955,8 +965,8 @@ t.list <- function(x, ...) {
 #' @export
 as.matrix.projection <- function(x, ...) {
   if (x$p_type) {
-    warning(paste0(
-      "Note, that projection was performed using",
+    warning(paste(
+      "Note that projection was performed using",
       "clustering and the clusters might have different weights."
     ))
   }

R/project.R
@@ -203,7 +203,7 @@ project <- function(object, nterms = NULL, solution_terms = NULL,
   ## add family
   proj <- lapply(subm, function(model) {
     model <- c(model, nlist(family))
-    model$p_type <- is.null(ndraws)
+    model$p_type <- !is.null(nclusters)
     model$intercept <- intercept
     model$extract_model_data <- refmodel$extract_model_data
     model$refmodel <- refmodel

commit 58b099d88cc301c22b43617a48f56ffa75f576f9

Fix PR #128 (#129)
* Suppress an expected warning when using as.matrix.projection() for a clustered projection.

* Fix a bug introduced by PR #128.

R/methods.R
@@ -578,7 +578,7 @@ print.vselsummary <- function(x, digits = 1, ...) {
   nterms_max <- max(x$selection$size)
   cat(paste0("Search method: ", x$method, ", maximum number of terms ",
              nterms_max, "\n"))
-  if (!is.null(nclusters)) {
+  if (!is.null(x$nclusters)) {
     cat(paste0(
       "Number of clusters used for selection: ", x$nclusters, "\n"
     ))
@@ -587,7 +587,7 @@ print.vselsummary <- function(x, digits = 1, ...) {
       "Number of draws used for selection: ", x$ndraws, "\n"
     ))
   }
-  if (!is.null(nclusters_pred)) {
+  if (!is.null(x$nclusters_pred)) {
     cat(paste0(
       "Number of clusters used for prediction: ", x$nclusters_pred, "\n"
     ))

tests/testthat/test_as_matrix.R
@@ -88,7 +88,7 @@ if (require(rstanarm)) {
     p_clust <- project(vs_gauss,
                        solution_terms = vs_gauss$solution_terms[solution_terms],
                        nclusters = nclusters)
-    m <- as.matrix(p_clust)
+    SW(m <- as.matrix(p_clust))
     expect_length(
       setdiff(
         colnames(m),

tests/testthat/test_project.R
@@ -69,7 +69,8 @@ if (require(rstanarm)) {
         )
         # number of ndraws should equal to the number of draw weights
         ndraws <- length(p[[j]]$weights)
-        ## expect_equal(NROW(as.matrix(p[[j]])), ndraws)
+        ## SW(ndraws_prj <- NROW(as.matrix(p[[j]])))
+        ## expect_equal(ndraws_prj, ndraws)
         expect_length(p[[j]]$dis, ndraws)
         # j:th element should have j variables, including the intercept
         expect_length(p[[j]]$solution_terms, max(j - 1, 1))
@@ -210,7 +211,8 @@ if (require(rstanarm)) {
       p <- project(vs_list[[i]], ndraws = ndraws, nterms = nterms)
       # expected number of ndraws
       expect_length(p$weights, ndraws)
-      expect_equal(NROW(as.matrix(p)), ndraws)
+      SW(ndraws_prj <- NROW(as.matrix(p)))
+      expect_equal(ndraws_prj, ndraws)
       expect_equal(p$weights, 1, info = i_inf)
     }
   })
@@ -222,7 +224,8 @@ if (require(rstanarm)) {
       p <- project(vs_list[[i]], ndraws = ndraws, nterms = nterms)
       # expected number of ndraws
       expect_length(p$weights, ndraws)
-      ## expect_equal(NROW(as.matrix(p)), ndraws)
+      ## SW(ndraws_prj <- NROW(as.matrix(p)))
+      ## expect_equal(ndraws_prj, ndraws)
     }
   })
 