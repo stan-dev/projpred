a6a1e05e83701ae5f50c937ecffd7772fb989438
fweber144 <fweber144@protonmail.com>
2021-06-08 21:33:13 +0200

    Major refactoring of `test_as_matrix.R`.

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,39 +1,136 @@
-seed <- 1235
-set.seed(seed)
-n <- 40L
-nterms <- 5L
-x <- matrix(rnorm(n * nterms, 0, 1), n, nterms)
-b <- runif(nterms) - 0.5
-dis <- runif(1L, 1, 2)
-weights <- sample(1:4, n, replace = TRUE)
-offset <- rnorm(n)
-chains <- 2L
-iter <- 500L
-source(testthat::test_path("helpers", "SW.R"))
+#_______________________________________________________________________________
+# Setup for the unit tests
+#_______________________________________________________________________________
 
-f_gauss <- gaussian()
-df_gauss <- data.frame(y = rnorm(n, f_gauss$linkinv(x %*% b), dis), x = x)
-f_binom <- binomial()
-df_binom <- data.frame(y = rbinom(n, weights, f_binom$linkinv(x %*% b)),
-                       x = x, weights = weights, offset = offset)
+# General setup -----------------------------------------------------------
+
+seed_tst <- 1235
+set.seed(seed_tst)
+source(testthat::test_path("helpers", "SW.R"))
 fam_nms <- setNames(nm = c("gauss", "binom"))
-ys <- lapply(fam_nms, function(fam_nm) {
-  get(paste0("df_", fam_nm))$y
+
+# rstanarm setup ----------------------------------------------------------
+
+chains_tst <- 2L
+iter_tst <- 500L
+
+# projpred setup ----------------------------------------------------------
+
+nclusters_tst <- 2L
+nclusters_pred_tst <- 3L
+nresample_clusters_tst <- 100L
+nresample_clusters_default <- 1000L # Adopt this if the default is changed.
+seed2_tst <- 866028
+solterms_tst <- c("x.2", "x.4")
+
+# GLMs --------------------------------------------------------------------
+
+## Data -------------------------------------------------------------------
+
+n_obs <- 40L
+nterms_pop <- 5L
+x_pop <- matrix(rnorm(n_obs * nterms_pop, 0, 1), n_obs, nterms_pop)
+b_pop <- runif(nterms_pop) - 0.5
+icpt <- -0.42
+offs <- rnorm(n_obs)
+eta_glm <- icpt +
+  x_pop %*% b_pop +
+  offs
+disp <- runif(1L, 1, 2)
+w_obs <- sample(1:4, n_obs, replace = TRUE)
+f_gauss <- gaussian()
+f_binom <- binomial()
+df_glm <- data.frame(y_gauss = rnorm(n_obs, f_gauss$linkinv(eta_glm), disp),
+                     y_binom = rbinom(n_obs, w_obs, f_binom$linkinv(eta_glm)),
+                     x = x_pop, w_obs_col = w_obs, offs_col = offs)
+ys_glm <- lapply(fam_nms, function(fam_nm) {
+  df_glm[[paste0("y_", fam_nm)]]
 })
+
+## Fit --------------------------------------------------------------------
+
 SW({
-  fit_gauss <- rstanarm::stan_glm(
-    y ~ x.1 + x.2 + x.3 + x.4 + x.5,
-    family = f_gauss, data = df_gauss,
-    weights = weights, offset = offset,
-    chains = chains, seed = seed, iter = iter, QR = TRUE
+  fit_gauss_glm <- rstanarm::stan_glm(
+    y_gauss ~ x.1 + x.2 + x.3 + x.4 + x.5,
+    family = f_gauss, data = df_glm,
+    weights = w_obs, offset = offs,
+    chains = chains_tst, seed = seed_tst, iter = iter_tst, QR = TRUE
   )
-  fit_binom <- rstanarm::stan_glm(
-    cbind(y, weights - y) ~ x.1 + x.2 + x.3 + x.4 + x.5,
-    family = f_binom, data = df_binom,
-    offset = offset, # `weights` is not needed when using the cbind() syntax
-    chains = chains, seed = seed, iter = iter
+  fit_binom_glm <- rstanarm::stan_glm(
+    cbind(y_binom, w_obs_col - y_binom) ~ x.1 + x.2 + x.3 + x.4 + x.5,
+    family = f_binom, data = df_glm,
+    offset = offs, # `weights` is not needed when using the cbind() syntax
+    chains = chains_tst, seed = seed_tst, iter = iter_tst
   )
 })
-fit_list <- lapply(fam_nms, function(fam_nm) {
-  get(paste0("fit_", fam_nm))
+fits_glm <- lapply(fam_nms, function(fam_nm) {
+  get(paste0("fit_", fam_nm, "_glm"))
 })
+
+## projpred ---------------------------------------------------------------
+
+# For the binomial family with > 1 trials, we currently expect the warning
+# "Using formula(x) is deprecated when x is a character vector of length > 1"
+# (see GitHub issue #136), so temporarily wrap the following call in SW():
+SW(refmods_glm <- lapply(fits_glm, get_refmodel))
+vss_glm <- lapply(refmods_glm, varsel,
+                  nclusters = nclusters_tst,
+                  nclusters_pred = nclusters_pred_tst,
+                  nterms_max = nterms_pop, verbose = FALSE)
+
+# GLMMs -------------------------------------------------------------------
+
+## Data -------------------------------------------------------------------
+
+ngr <- 8L
+xgr <- gl(n = ngr, k = floor(n_obs / ngr), length = n_obs,
+          labels = paste0("gr", seq_len(ngr)))
+bgr_icpts <- rnorm(ngr, sd = 0.8)
+bgr_x.1 <- rnorm(ngr, sd = 0.8)
+eta_glmm <- icpt +
+  x_pop %*% b_pop +
+  bgr_icpts[xgr] +
+  bgr_x.1[xgr] * x_pop[, 1] +
+  offs
+df_glmm <- data.frame(y_gauss = rnorm(n_obs, f_gauss$linkinv(eta_glmm), disp),
+                      y_binom = rbinom(n_obs, w_obs, f_binom$linkinv(eta_glmm)),
+                      x = x_pop, x.gr = xgr, w_obs_col = w_obs, offs_col = offs)
+ys_glmm <- lapply(fam_nms, function(fam_nm) {
+  df_glmm[[paste0("y_", fam_nm)]]
+})
+
+# Add the number of multilevel terms to the number of population-level terms to
+# obtain the total number of terms:
+nterms_all <- nterms_pop + length(c("(1 | x.gr)", "(x.1 | x.gr)"))
+
+## Fit --------------------------------------------------------------------
+
+SW({
+  fit_gauss_glmm <- rstanarm::stan_glmer(
+    y_gauss ~ x.1 + x.2 + x.3 + x.4 + x.5 + (x.1 | x.gr),
+    family = f_gauss, data = df_glmm,
+    weights = w_obs, offset = offs,
+    chains = chains_tst, seed = seed_tst, iter = iter_tst, QR = TRUE
+  )
+  fit_binom_glmm <- rstanarm::stan_glmer(
+    cbind(y_binom, w_obs_col - y_binom) ~
+      x.1 + x.2 + x.3 + x.4 + x.5 + (x.1 | x.gr),
+    family = f_binom, data = df_glmm,
+    offset = offs, # `weights` is not needed when using the cbind() syntax
+    chains = chains_tst, seed = seed_tst, iter = iter_tst
+  )
+})
+fits_glmm <- lapply(fam_nms, function(fam_nm) {
+  get(paste0("fit_", fam_nm, "_glmm"))
+})
+
+## projpred ---------------------------------------------------------------
+
+# For the binomial family with > 1 trials, we currently expect the warning
+# "Using formula(x) is deprecated when x is a character vector of length > 1"
+# (see GitHub issue #136), so temporarily wrap the following call in SW():
+SW(refmods_glmm <- lapply(fits_glmm, get_refmodel))
+vss_glmm <- lapply(refmods_glmm, varsel,
+                   nclusters = nclusters_tst,
+                   nclusters_pred = nclusters_pred_tst,
+                   nterms_max = nterms_pop, verbose = FALSE)
--- tests/testthat/test_as_matrix.R
+++ tests/testthat/test_as_matrix.R
@@ -3,38 +3,23 @@ context("as.matrix.projection")
 # Gaussian and binomial reference models without multilevel or additive terms:
 settings_list <- list(
   gauss = list(
-    fitobj = fit_gauss,
-    solution_terms_list = list(character(), c("x.3", "x.5")),
-    ndraws_list = list(25, 2, 1)
+    refmod = refmods_glm$gauss,
+    solution_terms_list = list(character(), solterms_tst),
+    ndraws_list = list(25L, 2L, 1L)
   ),
   binom = list(
-    fitobj = fit_binom,
-    solution_terms_list = list(c("x.3", "x.5")),
-    ndraws_list = list(25)
+    refmod = refmods_glm$binom,
+    solution_terms_list = list(solterms_tst),
+    ndraws_list = list(25L)
   )
 )
 
 for (fam_type in settings_list) {
   for (solution_terms in fam_type$solution_terms_list) {
     for (ndraws in fam_type$ndraws_list) {
-      # Expected warning (more precisely: regexp which is matched against the
-      # warning; NA means no warning) for project() and family-specific
-      # parameters:
-      if (family(fam_type$fitobj)$family == "gaussian") {
-        warn_prj_expect <- NA
-        npars_fam <- "sigma"
-      } else if (family(fam_type$fitobj)$family == "binomial") {
-        # For the binomial family with > 1 trials, we expect a warning (see
-        # GitHub issue #136):
-        warn_prj_expect <- paste("Using formula\\(x\\) is deprecated when x",
-                                 "is a character vector of length > 1")
-        npars_fam <- character()
-      }
-
-      expect_warning(prj <- project(fam_type$fitobj,
-                                    solution_terms = solution_terms,
-                                    ndraws = ndraws),
-                     warn_prj_expect)
+      prj <- project(fam_type$refmod,
+                     solution_terms = solution_terms,
+                     ndraws = ndraws)
 
       # Expected warning (more precisely: regexp which is matched against the
       # warning; NA means no warning) for as.matrix.projection():
@@ -47,6 +32,11 @@ for (fam_type in settings_list) {
 
       expect_warning(m <- as.matrix(prj), warn_prjmat_expect)
 
+      if (fam_type$refmod$family$family == "gaussian") {
+        npars_fam <- "sigma"
+      } else if (fam_type$refmod$family$family == "binomial") {
+        npars_fam <- character()
+      }
       test_that("as.matrix.projection()'s output structure is correct", {
         expect_equal(
           dim(m),
@@ -61,152 +51,91 @@ for (fam_type in settings_list) {
   }
 }
 
-# Gaussian and binomial reference models with multilevel but without additive
-# terms:
-if (require(rstanarm)) {
-  seed <- 1235
-  set.seed(seed)
-  n <- 40L
-  nterms <- 7L
-  # Subtract the number of multilevel terms to obtain the number of
-  # population-level terms:
-  nterms_pop <- nterms - 2L
-  x <- matrix(rnorm(n * nterms_pop, 0, 1), n, nterms_pop)
-  b <- runif(nterms_pop) - 0.5
-  dis <- runif(1L, 1, 2)
-  weights <- sample(1:4, n, replace = TRUE)
-  offset <- rnorm(n)
-  icpt <- -0.42
-  ngr <- 8L
-  xgr <- gl(n = ngr, k = floor(n / ngr), length = n,
-            labels = paste0("gr", seq_len(ngr)))
-  bgr_icpts <- rnorm(ngr, sd = 0.8)
-  bgr_x.1 <- rnorm(ngr, sd = 0.8)
-  eta <- icpt +
-    x %*% b +
-    bgr_icpts[xgr] +
-    bgr_x.1[xgr] * x[, 1] +
-    offset
-  chains <- 2L
-  iter <- 500L
-  source(testthat::test_path("helpers", "SW.R"))
+# GLMMs -------------------------------------------------------------------
 
-  f_gauss <- gaussian()
-  df_gauss <- data.frame(y = rnorm(n, f_gauss$linkinv(eta), dis),
-                         x = x, xgr = xgr, weights = weights, offset = offset)
-  f_binom <- binomial()
-  df_binom <- data.frame(y = rbinom(n, weights, f_binom$linkinv(eta)),
-                         x = x, xgr = xgr, weights = weights, offset = offset)
-
-  SW({
-    fit_gauss <- stan_glmer(
-      y ~ x.1 + x.2 + x.3 + x.4 + x.5 + (x.1 | xgr),
-      data = df_gauss, family = f_gauss,
-      weights = weights, offset = offset,
-      chains = chains, seed = seed, iter = iter
-    )
-    fit_binom <- stan_glmer(
-      cbind(y, weights - y) ~ x.1 + x.2 + x.3 + x.4 + x.5 + (x.1 | xgr),
-      data = df_binom, family = f_binom,
-      weights = weights, offset = offset,
-      chains = chains, seed = seed, iter = iter
-    )
-  })
-
-  settings_list <- list(
-    gauss = list(
-      fitobj = fit_gauss,
-      solution_terms_list = list(character(),
-                                 c("x.3", "x.5"),
-                                 c("x.3", "(1 | xgr)", "x.1 + (x.1 | xgr)")),
-      ndraws_list = list(25, 2, 1)
-    ),
-    binom = list(
-      fitobj = fit_binom,
-      solution_terms_list = list(c("x.3", "x.5")),
-      ndraws_list = list(25)
-    )
+settings_list <- list(
+  gauss = list(
+    refmod = refmods_glmm$gauss,
+    solution_terms_list = list(character(),
+                               solterms_tst,
+                               c("x.3", "(1 | xgr)", "x.1 + (x.1 | xgr)")),
+    ndraws_list = list(25L, 2L, 1L)
+  ),
+  binom = list(
+    refmod = refmods_glmm$binom,
+    solution_terms_list = list(solterms_tst),
+    ndraws_list = list(25L)
   )
+)
 
-  for (fam_type in settings_list) {
-    for (solution_terms in fam_type$solution_terms_list) {
-      for (ndraws in fam_type$ndraws_list) {
-        # Expected warning (more precisely: regexp which is matched against the
-        # warning; NA means no warning) for project() and family-specific
-        # parameters:
-        if (family(fam_type$fitobj)$family == "gaussian") {
-          warn_prj_expect <- NA
-          npars_fam <- "sigma"
-        } else if (family(fam_type$fitobj)$family == "binomial") {
-          # For the binomial family with > 1 trials, we expect a warning (see
-          # GitHub issue #136):
-          warn_prj_expect <- paste("Using formula\\(x\\) is deprecated when x",
-                                   "is a character vector of length > 1")
-          npars_fam <- character()
-        }
+for (fam_type in settings_list) {
+  for (solution_terms in fam_type$solution_terms_list) {
+    for (ndraws in fam_type$ndraws_list) {
+      prj <- project(fam_type$refmod,
+                     solution_terms = solution_terms,
+                     ndraws = ndraws)
 
-        expect_warning(prj <- project(fam_type$fitobj,
-                                      solution_terms = solution_terms,
-                                      ndraws = ndraws),
-                       warn_prj_expect)
-
-        # Expected warning (more precisely: regexp which is matched against the
-        # warning; NA means no warning) for as.matrix.projection():
-        if (ndraws > 20) {
-          warn_prjmat_expect <- NA
-        } else {
-          # Clustered projection, so we expect a warning:
-          warn_prjmat_expect <- "the clusters might have different weights"
-        }
-
-        expect_warning(m <- as.matrix(prj), warn_prjmat_expect)
-
-        test_that("as.matrix.projection()'s output structure is correct", {
-          colnms_prjmat_expect <- c(
-            "Intercept",
-            grep("^x\\.[[:digit:]]$", solution_terms,
-                 value = TRUE)
-          )
-          if ("x.1 + (x.1 | xgr)" %in% solution_terms) {
-            colnms_prjmat_expect <- c(colnms_prjmat_expect, "x.1")
-          }
-          colnms_prjmat_expect <- paste0("b_", colnms_prjmat_expect)
-          if ("(1 | xgr)" %in% solution_terms) {
-            colnms_prjmat_expect <- c(
-              colnms_prjmat_expect,
-              "sd_xgr__Intercept"
-            )
-          }
-          if ("x.1 + (x.1 | xgr)" %in% solution_terms) {
-            colnms_prjmat_expect <- c(
-              colnms_prjmat_expect,
-              "sd_xgr__x.1"
-            )
-          }
-          if (all(c("(1 | xgr)", "x.1 + (x.1 | xgr)") %in% solution_terms)) {
-            colnms_prjmat_expect <- c(
-              colnms_prjmat_expect,
-              "cor_xgr__Intercept__x.1"
-            )
-          }
-          if ("(1 | xgr)" %in% solution_terms) {
-            colnms_prjmat_expect <- c(
-              colnms_prjmat_expect,
-              paste0("r_xgr[gr", seq_len(ngr), ",Intercept]")
-            )
-          }
-          if ("x.1 + (x.1 | xgr)" %in% solution_terms) {
-            colnms_prjmat_expect <- c(
-              colnms_prjmat_expect,
-              paste0("r_xgr[gr", seq_len(ngr), ",x.1]")
-            )
-          }
-          colnms_prjmat_expect <- c(colnms_prjmat_expect, npars_fam)
-
-          expect_equal(dim(m), c(ndraws, length(colnms_prjmat_expect)))
-          expect_identical(colnames(m), colnms_prjmat_expect)
-        })
+      # Expected warning (more precisely: regexp which is matched against the
+      # warning; NA means no warning) for as.matrix.projection():
+      if (ndraws > 20) {
+        warn_prjmat_expect <- NA
+      } else {
+        # Clustered projection, so we expect a warning:
+        warn_prjmat_expect <- "the clusters might have different weights"
       }
+
+      expect_warning(m <- as.matrix(prj), warn_prjmat_expect)
+
+      if (fam_type$refmod$family$family == "gaussian") {
+        npars_fam <- "sigma"
+      } else if (fam_type$refmod$family$family == "binomial") {
+        npars_fam <- character()
+      }
+      test_that("as.matrix.projection()'s output structure is correct", {
+        colnms_prjmat_expect <- c(
+          "Intercept",
+          grep("^x\\.[[:digit:]]$", solution_terms,
+               value = TRUE)
+        )
+        if ("x.1 + (x.1 | xgr)" %in% solution_terms) {
+          colnms_prjmat_expect <- c(colnms_prjmat_expect, "x.1")
+        }
+        colnms_prjmat_expect <- paste0("b_", colnms_prjmat_expect)
+        if ("(1 | xgr)" %in% solution_terms) {
+          colnms_prjmat_expect <- c(
+            colnms_prjmat_expect,
+            "sd_xgr__Intercept"
+          )
+        }
+        if ("x.1 + (x.1 | xgr)" %in% solution_terms) {
+          colnms_prjmat_expect <- c(
+            colnms_prjmat_expect,
+            "sd_xgr__x.1"
+          )
+        }
+        if (all(c("(1 | xgr)", "x.1 + (x.1 | xgr)") %in% solution_terms)) {
+          colnms_prjmat_expect <- c(
+            colnms_prjmat_expect,
+            "cor_xgr__Intercept__x.1"
+          )
+        }
+        if ("(1 | xgr)" %in% solution_terms) {
+          colnms_prjmat_expect <- c(
+            colnms_prjmat_expect,
+            paste0("r_xgr[gr", seq_len(ngr), ",Intercept]")
+          )
+        }
+        if ("x.1 + (x.1 | xgr)" %in% solution_terms) {
+          colnms_prjmat_expect <- c(
+            colnms_prjmat_expect,
+            paste0("r_xgr[gr", seq_len(ngr), ",x.1]")
+          )
+        }
+        colnms_prjmat_expect <- c(colnms_prjmat_expect, npars_fam)
+
+        expect_equal(dim(m), c(ndraws, length(colnms_prjmat_expect)))
+        expect_identical(colnames(m), colnms_prjmat_expect)
+      })
     }
   }
 }
