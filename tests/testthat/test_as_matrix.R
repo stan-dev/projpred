7804ad3e971241fbc7e91098e2047b4fcba6bbdb
fweber144 <fweber144@protonmail.com>
2021-06-08 17:54:26 +0200

    Move the setup from `tests/testthat/test_as_matrix.R` to `tests/testthat/setup.R`.

--- tests/testthat/test_as_matrix.R
+++ tests/testthat/test_as_matrix.R
@@ -2,42 +2,6 @@ context("as.matrix.projection")
 
 # Gaussian and binomial reference models without multilevel or additive terms:
 if (require(rstanarm)) {
-  seed <- 1235
-  set.seed(seed)
-  n <- 40L
-  nterms <- 5L
-  x <- matrix(rnorm(n * nterms, 0, 1), n, nterms)
-  b <- runif(nterms) - 0.5
-  dis <- runif(1L, 1, 2)
-  weights <- sample(1:4, n, replace = TRUE)
-  offset <- rnorm(n)
-  chains <- 2L
-  iter <- 500L
-  source(testthat::test_path("helpers", "SW.R"))
-
-  f_gauss <- gaussian()
-  df_gauss <- data.frame(y = rnorm(n, f_gauss$linkinv(x %*% b), dis), x = x)
-  f_binom <- binomial()
-  df_binom <- data.frame(y = rbinom(n, weights, f_binom$linkinv(x %*% b)),
-                         x = x, weights = weights, offset = offset)
-  fam_nms <- setNames(nm = c("gauss", "binom"))
-  ys <- lapply(fam_nms, function(fam_nm) {
-    get(paste0("df_", fam_nm))$y
-  })
-  SW({
-    fit_gauss <- stan_glm(y ~ x.1 + x.2 + x.3 + x.4 + x.5,
-                          family = f_gauss, data = df_gauss,
-                          weights = weights, offset = offset,
-                          chains = chains, seed = seed, iter = iter, QR = TRUE)
-    fit_binom <- stan_glm(cbind(y, weights - y) ~ x.1 + x.2 + x.3 + x.4 + x.5,
-                          family = f_binom, data = df_binom,
-                          weights = weights,
-                          chains = chains, seed = seed, iter = iter)
-  })
-  fit_list <- lapply(fam_nms, function(fam_nm) {
-    get(paste0("fit_", fam_nm))
-  })
-
   settings_list <- list(
     gauss = list(
       fitobj = fit_gauss,
