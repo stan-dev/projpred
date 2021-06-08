1887a06f8115a3d80335ed1a3cecff4f281528d8
fweber144 <fweber144@protonmail.com>
2021-06-08 20:03:00 +0200

    Don't attach rstanarm in `test_as_matrix.R`.

--- tests/testthat/test_as_matrix.R
+++ tests/testthat/test_as_matrix.R
@@ -1,64 +1,62 @@
 context("as.matrix.projection")
 
 # Gaussian and binomial reference models without multilevel or additive terms:
-if (require(rstanarm)) {
-  settings_list <- list(
-    gauss = list(
-      fitobj = fit_gauss,
-      solution_terms_list = list(character(), c("x.3", "x.5")),
-      ndraws_list = list(25, 2, 1)
-    ),
-    binom = list(
-      fitobj = fit_binom,
-      solution_terms_list = list(c("x.3", "x.5")),
-      ndraws_list = list(25)
-    )
+settings_list <- list(
+  gauss = list(
+    fitobj = fit_gauss,
+    solution_terms_list = list(character(), c("x.3", "x.5")),
+    ndraws_list = list(25, 2, 1)
+  ),
+  binom = list(
+    fitobj = fit_binom,
+    solution_terms_list = list(c("x.3", "x.5")),
+    ndraws_list = list(25)
   )
+)
 
-  for (fam_type in settings_list) {
-    for (solution_terms in fam_type$solution_terms_list) {
-      for (ndraws in fam_type$ndraws_list) {
-        # Expected warning (more precisely: regexp which is matched against the
-        # warning; NA means no warning) for project() and family-specific
-        # parameters:
-        if (family(fam_type$fitobj)$family == "gaussian") {
-          warn_prj_expect <- NA
-          npars_fam <- "sigma"
-        } else if (family(fam_type$fitobj)$family == "binomial") {
-          # For the binomial family with > 1 trials, we expect a warning (see
-          # GitHub issue #136):
-          warn_prj_expect <- paste("Using formula\\(x\\) is deprecated when x",
-                                   "is a character vector of length > 1")
-          npars_fam <- character()
-        }
-
-        expect_warning(prj <- project(fam_type$fitobj,
-                                      solution_terms = solution_terms,
-                                      ndraws = ndraws),
-                       warn_prj_expect)
-
-        # Expected warning (more precisely: regexp which is matched against the
-        # warning; NA means no warning) for as.matrix.projection():
-        if (ndraws > 20) {
-          warn_prjmat_expect <- NA
-        } else {
-          # Clustered projection, so we expect a warning:
-          warn_prjmat_expect <- "the clusters might have different weights"
-        }
-
-        expect_warning(m <- as.matrix(prj), warn_prjmat_expect)
-
-        test_that("as.matrix.projection()'s output structure is correct", {
-          expect_equal(
-            dim(m),
-            c(ndraws, length(solution_terms) + 1 + length(npars_fam))
-          )
-          expect_identical(
-            colnames(m),
-            c(paste0("b_", c("Intercept", solution_terms)), npars_fam)
-          )
-        })
+for (fam_type in settings_list) {
+  for (solution_terms in fam_type$solution_terms_list) {
+    for (ndraws in fam_type$ndraws_list) {
+      # Expected warning (more precisely: regexp which is matched against the
+      # warning; NA means no warning) for project() and family-specific
+      # parameters:
+      if (family(fam_type$fitobj)$family == "gaussian") {
+        warn_prj_expect <- NA
+        npars_fam <- "sigma"
+      } else if (family(fam_type$fitobj)$family == "binomial") {
+        # For the binomial family with > 1 trials, we expect a warning (see
+        # GitHub issue #136):
+        warn_prj_expect <- paste("Using formula\\(x\\) is deprecated when x",
+                                 "is a character vector of length > 1")
+        npars_fam <- character()
       }
+
+      expect_warning(prj <- project(fam_type$fitobj,
+                                    solution_terms = solution_terms,
+                                    ndraws = ndraws),
+                     warn_prj_expect)
+
+      # Expected warning (more precisely: regexp which is matched against the
+      # warning; NA means no warning) for as.matrix.projection():
+      if (ndraws > 20) {
+        warn_prjmat_expect <- NA
+      } else {
+        # Clustered projection, so we expect a warning:
+        warn_prjmat_expect <- "the clusters might have different weights"
+      }
+
+      expect_warning(m <- as.matrix(prj), warn_prjmat_expect)
+
+      test_that("as.matrix.projection()'s output structure is correct", {
+        expect_equal(
+          dim(m),
+          c(ndraws, length(solution_terms) + 1 + length(npars_fam))
+        )
+        expect_identical(
+          colnames(m),
+          c(paste0("b_", c("Intercept", solution_terms)), npars_fam)
+        )
+      })
     }
   }
 }
