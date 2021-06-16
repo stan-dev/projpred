f3e2125dce49cfa40a3802b0ee454978f73bfc93
fweber144 <fweber144@protonmail.com>
2021-06-16 17:42:38 +0200

    `prj_vsel_str_tester()`: Add argument `info_str`.

--- tests/testthat/helpers/testers.R
+++ tests/testthat/helpers/testers.R
@@ -3,13 +3,15 @@
 #
 # @param p An object returned by project().
 # @param fam_expected The expected "family" object.
+# @param info_str A single character string giving info to be printed in case of
+#   failure.
 #
 # @return `TRUE` (invisible).
 #
-prj_vsel_str_tester <- function(p, fam_expected) {
+prj_vsel_str_tester <- function(p, fam_expected, info_str = "") {
   expect_type(p, "list")
   expect_length(p, nterms_max_tst + 1)
-  expect_true(.is_proj_list(p))
+  expect_true(.is_proj_list(p), info = info_str)
 
   prjdraw_weights <- p[[1]]$weights
   for (j in seq_along(p)) {
@@ -17,7 +19,7 @@ prj_vsel_str_tester <- function(p, fam_expected) {
     # Check the names using `ignore.order = FALSE` because an incorrect
     # order would mean that the documentation of project()'s return value
     # would have to be updated:
-    expect_named(p[[!!j]], projection_nms)
+    expect_named(p[[!!j]], projection_nms, info = info_str)
     # Number of projected draws should be equal to the default of `ndraws`
     # (note that more extensive tests for as.matrix.projection() may be
     # found in "test_as_matrix.R"):
@@ -25,23 +27,24 @@ prj_vsel_str_tester <- function(p, fam_expected) {
     expect_length(p[[!!j]]$weights, nclusters_pred_tst)
     expect_length(p[[!!j]]$dis, nclusters_pred_tst)
     SW(nprjdraws <- NROW(as.matrix(p[[!!j]])))
-    expect_identical(nprjdraws, nclusters_pred_tst)
+    expect_identical(nprjdraws, nclusters_pred_tst, info = info_str)
     # The j-th element should have j solution terms (usually excluding the
     # intercept, but counting it for `j == 1`):
     expect_length(p[[!!j]]$solution_terms, max(j - 1, 1))
     # Same check, but using count_terms_chosen():
-    expect_equal(count_terms_chosen(p[[!!j]]$solution_terms), !!j)
-    expect_identical(p[[!!j]]$family, fam_expected)
+    expect_equal(count_terms_chosen(p[[!!j]]$solution_terms), !!j,
+                 info = info_str)
+    expect_identical(p[[!!j]]$family, fam_expected, info = info_str)
     # All submodels should use the same clustering:
-    expect_identical(p[[!!j]]$weights, prjdraw_weights)
+    expect_identical(p[[!!j]]$weights, prjdraw_weights, info = info_str)
   }
   # kl should be non-increasing on training data
   klseq <- sapply(p, function(x) sum(x$kl))
   # Remove intercept from the comparison:
   klseq <- klseq[-1]
-  expect_identical(klseq, cummin(klseq))
+  expect_identical(klseq, cummin(klseq), info = info_str)
   ### Check with tolerance:
-  # expect_true(all(diff(klseq) - 1e-1 < 0))
+  # expect_true(all(diff(klseq) - 1e-1 < 0), info = info_str)
   ###
   return(invisible(TRUE))
 }
