92e57bde62d784627a0455f057a0f659761dd37c
fweber144 <fweber144@protonmail.com>
2021-06-16 17:21:08 +0200

    `test_project.R`: Use `prj_vsel_str_tester()` from `testers.R`.

--- tests/testthat/test_project.R
+++ tests/testthat/test_project.R
@@ -11,45 +11,8 @@ test_that(paste(
   "`object` of class \"vsel\" (created by varsel()) leads to correct output",
   "structure"
 ), {
-  for (i in fam_nms) {
-    p <- project(vss[[tstsetup]], nterms = 0:nterms, nclusters = nclusters_pred_tst)
-    expect_type(p, "list")
-    expect_length(p, nterms + 1)
-    expect_true(.is_proj_list(p), info = tstsetup)
-
-    prjdraw_weights <- p[[1]]$weights
-    for (j in seq_along(p)) {
-      expect_s3_class(p[[!!j]], "projection")
-      # Check the names using `ignore.order = FALSE` because an incorrect
-      # order would mean that the documentation of project()'s return value
-      # would have to be updated:
-      expect_named(p[[!!j]], projection_nms, info = tstsetup)
-      # Number of projected draws should be equal to the default of `ndraws`
-      # (note that more extensive tests for as.matrix.projection() may be
-      # found in "test_as_matrix.R"):
-      expect_length(p[[!!j]]$sub_fit, nclusters_pred_tst)
-      expect_length(p[[!!j]]$weights, nclusters_pred_tst)
-      expect_length(p[[!!j]]$dis, nclusters_pred_tst)
-      SW(nprjdraws <- NROW(as.matrix(p[[!!j]])))
-      expect_identical(nprjdraws, nclusters_pred_tst, info = tstsetup)
-      # The j-th element should have j solution terms (usually excluding the
-      # intercept, but counting it for `j == 1`):
-      expect_length(p[[!!j]]$solution_terms, max(j - 1, 1))
-      # Same check, but using count_terms_chosen():
-      expect_equal(count_terms_chosen(p[[!!j]]$solution_terms), !!j, info = tstsetup)
-      expect_identical(p[[!!j]]$family, vs_list[[!!i]]$family, info = tstsetup)
-      # All submodels should use the same clustering:
-      expect_identical(p[[!!j]]$weights, prjdraw_weights, info = tstsetup)
-    }
-    # kl should be non-increasing on training data
-    klseq <- sapply(p, function(x) sum(x$kl))
-    # Remove intercept from the comparison:
-    klseq <- klseq[-1]
-    expect_identical(klseq, cummin(klseq), info = tstsetup)
-    ### Check with tolerance:
-    # expect_true(all(diff(klseq) - 1e-1 < 0), info = tstsetup)
-    ###
-  }
+  skip_if_not(exists("prj_nterms_vs"))
+  prj_vsel_str_tester(prj_nterms_vs, fam_expected = prj_nterms_vs[[1]]$family)
 })
 
 test_that(paste(
