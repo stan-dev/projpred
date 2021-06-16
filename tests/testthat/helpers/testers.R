395462e4d29846ad2072c0e83d13c08401fa66e4
fweber144 <fweber144@protonmail.com>
2021-06-16 17:20:45 +0200

    `testers.R`: Remove/update old code.

--- tests/testthat/helpers/testers.R
+++ tests/testthat/helpers/testers.R
@@ -1,7 +1,7 @@
-prj_vsel_str_tester <- function(p) {
+prj_vsel_str_tester <- function(p, fam_expected) {
   expect_type(p, "list")
-  expect_length(p, nterms + 1)
-  expect_true(.is_proj_list(p), info = tstsetup)
+  expect_length(p, nterms_max_tst + 1)
+  expect_true(.is_proj_list(p))
 
   prjdraw_weights <- p[[1]]$weights
   for (j in seq_along(p)) {
@@ -9,7 +9,7 @@ prj_vsel_str_tester <- function(p) {
     # Check the names using `ignore.order = FALSE` because an incorrect
     # order would mean that the documentation of project()'s return value
     # would have to be updated:
-    expect_named(p[[!!j]], projection_nms, info = tstsetup)
+    expect_named(p[[!!j]], projection_nms)
     # Number of projected draws should be equal to the default of `ndraws`
     # (note that more extensive tests for as.matrix.projection() may be
     # found in "test_as_matrix.R"):
@@ -17,23 +17,23 @@ prj_vsel_str_tester <- function(p) {
     expect_length(p[[!!j]]$weights, nclusters_pred_tst)
     expect_length(p[[!!j]]$dis, nclusters_pred_tst)
     SW(nprjdraws <- NROW(as.matrix(p[[!!j]])))
-    expect_identical(nprjdraws, nclusters_pred_tst, info = tstsetup)
+    expect_identical(nprjdraws, nclusters_pred_tst)
     # The j-th element should have j solution terms (usually excluding the
     # intercept, but counting it for `j == 1`):
     expect_length(p[[!!j]]$solution_terms, max(j - 1, 1))
     # Same check, but using count_terms_chosen():
-    expect_equal(count_terms_chosen(p[[!!j]]$solution_terms), !!j, info = tstsetup)
-    expect_identical(p[[!!j]]$family, vs_list[[!!i]]$family, info = tstsetup)
+    expect_equal(count_terms_chosen(p[[!!j]]$solution_terms), !!j)
+    expect_identical(p[[!!j]]$family, fam_expected)
     # All submodels should use the same clustering:
-    expect_identical(p[[!!j]]$weights, prjdraw_weights, info = tstsetup)
+    expect_identical(p[[!!j]]$weights, prjdraw_weights)
   }
   # kl should be non-increasing on training data
   klseq <- sapply(p, function(x) sum(x$kl))
   # Remove intercept from the comparison:
   klseq <- klseq[-1]
-  expect_identical(klseq, cummin(klseq), info = tstsetup)
+  expect_identical(klseq, cummin(klseq))
   ### Check with tolerance:
-  # expect_true(all(diff(klseq) - 1e-1 < 0), info = tstsetup)
+  # expect_true(all(diff(klseq) - 1e-1 < 0))
   ###
   return(invisible(TRUE))
 }
