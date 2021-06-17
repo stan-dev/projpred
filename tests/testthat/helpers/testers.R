52f3ecdc6c7e7fbd46e4c60fc6ba10380464566a
fweber144 <fweber144@protonmail.com>
2021-06-17 13:54:36 +0200

    `testers.R`: Use `projection_tester()` in `proj_list_tester()`. Wrap the KL check in `is_seq`. Update (internal) docs.

--- tests/testthat/helpers/testers.R
+++ tests/testthat/helpers/testers.R
@@ -1,62 +1,63 @@
 # A helper function for testing the structure of an expected "proj_list" object
 #
 # @param p An object of (informal) class "proj_list" (at least expected so).
-# @param fam_expected The expected "family" object.
+# @param len_expected The expected length of `p`.
+# @param is_seq A single logical value indicating whether `p` is expected to be
+#   sequential (i.e., the number of solution terms increases by 1 from one
+#   element of `p` to the next).
 # @param info_str A single character string giving information to be printed in
 #   case of failure.
+# @param ... Arguments passed to projection_tester(), apart from
+#   projection_tester()'s arguments `p`, `solterms_expected`, and `info_str`.
 #
 # @return `TRUE` (invisible).
 #
 proj_list_tester <- function(p,
-                             fam_expected,
                              len_expected = nterms_max_tst + 1L,
                              is_seq = TRUE,
-                             info_str = "") {
+                             info_str = "",
+                             ...) {
   expect_type(p, "list")
   expect_length(p, len_expected)
   expect_true(.is_proj_list(p), info = info_str)
 
   prjdraw_weights <- p[[1]]$weights
   for (j in seq_along(p)) {
-    expect_s3_class(p[[!!j]], "projection")
-    # Check the names using `ignore.order = FALSE` because an incorrect
-    # order would mean that the documentation of project()'s return value
-    # would have to be updated:
-    expect_named(p[[!!j]], projection_nms, info = info_str)
-    # Number of projected draws should be equal to the default of `ndraws`
-    # (note that more extensive tests for as.matrix.projection() may be
-    # found in "test_as_matrix.R"):
-    expect_length(p[[!!j]]$sub_fit, nclusters_pred_tst)
-    expect_length(p[[!!j]]$weights, nclusters_pred_tst)
-    expect_length(p[[!!j]]$dis, nclusters_pred_tst)
-    SW(nprjdraws <- NROW(as.matrix(p[[!!j]])))
-    expect_identical(nprjdraws, nclusters_pred_tst, info = info_str)
-    expect_identical(p[[!!j]]$family, fam_expected, info = info_str)
-    # All submodels should use the same clustering:
-    expect_identical(p[[!!j]]$weights, prjdraw_weights, info = info_str)
     if (is_seq) {
-      # The j-th element should have j solution terms (usually excluding the
-      # intercept, but counting it for `j == 1`):
-      expect_length(p[[!!j]]$solution_terms, max(j - 1, 1))
-      # Same check, but using count_terms_chosen():
-      expect_equal(count_terms_chosen(p[[!!j]]$solution_terms), !!j,
-                   info = info_str)
+      # The j-th element should have j solution terms (not counting the
+      # intercept, even for the intercept-only model):
+      solterms_expected_crr <- j - 1
+    } else {
+      solterms_expected_crr <- NULL
     }
+    projection_tester(p[[j]],
+                      solterms_expected = solterms_expected_crr,
+                      info_str = paste(info_str, j, sep = "_"),
+                      ...)
   }
-  # kl should be non-increasing on training data
-  klseq <- sapply(p, function(x) sum(x$kl))
-  # Remove intercept from the comparison:
-  klseq <- klseq[-1]
-  expect_identical(klseq, cummin(klseq), info = info_str)
-  ### Check with tolerance:
-  # expect_true(all(diff(klseq) - 1e-1 < 0), info = info_str)
-  ###
+  if (is_seq) {
+    # kl should be non-increasing on training data
+    klseq <- sapply(p, function(x) sum(x$kl))
+    # Remove intercept from the comparison:
+    klseq <- klseq[-1]
+    expect_identical(klseq, cummin(klseq), info = info_str)
+    ### Check with tolerance:
+    # expect_true(all(diff(klseq) - 1e-1 < 0), info = info_str)
+    ###
+  }
   return(invisible(TRUE))
 }
 
 # A helper function for testing the structure of an expected "projection" object
 #
 # @param p An object of class "projection" (at least expected so).
+# @param solterms_expected Either a single numeric value giving the expected
+#   number of solution terms (not counting the intercept, even for the
+#   intercept-only model), a character vector giving the expected solution
+#   terms, or `NULL` for not testing the solution terms at all.
+# @param nprjdraws_expected A single numeric value giving the expected number of
+#   projected draws.
+# @param fam_expected The expected "family" object.
 # @param info_str A single character string giving information to be printed in
 #   case of failure.
 #
@@ -65,8 +66,12 @@ proj_list_tester <- function(p,
 projection_tester <- function(p,
                               solterms_expected,
                               nprjdraws_expected,
+                              fam_expected,
                               info_str = "") {
   expect_s3_class(p, "projection")
+  # Check the names using `ignore.order = FALSE` because an incorrect
+  # order would mean that the documentation of project()'s return value
+  # would have to be updated:
   expect_named(p, projection_nms, info = info_str)
   if (nprjdraws_expected == 1) {
     nprjdraws_sub_fit <- length(sub_fit_nms)
@@ -76,9 +81,27 @@ projection_tester <- function(p,
   expect_length(p$sub_fit, nprjdraws_sub_fit)
   expect_length(p$weights, nprjdraws_expected)
   expect_length(p$dis, nprjdraws_expected)
+  # Number of projected draws in as.matrix.projection() (note that more
+  # extensive tests for as.matrix.projection() may be found in
+  # "test_as_matrix.R"):
   SW(nprjdraws <- NROW(as.matrix(p)))
   expect_identical(nprjdraws, nprjdraws_expected, info = info_str)
-  solterms_out <- if (length(solterms_expected) == 0) "1" else solterms_expected
-  expect_identical(p$solution_terms, solterms_out, info = info_str)
+  expect_identical(p$family, fam_expected, info = info_str)
+  # All submodels should use the same clustering:
+  expect_identical(p$weights, prjdraw_weights, info = info_str)
+  if (is.numeric(solterms_expected)) {
+    # Do not count the intercept, except for the intercept-only model:
+    expect_length(p$solution_terms, max(solterms_expected, 1))
+    # Same check, but using count_terms_chosen() which counts the intercept:
+    expect_equal(count_terms_chosen(p$solution_terms), solterms_expected + 1,
+                 info = info_str)
+  } else if (is.character(solterms_expected)) {
+    if (length(solterms_expected) == 0) {
+      solterms_out <- "1"
+    } else {
+      solterms_out <- solterms_expected
+    }
+    expect_identical(p$solution_terms, solterms_out, info = info_str)
+  }
   return(invisible(TRUE))
 }
