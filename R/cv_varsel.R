38d762185bcfc5b0a5288587bb2cd2d6856fcfd7
Alejandro Catalina <alecatfel@gmail.com>
2021-06-27 14:35:04 +0300

    fix #161: avoid Inf nclusters_pred when both NULL

--- R/cv_varsel.R
+++ R/cv_varsel.R
@@ -549,10 +549,12 @@ kfold_varsel <- function(refmodel, method, nterms_max, ndraws,
   ## List of K elements, each containing d_train, p_pred, etc. corresponding
   ## to each fold.
   make_list_cv <- function(refmodel, d_test, msg) {
-    nclusters_pred <- min(
-      refmodel$nclusters_pred,
-      nclusters_pred
-    )
+    if (!is.null(nclusters_pred) || !is.null(refmodel$nclusters_pred)) {
+      nclusters_pred <- min(
+        refmodel$nclusters_pred,
+        nclusters_pred
+      )
+    }
     p_sel <- .get_refdist(refmodel, ndraws, nclusters, seed = seed)
     p_pred <- .get_refdist(refmodel, ndraws_pred, nclusters_pred, seed = seed)
     newdata <- d_test$newdata
