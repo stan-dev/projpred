e92238f4c932c8f5698b3bb8df7e05eb00ea8861
fweber144 <fweber144@protonmail.com>
2021-07-07 13:57:22 +0200

    Remove unnecessary comments.

--- tests/testthat/test_project.R
+++ tests/testthat/test_project.R
@@ -104,7 +104,6 @@ test_that(paste(
       })
       tstsetup_match_prj <- tstsetup_tries[match_prj]
       if (length(tstsetup_match_prj) == 1) {
-        # cat("Found match:", tstsetup, "and", tstsetup_match_prj, "\n")
         expect_identical(prjs_vs[[tstsetup]], prjs[[tstsetup_match_prj]],
                          ignore.environment = TRUE, info = tstsetup)
       } else if (length(tstsetup_match_prj) > 1) {
@@ -158,7 +157,6 @@ test_that(paste(
       })
       tstsetup_match_prj <- tstsetup_tries[match_prj]
       if (length(tstsetup_match_prj) == 1) {
-        # cat("Found match:", tstsetup, "and", tstsetup_match_prj, "\n")
         expect_identical(prjs_cvvs[[tstsetup]], prjs[[tstsetup_match_prj]],
                          ignore.environment = TRUE, info = tstsetup)
       } else if (length(tstsetup_match_prj) > 1) {
