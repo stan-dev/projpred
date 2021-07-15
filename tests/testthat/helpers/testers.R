0db104c5d45efd0dc9f7bc0ae41c631f7c552049
fweber144 <fweber144@protonmail.com>
2021-07-15 10:44:32 +0200

    Exclude the check for `pct_solution_terms_cv` in case of `validate_search = FALSE` because of issue #173.

--- tests/testthat/helpers/testers.R
+++ tests/testthat/helpers/testers.R
@@ -318,18 +318,20 @@ vsel_tester <- function(vs,
     expect_identical(vs$pct_solution_terms_cv[, "size"],
                      as.numeric(seq_len(solterms_len_expected)),
                      info = info_str)
-    if (isFALSE(vs$validate_search)) {
-      pct_nonsize_nms <- setdiff(colnames(vs$pct_solution_terms_cv), "size")
-      pct_solterms <- vs$pct_solution_terms_cv[, pct_nonsize_nms, drop = FALSE]
-      expect_true(all(pct_solterms %in% c(0, 1)), info = info_str)
-      # More specifically:
-      pct_solterms_ch <- matrix(0, nrow = nrow(pct_solterms),
-                                ncol = ncol(pct_solterms))
-      pct_solterms_ch[lower.tri(pct_solterms_ch)] <- 1
-      diag(pct_solterms_ch) <- 1
-      colnames(pct_solterms_ch) <- pct_nonsize_nms
-      expect_identical(pct_solterms_ch, pct_solterms, info = info_str)
-    }
+    ### Excluded because of issue #173:
+    # if (isFALSE(vs$validate_search)) {
+    #   pct_nonsize_nms <- setdiff(colnames(vs$pct_solution_terms_cv), "size")
+    #   pct_solterms <- vs$pct_solution_terms_cv[, pct_nonsize_nms, drop = FALSE]
+    #   expect_true(all(pct_solterms %in% c(0, 1)), info = info_str)
+    #   # More specifically:
+    #   pct_solterms_ch <- matrix(0, nrow = nrow(pct_solterms),
+    #                             ncol = ncol(pct_solterms))
+    #   pct_solterms_ch[lower.tri(pct_solterms_ch)] <- 1
+    #   diag(pct_solterms_ch) <- 1
+    #   colnames(pct_solterms_ch) <- pct_nonsize_nms
+    #   expect_identical(pct_solterms_ch, pct_solterms, info = info_str)
+    # }
+    ###
   }
 
   # nterms_max
