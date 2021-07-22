cdf4ff8243b37f7ee69ed9955cd0d8b65fe0a9b6
fweber144 <fweber144@protonmail.com>
2021-07-22 10:55:15 +0200

    `test_extend_family.R`: Add a `TODO` concerning other families.

--- tests/testthat/test_extend_family.R
+++ tests/testthat/test_extend_family.R
@@ -5,6 +5,9 @@ test_that("`family` works", {
   if (!"poiss" %in% fam_nms) {
     fam_nms_tst <- setNames(nm = c(fam_nms_tst, "poiss"))
   }
+  ### TODO: Also test other families extensible by projpred:
+  # fam_nms_tst <- setNames(nm = c(fam_nms_tst, "Gamma", "Student_t"))
+  ###
   for (fam_nm in fam_nms_tst) {
     fam_orig_crr <- get(paste0("f_", fam_nm))
     extfam_tester(
