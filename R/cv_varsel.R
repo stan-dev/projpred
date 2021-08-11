stan-dev/projpred

commit 76d22a166458701d73f793dd67ef1d56e75cc0ff

Minor modifications, mostly documentation (#153)
* Add argument `extract_y` also to `extract_model_data()` in `get_refmodel.default()`.

* Let the user pass arguments via the ellipsis (`...`) from `get_refmodel.default()` to `init_refmodel()`. Enhance documentation in `refmodel.R`. Minor cleaning in `refmodel.R`.

* Re-document.

* Minor cleaning in the docs of `project.R`.

* Fix documentation (KL output of `project()`).

* Update and fix documentation (output of `project()`).

* Re-indent documentation (output of `project()`).

* Re-document.

* Minor enhancements in the documentation of `project()`.

* Remove `glm_forward_c()` and `glm_forward()` (unused).

* Improve documentation of argument `seed` and perform minor cleaning.

* Re-document.

* Add a missing ellipsis.

* `linear_multilevel_mle()`: Remove unused argument `regul`.

* Use a roxygen template for arguments `newdata`, `offsetnew`, `weightsnew`.

* Enhance a comment.

* Don't apply the inverse-link within `ref_predfun_datafit()`.

* In `ref_predfun_datafit()`, the case `!is.null(fit)` should not occur (since a `"refmodel"` of additional class `"datafit"` is only possible if `object` (which is later used as `fit`) is `NULL`).

* Simplify `ref_predfun_datafit()`.

* Document that argument `ref_predfun` is degenerate (

i.e., ignored and the internal placeholder always returns `NA`) if argument
`object` is `NULL` (i.e., for a `"refmodel"` of additional class `"datafit"`).

* Minor cleaning in `varsel.R`.

* Minor cleaning in `cv_varsel.R`.

* Minor cleaning in `methods.R`.

* Enhance documentation for `project()`'s argument `nterms`.

* Remove `fit_glm_callback()` (commented).

* Minor cleaning in `divergence_minimizers.R`.

* `parse_args_varsel()`: Improve readability of `nterms_max`-related code.

* `parse_args_varsel()`: Improve readability of `nterms_max`-related and `intercept`-related code.

R/cv_varsel.R
@@ -146,16 +146,14 @@ cv_varsel.refmodel <- function(object, method = NULL, cv_method = NULL,
                   cv_search = cv_search, nterms_max = nterms_max - 1,
                   intercept = intercept, penalty = penalty, verbose = verbose,
                   lambda_min_ratio = lambda_min_ratio, nlambda = nlambda,
-                  regul = regul, search_terms = search_terms, seed = seed
-    )
+                  regul = regul, search_terms = search_terms, seed = seed)
   } else if (cv_method == "LOO") {
     sel <- sel_cv$sel
   }
 
   candidate_terms <- split_formula(refmodel$formula,
                                    data = refmodel$fetch_data(),
-                                   add_main_effects = FALSE
-  )
+                                   add_main_effects = FALSE)
   ## find out how many of cross-validated iterations select
   ## the same variables as the selection with all the data.
   solution_terms_cv_ch <- sapply(
@@ -170,8 +168,7 @@ cv_varsel.refmodel <- function(object, method = NULL, cv_method = NULL,
   )
   ## make sure it's always a matrix
   solution_terms_cv_ch <- matrix(solution_terms_cv_ch,
-                                 ncol = length(solution_terms)
-  )
+                                 ncol = length(solution_terms))
 
   ## these weights might be non-constant in case of subsampling LOO
   sel_solution_terms <- sel$solution_terms
@@ -186,8 +183,7 @@ cv_varsel.refmodel <- function(object, method = NULL, cv_method = NULL,
         size = size,
         sapply(vars, function(var) {
           sum((solution_terms_cv_ch[seq_len(size), , drop = FALSE] == var) * w,
-              na.rm = TRUE
-          )
+              na.rm = TRUE)
         })
       )
     }
@@ -210,8 +206,7 @@ cv_varsel.refmodel <- function(object, method = NULL, cv_method = NULL,
               nclusters,
               nclusters_pred,
               ndraws,
-              ndraws_pred
-  )
+              ndraws_pred)
   class(vs) <- "vsel"
   vs$suggested_size <- suggest_size(vs, warnings = FALSE)
   summary <- summary(vs)
@@ -298,26 +293,22 @@ loo_varsel <- function(refmodel, method, nterms_max, ndraws,
   p_sel <- .get_refdist(refmodel,
                         ndraws = ndraws,
                         nclusters = nclusters,
-                        seed = seed
-  )
+                        seed = seed)
   cl_sel <- p_sel$cl # clustering information
 
   ## the clustering/subsampling used for prediction
   p_pred <- .get_refdist(refmodel,
                          ndraws = ndraws_pred,
                          nclusters = nclusters_pred,
-                         seed = seed
-  )
+                         seed = seed)
   cl_pred <- p_pred$cl
 
   ## fetch the log-likelihood for the reference model to obtain the LOO weights
   if (is.null(refmodel$loglik)) {
     ## case where log-likelihood not available, i.e., the reference model is not
     ## a genuine model => cannot compute LOO
-    stop(
-      "LOO can be performed only if the reference model is a genuine ",
-      "probabilistic model for which the log-likelihood can be evaluated."
-    )
+    stop("LOO can be performed only if the reference model is a genuine ",
+         "probabilistic model for which the log-likelihood can be evaluated.")
   } else {
     ## log-likelihood available
     loglik <- refmodel$loglik
@@ -398,8 +389,7 @@ loo_varsel <- function(refmodel, method, nterms_max, ndraws,
       mu_k <- family$mu_fun(submodels[[k]]$sub_fit,
                             obs = inds,
                             offset = refmodel$offset,
-                            weights = 1
-      )
+                            weights = 1)
       log_lik_sub <- t(family$ll_fun(
         mu_k, submodels[[k]]$dis,
         y[inds], refmodel$wobs
@@ -425,8 +415,7 @@ loo_varsel <- function(refmodel, method, nterms_max, ndraws,
 
     candidate_terms <- split_formula(refmodel$formula,
                                      data = refmodel$fetch_data(),
-                                     add_main_effects = FALSE
-    )
+                                     add_main_effects = FALSE)
     ## with `match` we get the indices of the variables as they enter the
     ## solution path in solution_terms
     solution <- match(solution_terms, candidate_terms[-1])
@@ -484,8 +473,7 @@ loo_varsel <- function(refmodel, method, nterms_max, ndraws,
 
       candidate_terms <- split_formula(refmodel$formula,
                                        data = refmodel$fetch_data(),
-                                       add_main_effects = FALSE
-      )
+                                       add_main_effects = FALSE)
       ## with `match` we get the indices of the variables as they enter the
       ## solution path in solution_terms
       solution <- match(solution_terms, candidate_terms[-1])
@@ -574,14 +562,12 @@ kfold_varsel <- function(refmodel, method, nterms_max, ndraws,
     )
     mu_test <- family$linkinv(pred)
     nlist(refmodel, p_sel, p_pred, mu_test,
-          dis = refmodel$dis, w_test = refmodel$wsample, d_test, msg
-    )
+          dis = refmodel$dis, w_test = refmodel$wsample, d_test, msg)
   }
 
   msgs <- paste0(method, " search for fold ", 1:K, "/", K, ".")
   list_cv <- mapply(make_list_cv, refmodels_cv, d_test_cv, msgs,
-                    SIMPLIFY = FALSE
-  )
+                    SIMPLIFY = FALSE)
 
   ## Perform the selection for each of the K folds
   if (verbose) {
@@ -630,8 +616,7 @@ kfold_varsel <- function(refmodel, method, nterms_max, ndraws,
   }
 
   p_sub_cv <- mapply(get_submodels_cv, search_path_cv, seq_along(list_cv),
-                     SIMPLIFY = FALSE
-  )
+                     SIMPLIFY = FALSE)
   if (verbose && cv_search) {
     close(pb)
   }
@@ -678,8 +663,7 @@ kfold_varsel <- function(refmodel, method, nterms_max, ndraws,
 
   return(nlist(solution_terms_cv,
                summaries = list(sub = sub, ref = ref),
-               d_test = c(d_cv, type = "kfold")
-  ))
+               d_test = c(d_cv, type = "kfold")))
 }
 
 
@@ -761,7 +745,7 @@ kfold_varsel <- function(refmodel, method, nterms_max, ndraws,
     refmodel$proj_predfun(fit, newdata = newdata, weights = weights)
   }
   extract_model_data <- function(object, newdata = fetch_fold(), ...) {
-    refmodel$extract_model_data(object = object, newdata = newdata)
+    refmodel$extract_model_data(object = object, newdata = newdata, ...)
   }
   if (!inherits(cvfit, "brmsfit") && !inherits(cvfit, "stanreg")) {
     fit <- NULL

commit 672a53e6ef61473340de55a25bd1a1228cd3b0d6

Collection of minor changes (#189)
* Avoid `R CMD check` NOTE "no visible global function definition for ‘tail’".

* Add package optimx to `Suggests:` field (may be needed when running tests via `R CMD check`).

* Comment `get_replace_response()` (seems to be unused).

* Revert/adjust an incorrect comment modification introduced by PR #153.

* `ref_predfun_datafit()`: Remove unused argument `offset`.

* `print.vselsummary()`: Remove object `nterms_max` (and replace its later occurrence) to

make it clear that `print.vselsummary()` has no purpose other than printing.

* Minor cleaning.

* Define `hf()` (from `kfold_varsel()`) globally.

* Comment `.loo_subsample()` (seems to be unused).

* Fix a typo in a comment.

* Consistently use `inherits()`.

* `project()`: Improve readability of a modification of `cv_search`.

* Remove an unnecessary comment.

* `parse_args_cv_varsel()`: Remove unnecessary redefinitions of `nclusters` and `nclusters_pred`.

* `.get_sub_summaries()`: Remove unused `solution_terms` object.

* Enhance a comment.

* Replace the abbreviation `object$fam$` by `object$family$` to avoid ambiguities.

* Remove unused object `w` in `predict.subfit()`.

* After line `sub_fit <- refmodel$div_minimizer(`: Swap order of arguments `family` and `weights` (for consistency with the definitions of the divergence minimizers).

* Package **rngtools** is not necessary (use `.Random.seed` as is already done at similar places).

* Minor cleaning.

DESCRIPTION
@@ -32,7 +32,6 @@ Imports:
     ggplot2,
     Rcpp,
     utils,
-    rngtools (>= 1.2.4),
     magrittr,
     mgcv,
     gamm4,
@@ -48,7 +47,8 @@ Suggests:
     knitr,
     rmarkdown,
     glmnet,
-    bayesplot (>= 1.5.0)
+    bayesplot (>= 1.5.0),
+    optimx
 VignetteBuilder: knitr
 URL: https://mc-stan.org/projpred/, https://discourse.mc-stan.org
 BugReports: https://github.com/stan-dev/projpred/issues/

R/cv_varsel.R
@@ -162,7 +162,7 @@ cv_varsel.refmodel <- function(object, method = NULL, cv_method = NULL,
   candidate_terms <- split_formula(refmodel$formula,
                                    data = refmodel$fetch_data(),
                                    add_main_effects = FALSE)
-  candidate_terms <- unlist(tail(candidate_terms, -1))
+  candidate_terms <- unlist(utils::tail(candidate_terms, -1))
   solution_terms_cv_ch <- sapply(
     seq_len(NROW(sel_cv$solution_terms_cv)),
     function(i) {
@@ -233,7 +233,6 @@ parse_args_cv_varsel <- function(refmodel, cv_method = NULL, K = NULL,
                                  nclusters_pred = NULL) {
   if (is.null(cv_method)) {
     if (inherits(refmodel, "datafit")) {
-      ## only data given, no actual reference model
       cv_method <- "kfold"
     } else {
       cv_method <- "LOO"
@@ -260,8 +259,6 @@ parse_args_cv_varsel <- function(refmodel, cv_method = NULL, K = NULL,
   if (tolower(cv_method) == "kfold" && is.null(K)) {
     if (inherits(refmodel, "datafit")) {
       K <- 10
-      nclusters_pred <- 1
-      nclusters <- 1
     } else {
       K <- 5
     }
@@ -540,7 +537,7 @@ kfold_varsel <- function(refmodel, method, nterms_max, ndraws,
     )
   })
 
-  ## List of K elements, each containing d_train, p_pred, etc. corresponding
+  ## List of K elements, each containing d_test, p_pred, etc. corresponding
   ## to each fold.
   make_list_cv <- function(refmodel, d_test, msg) {
     if (!is.null(nclusters_pred) || !is.null(refmodel$nclusters_pred)) {
@@ -618,10 +615,6 @@ kfold_varsel <- function(refmodel, method, nterms_max, ndraws,
     close(pb)
   }
 
-  ## Helper function extract and combine mu and lppd from K lists with each
-  ## n/K of the elements to one list with n elements
-  hf <- function(x) as.list(do.call(rbind, x))
-
   ## Apply some magic to manipulate the structure of the list so that instead of
   ## list with K sub_summaries each containing n/K mu:s and lppd:s, we have only
   ## one sub_summary-list that contains with all n mu:s and lppd:s.
@@ -678,7 +671,7 @@ kfold_varsel <- function(refmodel, method, nterms_max, ndraws,
       # refmodel is datafit, cvfun will return an empty list and this will lead
       # to normal cross-validation for the submodels although we don't have an
       # actual reference model
-      if (verbose && !("datafit" %in% class(refmodel))) {
+      if (verbose && !inherits(refmodel, "datafit")) {
         print("Performing cross-validation for the reference model..")
       }
       nobs <- NROW(refmodel$y)
@@ -764,48 +757,48 @@ kfold_varsel <- function(refmodel, method, nterms_max, ndraws,
   return(nlist(refmodel = k_refmodel, omitted = cvfit$omitted))
 }
 
-.loo_subsample <- function(n, nloo, pareto_k, seed) {
-  ## decide which points to go through in the validation (i.e., which points
-  ## belong to the semi random subsample of validation points)
-
-  ## set random seed but ensure the old RNG state is restored on exit
-  if (exists(".Random.seed")) {
-    rng_state_old <- .Random.seed
-    on.exit(assign(".Random.seed", rng_state_old, envir = .GlobalEnv))
-  }
-  set.seed(seed)
-
-  resample <- function(x, ...) x[sample.int(length(x), ...)]
-
-  if (nloo < n) {
-    bad <- which(pareto_k > 0.7)
-    ok <- which(pareto_k <= 0.7 & pareto_k > 0.5)
-    good <- which(pareto_k <= 0.5)
-    inds <- resample(bad, min(length(bad), floor(nloo / 3)))
-    inds <- c(inds, resample(ok, min(length(ok), floor(nloo / 3))))
-    inds <- c(inds, resample(good, min(length(good), floor(nloo / 3))))
-    if (length(inds) < nloo) {
-      ## not enough points selected, so choose randomly among the rest
-      inds <- c(inds, resample(setdiff(seq_len(n), inds), nloo - length(inds)))
-    }
-
-    ## assign the weights corresponding to this stratification (for example, the
-    ## 'bad' values are likely to be overpresented in the sample)
-    w <- rep(0, n)
-    w[inds[inds %in% bad]] <- length(bad) / sum(inds %in% bad)
-    w[inds[inds %in% ok]] <- length(ok) / sum(inds %in% ok)
-    w[inds[inds %in% good]] <- length(good) / sum(inds %in% good)
-  } else {
-    ## all points used
-    inds <- seq_len(n)
-    w <- rep(1, n)
-  }
-
-  ## ensure weights are normalized
-  w <- w / sum(w)
-
-  return(nlist(inds, w))
-}
+# .loo_subsample <- function(n, nloo, pareto_k, seed) {
+#   ## decide which points to go through in the validation (i.e., which points
+#   ## belong to the semi random subsample of validation points)
+#
+#   ## set random seed but ensure the old RNG state is restored on exit
+#   if (exists(".Random.seed")) {
+#     rng_state_old <- .Random.seed
+#     on.exit(assign(".Random.seed", rng_state_old, envir = .GlobalEnv))
+#   }
+#   set.seed(seed)
+#
+#   resample <- function(x, ...) x[sample.int(length(x), ...)]
+#
+#   if (nloo < n) {
+#     bad <- which(pareto_k > 0.7)
+#     ok <- which(pareto_k <= 0.7 & pareto_k > 0.5)
+#     good <- which(pareto_k <= 0.5)
+#     inds <- resample(bad, min(length(bad), floor(nloo / 3)))
+#     inds <- c(inds, resample(ok, min(length(ok), floor(nloo / 3))))
+#     inds <- c(inds, resample(good, min(length(good), floor(nloo / 3))))
+#     if (length(inds) < nloo) {
+#       ## not enough points selected, so choose randomly among the rest
+#       inds <- c(inds, resample(setdiff(seq_len(n), inds), nloo - length(inds)))
+#     }
+#
+#     ## assign the weights corresponding to this stratification (for example, the
+#     ## 'bad' values are likely to be overpresented in the sample)
+#     w <- rep(0, n)
+#     w[inds[inds %in% bad]] <- length(bad) / sum(inds %in% bad)
+#     w[inds[inds %in% ok]] <- length(ok) / sum(inds %in% ok)
+#     w[inds[inds %in% good]] <- length(good) / sum(inds %in% good)
+#   } else {
+#     ## all points used
+#     inds <- seq_len(n)
+#     w <- rep(1, n)
+#   }
+#
+#   ## ensure weights are normalized
+#   w <- w / sum(w)
+#
+#   return(nlist(inds, w))
+# }
 
 .loo_subsample_pps <- function(nloo, lppd, seed) {
   ## decide which points to go through in the validation based on

R/divergence_minimizers.R
@@ -252,7 +252,6 @@ predict.subfit <- function(subfit, newdata = NULL, weights = NULL) {
   beta <- subfit$beta
   alpha <- subfit$alpha
   x <- subfit$x
-  w <- subfit$w
   if (is.null(newdata)) {
     if (is.null(beta)) {
       return(as.matrix(rep(alpha, NROW(subfit$x))))

R/formula.R
@@ -551,44 +551,43 @@ subset_formula_and_data <- function(formula, terms_, data, y = NULL,
   return(nlist(formula, data))
 }
 
-## Utility to just replace the response in the data frame
-## @param formula A formula for a valid model.
-## @param terms_ A vector of terms to subset.
-## @param split_formula If TRUE breaks the response down into single response
-##   formulas.
-## Default FALSE. It only works if `y` represents a multi-output response.
-## @return a function that replaces the response in the data with arguments
-## @param y The response vector. Default NULL.
-## @param data The original data frame for the full formula.
-get_replace_response <- function(formula, terms_, split_formula = FALSE) {
-  formula <- make_formula(terms_, formula = formula)
-  tt <- extract_terms_response(formula)
-  response_name <- tt$response
-
-  response_cols <- paste0(".", response_name)
-  replace_response <- function(y, data) {
-    response_ncol <- ncol(y) %||% 1
-    if (!is.null(ncol(y)) && ncol(y) > 1) {
-      response_cols <- paste0(response_cols, ".", seq_len(ncol(y)))
-      if (!split_formula) {
-        response_vector <- paste0("cbind(", paste(response_cols,
-                                                  collapse = ", "
-        ), ")")
-      }
-    }
-
-    ## don't overwrite original y name
-    if (all(response_cols %in% colnames(data))) {
-      data[, response_cols] <- y
-    } else {
-      data <- data.frame(.z = y, data)
-      colnames(data)[seq_len(response_ncol)] <- response_cols
-    }
-    return(data)
-  }
-  return(replace_response)
-}
-
+# ## Utility to just replace the response in the data frame
+# ## @param formula A formula for a valid model.
+# ## @param terms_ A vector of terms to subset.
+# ## @param split_formula If TRUE breaks the response down into single response
+# ##   formulas.
+# ## Default FALSE. It only works if `y` represents a multi-output response.
+# ## @return a function that replaces the response in the data with arguments
+# ## @param y The response vector. Default NULL.
+# ## @param data The original data frame for the full formula.
+# get_replace_response <- function(formula, terms_, split_formula = FALSE) {
+#   formula <- make_formula(terms_, formula = formula)
+#   tt <- extract_terms_response(formula)
+#   response_name <- tt$response
+#
+#   response_cols <- paste0(".", response_name)
+#   replace_response <- function(y, data) {
+#     response_ncol <- ncol(y) %||% 1
+#     if (!is.null(ncol(y)) && ncol(y) > 1) {
+#       response_cols <- paste0(response_cols, ".", seq_len(ncol(y)))
+#       if (!split_formula) {
+#         response_vector <- paste0("cbind(", paste(response_cols,
+#                                                   collapse = ", "
+#         ), ")")
+#       }
+#     }
+#
+#     ## don't overwrite original y name
+#     if (all(response_cols %in% colnames(data))) {
+#       data[, response_cols] <- y
+#     } else {
+#       data <- data.frame(.z = y, data)
+#       colnames(data)[seq_len(response_ncol)] <- response_cols
+#     }
+#     return(data)
+#   }
+#   return(replace_response)
+# }
 
 ## Subsets a formula by the given terms.
 ## @param terms_ A vector of terms to subset from the right hand side.

R/methods.R
@@ -257,8 +257,8 @@ proj_predict <- function(object, newdata = NULL,
                          filter_nterms = NULL,
                          nresample_clusters = 1000, .seed = NULL, ...) {
   ## set random seed but ensure the old RNG state is restored on exit
-  rng_state_old <- rngtools::RNGseed()
-  on.exit(rngtools::RNGseed(rng_state_old))
+  rng_state_old <- .Random.seed
+  on.exit(assign(".Random.seed", rng_state_old, envir = .GlobalEnv))
   set.seed(.seed)
 
   ## proj_helper lapplies fun to each projection in object
@@ -596,9 +596,8 @@ print.vselsummary <- function(x, digits = 1, ...) {
   if (!is.null(x$cv_method)) {
     cat(paste("CV method:", x$cv_method, x$search_included, "\n"))
   }
-  nterms_max <- max(x$selection$size)
   cat(paste0("Search method: ", x$method, ", maximum number of terms ",
-             nterms_max, "\n"))
+             max(x$selection$size), "\n"))
   if (!is.null(x$nclusters)) {
     cat(paste0(
       "Number of clusters used for selection: ", x$nclusters, "\n"
@@ -620,11 +619,12 @@ print.vselsummary <- function(x, digits = 1, ...) {
   cat(paste0("Suggested Projection Size: ", x$suggested_size, "\n"))
   cat("\n")
   cat("Selection Summary:\n")
-  print(x$selection %>% dplyr::mutate(dplyr::across(
-    where(is.numeric),
-    ~ round(., digits)
-  )),
-  row.names = FALSE
+  print(
+    x$selection %>% dplyr::mutate(dplyr::across(
+      where(is.numeric),
+      ~ round(., digits)
+    )),
+    row.names = FALSE
   )
   return(invisible(x))
 }

R/misc.R
@@ -157,11 +157,10 @@ bootstrap <- function(x, fun = mean, b = 1000, oobfun = NULL, seed = NULL,
 
 .get_standard_y <- function(y, weights, fam) {
   # Return `y` and the corresponding observation weights in the "standard" form:
-  # For the binomial family, `y` is transformed into a vector with values
-  # between 0 and 1 (i.e., proportions), and `weights` gives the number of
-  # trials for each observation. For all other families, `y` and `weights` are
-  # kept as they are (unless `weights` has length zero in which case it is
-  # replaced by a vector of ones).
+  # For the binomial family, `y` is transformed into a vector with values in the
+  # set {0, 1}, and `weights` gives the number of trials for each observation.
+  # For all other families, `y` and `weights` are kept as they are (unless
+  # `weights` has length zero in which case it is replaced by a vector of ones).
   if (NCOL(y) == 1) {
     if (length(weights) > 0) {
       weights <- unname(weights)
@@ -474,3 +473,6 @@ get_as.matrix_cls_projpred <- function() {
   return(as.matrix_cls_projpred)
 }
 
+## Helper function extract and combine mu and lppd from K lists with each
+## n/K of the elements to one list with n elements
+hf <- function(x) as.list(do.call(rbind, x))

R/project.R
@@ -108,20 +108,20 @@ NULL
 project <- function(object, nterms = NULL, solution_terms = NULL,
                     cv_search = TRUE, ndraws = 400, nclusters = NULL,
                     seed = NULL, regul = 1e-4, ...) {
-  if (!("vsel" %in% class(object)) && is.null(solution_terms)) {
+  if (!inherits(object, "vsel") && is.null(solution_terms)) {
     stop("The given object is not an object of class \"vsel\". Run the ",
          "variable selection first, or provide argument `solution_terms`.")
   }
-  if (!("vsel" %in% class(object)) && !cv_search) {
+  if (!inherits(object, "vsel") && !cv_search) {
     stop("The given object is not an object of class \"vsel\". Run the ",
          "variable selection first, or provide argument `cv_search = TRUE`.")
   }
 
   refmodel <- get_refmodel(object, ...)
 
-  if (cv_search) {
+  if (inherits(refmodel, "datafit")) {
     ## use non-cv_searched solution for datafits
-    cv_search <- !inherits(refmodel, "datafit")
+    cv_search <- FALSE
   }
 
   if (!is.null(solution_terms) &&

R/projfun.R
@@ -15,8 +15,8 @@ project_submodel <- function(solution_terms, p_ref, refmodel, family, intercept,
   sub_fit <- refmodel$div_minimizer(
     formula = flatten_formula(subset$formula),
     data = subset$data,
-    weights = refmodel$wobs,
     family = family,
+    weights = refmodel$wobs,
     regul = regul,
     var = p_ref$var
   )

R/refmodel.R
@@ -201,7 +201,7 @@ predict.refmodel <- function(object, newdata, ynew = NULL, offsetnew = NULL,
   if (!(type %in% c("response", "link"))) {
     stop("type should be one of ('response', 'link')")
   }
-  if ("datafit" %in% class(object)) {
+  if (inherits(object, "datafit")) {
     stop("Cannot make predictions with data reference only.")
   }
   if (!is.null(ynew)) {
@@ -244,7 +244,7 @@ predict.refmodel <- function(object, newdata, ynew = NULL, offsetnew = NULL,
   } else {
 
     ## evaluate the log predictive density at the given ynew values
-    loglik <- object$fam$ll_fun(
+    loglik <- object$family$ll_fun(
       object$family$linkinv(mu), object$dis, ynew,
       weightsnew
     )
@@ -536,7 +536,7 @@ init_refmodel <- function(object, data, formula, family, ref_predfun = NULL,
     mu <- family$linkinv(mu)
   } else {
     mu <- matrix(y / weights, NROW(y), 1)
-    ref_predfun_datafit <- function(fit = NULL, newdata = NULL, offset = 0) {
+    ref_predfun_datafit <- function(fit = NULL, newdata = NULL) {
       stopifnot(is.null(fit))
       if (is.null(newdata)) {
         matrix(rep(NA, NROW(y)))

R/search.R
@@ -1,8 +1,7 @@
 search_forward <- function(p_ref, refmodel, family, intercept, nterms_max,
                            verbose = TRUE, opt, search_terms = NULL,
                            increasing_order = TRUE) {
-  ## initialize the forward selection
-  ## proj performs the projection over draws
+  # projfun() performs the projection:
   projfun <- .get_proj_handle(refmodel, p_ref, family, opt$regul, intercept)
 
   formula <- refmodel$formula

R/summary_funs.R
@@ -2,10 +2,6 @@
                                search_terms = NULL) {
   has_group_features <- !is.null(search_terms)
   lapply(submodels, function(model) {
-    solution_terms <- model$solution_terms
-    if (length(solution_terms) == 0) {
-      solution_terms <- c("1")
-    }
     sub_fit <- model$sub_fit
     weights <- refmodel$wobs[test_points]
     mu <- family$mu_fun(sub_fit,

R/varsel.R
@@ -120,7 +120,7 @@ varsel.refmodel <- function(object, d_test = NULL, method = NULL,
   ## predictive statistics of the reference model on test data. if no test data
   ## are provided,
   ## simply fetch the statistics on the train data
-  if ("datafit" %in% class(refmodel)) {
+  if (inherits(refmodel, "datafit")) {
     ## no actual reference model, so we don't know how to predict test
     ## observations
     ntest <- NROW(refmodel$y)
@@ -161,9 +161,7 @@ varsel.refmodel <- function(object, d_test = NULL, method = NULL,
   )
   ## suggest model size
   class(vs) <- "vsel"
-  vs$suggested_size <- suggest_size(vs,
-                                    warnings = FALSE
-  )
+  vs$suggested_size <- suggest_size(vs, warnings = FALSE)
   summary <- summary(vs)
   vs$summary <- summary$selection
 