stan-dev/projpred

commit 107132da4d0d4ef99f1380fa1d6541fcd60c44ca

Fix issue #185. (#193)

R/refmodel.R
@@ -535,7 +535,12 @@ init_refmodel <- function(object, data, formula, family, ref_predfun = NULL,
     mu <- unname(as.matrix(mu))
     mu <- family$linkinv(mu)
   } else {
-    mu <- matrix(y / weights, NROW(y), 1)
+    if (family$family != "binomial") {
+      mu <- y
+    } else {
+      mu <- y / weights
+    }
+    mu <- matrix(mu)
     ref_predfun_datafit <- function(fit = NULL, newdata = NULL) {
       stopifnot(is.null(fit))
       if (is.null(newdata)) {

commit 894933c805745ea547aa269c5917d4a675380b80

Fix a bug introduced by PR #193 when calculating `mu` for `"datafit"`s with a binomial family and a two-column response. Also fix another (potential) bug (in `mu <- y / weights`, `weights` could have length zero).

R/refmodel.R
@@ -483,6 +483,10 @@ init_refmodel <- function(object, data, formula, family, ref_predfun = NULL,
 
   data[, response_name] <- y
 
+  target <- .get_standard_y(y, weights, family)
+  y <- target$y
+  weights <- target$weights
+
   if (is.null(div_minimizer)) {
     if (length(terms$additive_terms) != 0) {
       div_minimizer <- additive_mle
@@ -559,13 +563,6 @@ init_refmodel <- function(object, data, formula, family, ref_predfun = NULL,
   if (is.null(offset)) {
     offset <- rep(0, NROW(y))
   }
-
-  if (is.null(weights)) {
-    weights <- rep(1, NROW(y))
-  }
-
-  target <- .get_standard_y(y, weights, family)
-  y <- target$y
 
   if (proper_model) {
     loglik <- t(family$ll_fun(mu, dis, y, weights = weights))