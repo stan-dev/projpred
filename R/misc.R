stan-dev/projpred

commit 107132da4d0d4ef99f1380fa1d6541fcd60c44ca

Fix issue #185. (#193)

R/refmodel.R
@@ -535,7 +535,12 @@ init_refmodel <- function(object, data, formula, family, ref_predfun = NULL,
     mu <- unname(as.matrix(mu))
     mu <- family$linkinv(mu)
   } else {
-    mu <- matrix(y / weights, NROW(y), 1)
+    if (family$family != "binomial") {
+      mu <- y
+    } else {
+      mu <- y / weights
+    }
+    mu <- matrix(mu)
     ref_predfun_datafit <- function(fit = NULL, newdata = NULL) {
       stopifnot(is.null(fit))
       if (is.null(newdata)) {

commit 0ad5c943789c2952e75e7994e31adce161b51515

Fix `"datafit"`s with binomial family and two-column response (#194)
* Improve a comment concerning `.get_standard_y()`.

* `.get_standard_y()`: Add an error for a two-column response and a non-binomial family. Improve an existing error message.

* Fix a bug introduced by PR #193 when calculating `mu` for `"datafit"`s with a binomial family and a two-column response. Also fix another (potential) bug (in `mu <- y / weights`, `weights` could have length zero).

* Fix a bug in `.get_standard_y()` (the second column of a two-column response is the number of failures, not the number of trials).

* Use `unname()` in `.get_standard_y()` consistently.

* Use `+` instead of `rowSums()` to avoid converting integer to numeric.

R/misc.R
@@ -155,12 +155,14 @@ bootstrap <- function(x, fun = mean, b = 1000, oobfun = NULL, seed = NULL,
   return(baseline)
 }
 
+# A function for retrieving `y` and the corresponding observation weights
+# `weights` in their "standard" forms:
+#   * If `NCOL(y) == 2`: `y` is the first column and `weights` the second.
+#   * If `NCOL(y) == 1`: `weights` is basically unchanged (unless of length zero
+#     in which case it is replaced by a vector of ones). For a binomial family,
+#     if `is.factor(y)`, `y` is transformed into a zero-one vector (i.e., with
+#     values in the set {0, 1}).
 .get_standard_y <- function(y, weights, fam) {
-  # Return `y` and the corresponding observation weights in the "standard" form:
-  # For the binomial family, `y` is transformed into a vector with values in the
-  # set {0, 1}, and `weights` gives the number of trials for each observation.
-  # For all other families, `y` and `weights` are kept as they are (unless
-  # `weights` has length zero in which case it is replaced by a vector of ones).
   if (NCOL(y) == 1) {
     if (length(weights) > 0) {
       weights <- unname(weights)
@@ -180,10 +182,13 @@ bootstrap <- function(x, fun = mean, b = 1000, oobfun = NULL, seed = NULL,
       }
     }
   } else if (NCOL(y) == 2) {
-    weights <- y[, 2]
-    y <- y[, 1]
+    if (fam$family != "binomial") {
+      stop("For non-binomial families, a two-column response is not allowed.")
+    }
+    weights <- unname(y[, 1] + y[, 2])
+    y <- unname(y[, 1])
   } else {
-    stop("y cannot have more than two columns.")
+    stop("The response is not allowed to have more than two columns.")
   }
   return(nlist(y, weights))
 }

R/refmodel.R
@@ -483,6 +483,10 @@ init_refmodel <- function(object, data, formula, family, ref_predfun = NULL,
 
   data[, response_name] <- y
 
+  target <- .get_standard_y(y, weights, family)
+  y <- target$y
+  weights <- target$weights
+
   if (is.null(div_minimizer)) {
     if (length(terms$additive_terms) != 0) {
       div_minimizer <- additive_mle
@@ -559,13 +563,6 @@ init_refmodel <- function(object, data, formula, family, ref_predfun = NULL,
   if (is.null(offset)) {
     offset <- rep(0, NROW(y))
   }
-
-  if (is.null(weights)) {
-    weights <- rep(1, NROW(y))
-  }
-
-  target <- .get_standard_y(y, weights, family)
-  y <- target$y
 
   if (proper_model) {
     loglik <- t(family$ll_fun(mu, dis, y, weights = weights))