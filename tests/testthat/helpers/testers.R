7a9ba11042bac054eb8eca8ffdd7e16135a9f63d
fweber144 <fweber144@protonmail.com>
2021-09-07 14:54:46 +0200

    `extfam_tester()`: Add a `TODO`.

--- tests/testthat/helpers/testers.R
+++ tests/testthat/helpers/testers.R
@@ -56,6 +56,9 @@ extfam_tester <- function(extfam,
     expect_type(extfam[[el_nm]], "closure")
   }
 
+  # TODO: Add some mathematical checks (i.e., check that the calculations for
+  # the objects listed in `extfam_nms_add` are mathematically correct).
+
   return(invisible(TRUE))
 }
 
