4a1acf06d295eec3789e3dabe25b189384beb481
fweber144 <fweber144@protonmail.com>
2021-09-15 16:36:02 +0200

    Enhance documentation (continued; `man-roxygen/args-newdata.R`).

--- man-roxygen/args-newdata.R
+++ man-roxygen/args-newdata.R
@@ -1,9 +1,10 @@
 #' @param newdata Passed to argument `newdata` of the reference model's
 #'   `extract_model_data` function (see [init_refmodel()]). Provides the
-#'   predictor (and possibly also the response) data for the new observations.
+#'   predictor (and possibly also the response) data for the new (or old)
+#'   observations.
 #' @param offsetnew Passed to argument `orhs` of the reference model's
 #'   `extract_model_data` function (see [init_refmodel()]). Used to get the
-#'   offsets for the (new) observations.
+#'   offsets for the new (or old) observations.
 #' @param weightsnew Passed to argument `wrhs` of the reference model's
 #'   `extract_model_data` function (see [init_refmodel()]). Used to get the
-#'   weights for the (new) observations.
+#'   weights for the new (or old) observations.
