d251df0dce6fef3e68d27616d657a4dbccedea37
fweber144 <fweber144@protonmail.com>
2021-11-26 12:12:46 +0100

    Comment out `validate_resp_formula()` (unused).

--- R/formula.R
+++ R/formula.R
@@ -789,30 +789,30 @@ lhs <- function(x) {
   if (length(x) == 3L) update(x, . ~ 1) else NULL
 }
 
-# taken from brms
-## validate formulas dedicated to response variables
-## @param x coerced to a formula object
-## @param empty_ok is an empty left-hand-side ok?
-## @return a formula of the form <response> ~ 1
-validate_resp_formula <- function(x, empty_ok = TRUE) {
-  out <- lhs(as.formula(x))
-  if (is.null(out)) {
-    if (empty_ok) {
-      out <- ~ 1
-    } else {
-      str_x <- formula2str(x, space = "trim")
-      stop2("Response variable is missing in formula ", str_x)
-    }
-  }
-  out <- gsub("\\|+[^~]*~", "~", formula2str(out))
-  out <- try(formula(out), silent = TRUE)
-  if (is(out, "try-error")) {
-    str_x <- formula2str(x, space = "trim")
-    stop2("Incorrect use of '|' on the left-hand side of ", str_x)
-  }
-  environment(out) <- environment(x)
-  out
-}
+# # taken from brms
+# ## validate formulas dedicated to response variables
+# ## @param x coerced to a formula object
+# ## @param empty_ok is an empty left-hand-side ok?
+# ## @return a formula of the form <response> ~ 1
+# validate_resp_formula <- function(x, empty_ok = TRUE) {
+#   out <- lhs(as.formula(x))
+#   if (is.null(out)) {
+#     if (empty_ok) {
+#       out <- ~ 1
+#     } else {
+#       str_x <- formula2str(x, space = "trim")
+#       stop2("Response variable is missing in formula ", str_x)
+#     }
+#   }
+#   out <- gsub("\\|+[^~]*~", "~", formula2str(out))
+#   out <- try(formula(out), silent = TRUE)
+#   if (is(out, "try-error")) {
+#     str_x <- formula2str(x, space = "trim")
+#     stop2("Incorrect use of '|' on the left-hand side of ", str_x)
+#   }
+#   environment(out) <- environment(x)
+#   out
+# }
 
 # taken from brms
 ## convert a formula to a character string
