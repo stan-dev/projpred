e2dc88cc50336799d925b66944f924c9cebdd931
fweber144 <fweber144@protonmail.com>
2022-02-01 15:52:08 +0100

    Improve/add comments for the test helpers from `getters.R`.

--- tests/testthat/helpers/getters.R
+++ tests/testthat/helpers/getters.R
@@ -1,3 +1,4 @@
+# A function to retrieve the formula from a fit (`fit_obj`):
 get_formul_from_fit <- function(fit_obj) {
   formul_out <- formula(fit_obj)
   if (inherits(fit_obj, "brmsfit")) {
@@ -6,6 +7,8 @@ get_formul_from_fit <- function(fit_obj) {
   return(formul_out)
 }
 
+# A function to adapt a given dataset (`dat`) appropriately to a given formula
+# (`formul_crr`):
 get_dat_formul <- function(formul_crr, needs_adj, dat_crr = dat) {
   if (needs_adj) {
     formul_crr <- rm_cbind(formul_crr)
@@ -17,14 +20,15 @@ get_dat_formul <- function(formul_crr, needs_adj, dat_crr = dat) {
   return(dat_crr)
 }
 
+# A function to adapt a given dataset (`dat`) appropriately to a given "test
+# setup" (`tstsetup`):
 get_dat <- function(tstsetup, dat_crr = dat) {
   get_dat_formul(args_fit[[args_prj[[tstsetup]]$tstsetup_fit]]$formula,
                  needs_adj = grepl("\\.spclformul", tstsetup),
                  dat_crr = dat_crr)
 }
 
-# A function to get the possible length of the vector supplied to argument
-# `penalty`:
+# A function to get the elements which may be supplied to argument `penalty`:
 get_penal_possbl <- function(formul_crr) {
   trms_crr <- labels(terms(formul_crr))
   if (length(trms_crr) > 0 && any(grepl("xca\\.", trms_crr))) {
