237b3f63a22cef148cc42e8b7352c3f1524ea8e1
fweber144 <fweber144@protonmail.com>
2022-02-03 14:33:29 +0100

    The `fit_latent()` function is not necessary (according to my understanding, but
    I might be wrong). For `extract_eta()`, I'm not sure (concerning future use), so
    I'm leaving this commented.

--- R/latent.R
+++ R/latent.R
@@ -3,52 +3,7 @@
 # Perform latent projection predictive variable selection for generalized
 # linear models.
 
-# utility function to perform kfolds validation over posterior samples
-#' @export
-.latent_cvfun <- function(folds, ...) {
-  cvres <- brms::kfold(
-    object,
-    K = max(folds),
-    save_fits = TRUE,
-    folds = folds,
-    ...
-  )
-  fits <- cvres$fits[, "fit"]
-  return(fits)
-}
-
-#' @export
-.extract_latent_model_data <- function(object,
-                               newdata = NULL,
-                               wrhs = NULL,
-                               orhs = NULL,
-                               extract_y = TRUE) {
-  if (!extract_y) {
-    resp_form <- NULL
-  } else {
-    resp_form <- ~ .y
-  }
-
-  if (is.null(newdata)) {
-    newdata <- data
-  }
-
-  if (is.null(wrhs) && !is.null(object) &&
-      !is.null(object$weights) && length(object$weights) != 0) {
-    wrhs <- ~ weights
-    newdata <- cbind(newdata, weights = object$weights)
-  }
-
-  if (is.null(orhs) && !is.null(object) &&
-      !is.null(object$offset) && length(object$offset) != 0) {
-    orhs <- ~ offset
-    newdata <- cbind(newdata, offset = object$offset)
-  }
-
-  args <- nlist(object, newdata, wrhs, orhs, resp_form)
-  return(do_call(projpred:::.extract_model_data, args))
-}
-
+# TODO: I guess the documentation is not up-to-date anymore.
 #' Extract the posterior draws of the latent predictor
 #'
 #' @param fit A reference model written
@@ -99,60 +54,3 @@ extract_eta <- function(fit, data) {
   )
   return(eta_post_draws)
 }
-
-#' Fit the latent reference model
-#'
-#' This function takes in a reference model, the posterior draws of its latent
-#' predictor, and the data on which the reference model was trained to fit a
-#' reference model in terms of the latent predictor using a Gaussianity
-#' assumption.
-#'
-#' @param fit A reference model written
-#' @param eta_post_draws The posterior predictive draws of the latent predictor from
-#' the reference model
-#' @param data The data on which the reference model was trained
-#' @param latent_div_minimizer The divergence minimising formula to be used
-#' on the latent predictor
-#' @param latent_proj_predfun The projection predictive function
-#' @param dis_latent An array of the dispertion parameter for the latent predictor
-#' @param extract_model_data A function to extract data from the model
-#' @param ref_predfun The predictive function of the reference model
-#' @param cv_fun A function for cross-validation
-#' @return A latent predictor reference model
-#' @export
-fit_latent <-
-  function(fit,
-           eta_post_draws,
-           data,
-           latent_family = brms::brmsfamily("gaussian"),
-           latent_div_minimizer = divmin,
-           latent_proj_predfun = subprd,
-           dis_latent = rep(1, 4000), # fixed and uniform
-           extract_model_data = .extract_latent_model_data,
-           ref_predfun = refprd,
-           cv_fun = .latent_cvfun) {
-    # update the reference model's formula to fit to the latent predictor
-    latent_formula <- formula(update(fit$formula, ".y ~ ."))
-    # add latent predictor to input data
-    data[[".y"]] <- rowMeans(eta_post_draws)
-    # define new reference model over latent parameter space with Gaussian family
-    latent_ref <- init_refmodel(
-      fit,
-      data = data,
-      formula = latent_formula,
-      family = latent_family,
-      ref_predfun = ref_predfun,
-      div_minimizer = latent_div_minimizer,
-      proj_predfun = latent_proj_predfun,
-      dis = dis_latent,
-      extract_model_data = extract_model_data,
-      cvfun = cv_fun
-    )
-    message(
-      paste(
-        "To proceed to variable selection in the latent space, kindly pass",
-        "the returned `latent_ref` object to the `varsel` or `cv_varsel` method."
-      )
-    )
-    return(latent_ref)
-  }
