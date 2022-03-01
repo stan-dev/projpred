019085da97f1220aaafc09480098dd243efab937
fweber144 <fweber144@protonmail.com>
2022-03-01 10:39:50 +0100

    `.get_refdist()`: Set the seed where it is used, i.e., in
    `.get_p_clust()` and in a new function for subsampling the parameter draws
    (`draws_subsample()`). This is only done for conceptual reasons (to point out
    where the seed is actually used).

--- R/misc.R
+++ R/misc.R
@@ -201,15 +201,7 @@ bootstrap <- function(x, fun = mean, b = 2000,
 #   has length equal to the number of posterior draws and each value is an
 #   integer between 1 and \eqn{S_{\mbox{prj}}}{S_prj}.
 .get_refdist <- function(refmodel, ndraws = NULL, nclusters = NULL,
-                         seed = sample.int(.Machine$integer.max, 1),
-                         thinning = TRUE) {
-  # Set seed, but ensure the old RNG state is restored on exit:
-  if (exists(".Random.seed", envir = .GlobalEnv)) {
-    rng_state_old <- get(".Random.seed", envir = .GlobalEnv)
-    on.exit(assign(".Random.seed", rng_state_old, envir = .GlobalEnv))
-  }
-  set.seed(seed)
-
+                         thinning = TRUE, ...) {
   S <- NCOL(refmodel$mu) # number of draws in the reference model
   if (is.null(ndraws)) {
     ndraws <- S
@@ -221,10 +213,10 @@ bootstrap <- function(x, fun = mean, b = 2000,
       # special case, only one cluster
       cl <- rep(1, S)
       p_ref <- .get_p_clust(refmodel$family, refmodel$mu, refmodel$dis,
-                            wobs = refmodel$wobs, cl = cl)
+                            wobs = refmodel$wobs, cl = cl, ...)
     } else if (nclusters == NCOL(refmodel$mu)) {
       # number of clusters equal to the number of samples, so return the samples
-      return(.get_refdist(refmodel, ndraws = nclusters))
+      return(.get_refdist(refmodel, ndraws = nclusters, ...))
     } else {
       # several clusters
       if (nclusters > NCOL(refmodel$mu)) {
@@ -232,7 +224,7 @@ bootstrap <- function(x, fun = mean, b = 2000,
              "columns in mu.")
       }
       p_ref <- .get_p_clust(refmodel$family, refmodel$mu, refmodel$dis,
-                            wobs = refmodel$wobs, nclusters = nclusters)
+                            wobs = refmodel$wobs, nclusters = nclusters, ...)
     }
   } else {
     if (ndraws > NCOL(refmodel$mu)) {
@@ -242,7 +234,7 @@ bootstrap <- function(x, fun = mean, b = 2000,
     if (thinning) {
       s_ind <- round(seq(from = 1, to = S, length.out = ndraws))
     } else {
-      s_ind <- sample(seq_len(S), size = ndraws)
+      s_ind <- draws_subsample(S = S, ndraws = ndraws, ...)
     }
     cl <- rep(NA, S)
     cl[s_ind] <- c(1:ndraws)
@@ -258,13 +250,20 @@ bootstrap <- function(x, fun = mean, b = 2000,
   return(p_ref)
 }
 
+# Function for clustering the parameter draws:
 .get_p_clust <- function(family, mu, dis, nclusters = 10,
                          wobs = rep(1, dim(mu)[1]),
-                         wsample = rep(1, dim(mu)[2]), cl = NULL) {
-  # Function for perfoming the clustering over the samples.
-  #
+                         wsample = rep(1, dim(mu)[2]), cl = NULL,
+                         seed = sample.int(.Machine$integer.max, 1)) {
   # cluster the samples in the latent space if no clustering provided
   if (is.null(cl)) {
+    # Set seed, but ensure the old RNG state is restored on exit:
+    if (exists(".Random.seed", envir = .GlobalEnv)) {
+      rng_state_old <- get(".Random.seed", envir = .GlobalEnv)
+      on.exit(assign(".Random.seed", rng_state_old, envir = .GlobalEnv))
+    }
+    set.seed(seed)
+
     f <- family$linkfun(mu)
     out <- kmeans(t(f), nclusters, iter.max = 50)
     cl <- out$cluster # cluster indices for each sample
@@ -313,6 +312,18 @@ bootstrap <- function(x, fun = mean, b = 2000,
     cl = cl
   )
   return(p)
+}
+
+draws_subsample <- function(S, ndraws,
+                            seed = sample.int(.Machine$integer.max, 1)) {
+  # Set seed, but ensure the old RNG state is restored on exit:
+  if (exists(".Random.seed", envir = .GlobalEnv)) {
+    rng_state_old <- get(".Random.seed", envir = .GlobalEnv)
+    on.exit(assign(".Random.seed", rng_state_old, envir = .GlobalEnv))
+  }
+  set.seed(seed)
+
+  return(sample(seq_len(S), size = ndraws))
 }
 
 .is_proj_list <- function(proj) {
