e4d879aab92e1f96fafab69d562b8629b62e4949
fweber144 <fweber144@protonmail.com>
2022-08-16 11:07:12 +0200

    Sort all references (we only have author-year style) alphabetically.

--- R/projpred-package.R
+++ R/projpred-package.R
@@ -86,13 +86,17 @@
 #'
 #' @references
 #'
-#' Goutis, C. and Robert, C. P. (1998). Model choice in generalised linear
-#' models: A Bayesian approach via Kullback–Leibler projections. *Biometrika*,
-#' **85**(1):29–37.
+#' Catalina, A., Bürkner, P.-C., and Vehtari, A. (2020). Projection predictive
+#' inference for generalized linear and additive multilevel models.
+#' *arXiv:2010.06994*. URL: <https://arxiv.org/abs/2010.06994>.
 #'
 #' Dupuis, J. A. and Robert, C. P. (2003). Variable selection in qualitative
 #' models via an entropic explanatory power. *Journal of Statistical Planning
 #' and Inference*, **111**(1-2):77–94. \doi{10.1016/S0378-3758(02)00286-0}.
+#'
+#' Goutis, C. and Robert, C. P. (1998). Model choice in generalised linear
+#' models: A Bayesian approach via Kullback–Leibler projections. *Biometrika*,
+#' **85**(1):29–37.
 #'
 #' Piironen, J. and Vehtari, A. (2017). Comparison of Bayesian predictive
 #' methods for model selection. *Statistics and Computing*, **27**(3):711-735.
@@ -101,9 +105,5 @@
 #' Piironen, J., Paasiniemi, M., and Vehtari, A. (2020). Projective inference in
 #' high-dimensional problems: Prediction and feature selection. *Electronic
 #' Journal of Statistics*, **14**(1):2155-2197. \doi{10.1214/20-EJS1711}.
-#'
-#' Catalina, A., Bürkner, P.-C., and Vehtari, A. (2020). Projection predictive
-#' inference for generalized linear and additive multilevel models.
-#' *arXiv:2010.06994*. URL: <https://arxiv.org/abs/2010.06994>.
 #'
 "_PACKAGE"
