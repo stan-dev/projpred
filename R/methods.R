bf0a391ee17952970e6c9c813716ac725c404d98
fweber144 <fweber144@protonmail.com>
2022-08-16 21:20:21 +0200

    Avoid a redundant `.validate_vsel_object_stats()` call in `suggest_size.vsel()`.

--- R/methods.R
+++ R/methods.R
@@ -829,10 +829,16 @@ suggest_size.vsel <- function(
     warnings = TRUE,
     ...
 ) {
-  .validate_vsel_object_stats(object, stat)
   if (length(stat) > 1) {
     stop("Only one statistic can be specified to suggest_size")
   }
+  stats <- summary.vsel(object,
+                        stats = stat,
+                        type = c("mean", "upper", "lower"),
+                        deltas = TRUE,
+                        ...)
+  nobs_test <- stats$nobs_test %||% stats$nobs_train
+  stats <- stats$selection
 
   if (.is_util(stat)) {
     sgn <- 1
@@ -850,13 +856,7 @@ suggest_size.vsel <- function(
     suffix <- ""
   }
   bound <- type
-  stats <- summary.vsel(object,
-                        stats = stat,
-                        type = c("mean", "upper", "lower"),
-                        deltas = TRUE,
-                        ...)
-  nobs_test <- stats$nobs_test %||% stats$nobs_train
-  stats <- stats$selection
+
   util_null <- sgn * unlist(unname(subset(
     stats, stats$size == 0,
     paste0(stat, suffix)
