5ae3392f80e15c6a2e53c6717ba47a0e7b352b55
fweber144 <fweber144@protonmail.com>
2022-08-17 17:19:51 +0200

    Split up the `extend_family()` test into two separate tests.

--- tests/testthat/test_extend_family.R
+++ tests/testthat/test_extend_family.R
@@ -1,15 +1,26 @@
 context("extend_family()")
 
-test_that("`family` works", {
-  fam_nms_tst <- c("gauss", "binom", "poiss", "binom_augdat",
-                   "cumul_augdat_rstanarm")
+test_that("extend_family() works for the traditional projection", {
+  fam_nms_tst <- c("gauss", "binom", "poiss")
+  ### TODO: Also test other families extensible by projpred:
+  # fam_nms_tst <- c(fam_nms_tst, "Gamma", "Student_t")
+  ###
+  fam_nms_tst <- setNames(nm = fam_nms_tst)
+  for (fam_nm in fam_nms_tst) {
+    fam_orig_crr <- get(paste0("f_", fam_nm))
+    extfam_crr <- extend_family(fam_orig_crr)
+    extfam_tester(extfam = extfam_crr,
+                  fam_orig = fam_orig_crr,
+                  info_str = fam_nm)
+  }
+})
+
+test_that("extend_family() works for the augmented-data projection", {
+  fam_nms_tst <- c("binom_augdat", "cumul_augdat_rstanarm")
   # For families brms::categorical(), brms::cumulative(), brms::cratio(),
   # brms::sratio(), and brms::acat(), extend_family() can't be tested easily
   # without receiving such a family from a `brmsfit`, so skip them here (they
   # are tested by refmodel_tester() in `test_refmodel.R` anyway).
-  ### TODO: Also test other families extensible by projpred:
-  # fam_nms_tst <- c(fam_nms_tst, "Gamma", "Student_t")
-  ###
   fam_nms_tst <- setNames(nm = fam_nms_tst)
   for (fam_nm in fam_nms_tst) {
     if (fam_nm == "binom_augdat") {
@@ -18,7 +29,6 @@ test_that("`family` works", {
                                   augdat_y_unqs = c("0", "1"),
                                   augdat_link = augdat_link_binom,
                                   augdat_ilink = augdat_ilink_binom)
-      augdat_expected_crr <- TRUE
     } else if (fam_nm == "cumul_augdat_rstanarm") {
       fam_orig_crr <- structure(list(family = "cumulative_rstanarm",
                                      link = "logit"),
@@ -29,15 +39,10 @@ test_that("`family` works", {
                                   augdat_ilink = augdat_ilink_cumul,
                                   augdat_args_link = list(link = "logit"),
                                   augdat_args_ilink = list(link = "logit"))
-      augdat_expected_crr <- TRUE
-    } else {
-      fam_orig_crr <- get(paste0("f_", fam_nm))
-      extfam_crr <- extend_family(fam_orig_crr)
-      augdat_expected_crr <- FALSE
     }
     extfam_tester(extfam = extfam_crr,
                   fam_orig = fam_orig_crr,
-                  augdat_expected = augdat_expected_crr,
+                  augdat_expected = TRUE,
                   info_str = fam_nm)
   }
 })
