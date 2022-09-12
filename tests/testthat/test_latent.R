49aab8ae864cf0a26d3e759cce5717a933edb77c
fweber144 <fweber144@protonmail.com>
2022-09-12 16:41:17 +0200

    Add test "for the gaussian() family, the latent projection is the
    same as the traditional projection (when setting `dis` appropriately)".

--- tests/testthat/test_latent.R
+++ tests/testthat/test_latent.R
@@ -142,6 +142,74 @@ test_that(paste(
   if (exists("rng_old")) assign(".Random.seed", rng_old, envir = .GlobalEnv)
 })
 
+## Gaussian family --------------------------------------------------------
+
+test_that(paste(
+  "for the gaussian() family, the latent projection is the same as the",
+  "traditional projection (when setting `dis` appropriately)"
+), {
+  skip_if_not(run_prj)
+  tstsetups <- grep("\\.gauss\\.", names(prjs), value = TRUE)
+  if (any(grepl("^brms\\.|\\.without_wobs\\.", tstsetups))) {
+    message("The test \"for the gaussian() family, the latent projection is ",
+            "the same as the traditional projection (when setting `dis` ",
+            "appropriately)\" could be simplified.")
+  }
+  fits_no_wobs <- NULL
+  for (tstsetup in tstsetups) {
+    args_prj_i <- args_prj[[tstsetup]]
+    ndr_ncl <- ndr_ncl_dtls(args_prj[[tstsetup]])
+    if (!args_prj_i$tstsetup_fit %in% names(fits_no_wobs)) {
+      args_fit_i <- args_fit[[args_prj_i$tstsetup_fit]]
+      args_fit_i$weights <- NULL
+      fit_fun_crr <- switch(args_fit_i$mod_nm,
+                            "glm" = "stan_glm",
+                            "glmm" = "stan_glmer",
+                            "stan_gamm4")
+      fits_no_wobs[[args_prj_i$tstsetup_fit]] <- suppressWarnings(
+        do.call(get(fit_fun_crr, asNamespace(args_fit_i$pkg_nm)),
+                excl_nonargs(args_fit_i))
+      )
+      rm(args_fit_i)
+      rm(fit_fun_crr)
+    }
+    fit_crr <- fits_no_wobs[[args_prj_i$tstsetup_fit]]
+    dis_crr <- as.matrix(fit_crr)[, "sigma"]
+    refmod_crr_trad <- suppressMessages(do.call(get_refmodel, c(
+      list(object = fit_crr),
+      excl_nonargs(args_ref[[args_prj_i$tstsetup_ref]])
+    )))
+    prj_crr_trad <- do.call(project, c(
+      list(object = refmod_crr_trad),
+      excl_nonargs(args_prj_i)
+    ))
+    refmod_crr_lat <- suppressMessages(do.call(get_refmodel, c(
+      list(object = fit_crr),
+      excl_nonargs(args_ref[[args_prj_i$tstsetup_ref]]),
+      list(latent = TRUE, dis = dis_crr)
+    )))
+    prj_crr_lat <- do.call(project, c(
+      list(object = refmod_crr_lat),
+      excl_nonargs(args_prj_i)
+    ))
+    tstsetup_mod <- sub("\\.with_wobs\\.", ".without_wobs.", tstsetup)
+    if (ndr_ncl$clust_used) {
+      warn_prjmat_expect <- "the clusters might have different weights"
+    } else {
+      warn_prjmat_expect <- NA
+    }
+    expect_warning(prjmat_crr_lat <- as.matrix(prj_crr_lat),
+                   warn_prjmat_expect, info = tstsetup_mod)
+    expect_warning(prjmat_crr_trad <- as.matrix(prj_crr_trad),
+                   warn_prjmat_expect, info = tstsetup_mod)
+    expect_identical(prjmat_crr_lat, prjmat_crr_trad, info = tstsetup_mod)
+    prj_el_excl <- c("submodl", "refmodel")
+    expect_identical(prj_crr_lat[setdiff(names(prj_crr_lat), prj_el_excl)],
+                     prj_crr_trad[setdiff(names(prj_crr_trad), prj_el_excl)],
+                     info = tstsetup_mod)
+  }
+})
+
 # Teardown ----------------------------------------------------------------
 
 if (exists("rng_old")) assign(".Random.seed", rng_old, envir = .GlobalEnv)
