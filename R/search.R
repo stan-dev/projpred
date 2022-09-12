stan-dev/projpred

commit fa57268b6abdd642ac4e17a3583888bff882d96e

Fix issue #149
commit a7b0849a5375dbc5467c09c2d34440cd7686c623
Author: fweber144 <fweber144@protonmail.com>
Date:   Mon Mar 14 13:10:27 2022 +0100

    Tests: Project onto submodels including categorical

    predictors also in case of "special" formulas.

commit df4cc68c8aa62bfffc61602c578a08c3d02aa976
Author: fweber144 <fweber144@protonmail.com>
Date:   Mon Mar 14 12:26:45 2022 +0100

    Tests: Adapt "invalid `newdata` fails" tests now that categorical

    predictors are used more often.

commit d10ff14da0369c00396f793a03193542e7dbbfba
Author: fweber144 <fweber144@protonmail.com>
Date:   Mon Mar 14 12:25:49 2022 +0100

    As discussed at issue #169, remove the regularization test for

    the intercept in case of a non-intercept-only model (for GLMs).

    This is a fixup for commit 5be4c578cf065c33ab721c82e4944e8a573f16c6.

commit 0a62c80bb020172ad4a434d1ee1edeac807656ed
Author: fweber144 <fweber144@protonmail.com>
Date:   Mon Mar 14 11:01:05 2022 +0100

    Tests: Project more often onto submodels including categorical

    predictors and update a related comment in the `penalty` test.

commit 97932334d46ed7c92243b10804f22b9ad8e78e7c
Author: fweber144 <fweber144@protonmail.com>
Date:   Mon Mar 14 09:55:50 2022 +0100

    Update `NEWS.md`.

commit 4e13780a0ef7a14233a037cd8eb4a094db0b2e88
Author: fweber144 <fweber144@protonmail.com>
Date:   Mon Mar 14 08:44:23 2022 +0100

    Revert "Simplify `predict.subfit()`."

    This reverts commit 68f95c3a1654bca6eb7e779742f7855e103fb5e8.

    Reason: Deleting the response is actually necessary. Otherwise, errors like
    `Error in eval(predvars, data, env) : object '.y_glm_gauss.1' not found` are
    thrown.

commit ea2868ab5737009a621f240a958397ea03464cc6
Author: fweber144 <fweber144@protonmail.com>
Date:   Mon Mar 14 08:03:02 2022 +0100

    Improve the `TODO` comment for `contrasts.arg` and avoid setting the

    default explicitly. Also name argument `data`.

commit a21667a991787dbc80eb9a8479a415390b0afd61

R/search.R
@@ -131,12 +131,13 @@ search_L1 <- function(p_ref, refmodel, nterms_max, penalty, opt) {
     stop("L1 search cannot be used for an empty (i.e. intercept-only) ",
          "reference model.")
   }
-  frame <- model.frame(refmodel$formula, refmodel$fetch_data())
-  contrasts_arg <- get_contrasts_arg_list(refmodel$formula,
-                                          refmodel$fetch_data())
-  x <- model.matrix(delete.intercept(refmodel$formula),
-                    data = frame,
-                    contrasts.arg = contrasts_arg)
+  # TODO: In the following model.matrix() call, allow user-specified contrasts
+  # to be passed to argument `contrasts.arg`. The `contrasts.arg` default
+  # (`NULL`) uses `options("contrasts")` internally, but it might be more
+  # convenient to let users specify contrasts directly. At that occasion,
+  # contrasts should also be tested thoroughly (not done until now).
+  x <- model.matrix(refmodel$formula, data = refmodel$fetch_data())
+  x <- x[, colnames(x) != "(Intercept)", drop = FALSE]
   ## it's important to keep the original order because that's the order
   ## in which lasso will estimate the parameters
   tt <- terms(refmodel$formula)
@@ -159,14 +160,15 @@ search_L1 <- function(p_ref, refmodel, nterms_max, penalty, opt) {
       variables <- unlist(lapply(
         solution_terms[seq_len(nterms)],
         function(term) {
-          form <- as.formula(paste("~ 0 +", term))
-          contrasts_arg <- get_contrasts_arg_list(
-            form,
-            refmodel$fetch_data()
-          )
-          return(colnames(model.matrix(form,
-                                       data = refmodel$fetch_data(),
-                                       contrasts.arg = contrasts_arg)))
+          # TODO: In the following model.matrix() call, allow user-specified
+          # contrasts to be passed to argument `contrasts.arg`. The
+          # `contrasts.arg` default (`NULL`) uses `options("contrasts")`
+          # internally, but it might be more convenient to let users specify
+          # contrasts directly. At that occasion, contrasts should also be
+          # tested thoroughly (not done until now).
+          mm <- model.matrix(as.formula(paste("~ 1 +", term)),
+                             data = refmodel$fetch_data())
+          return(setdiff(colnames(mm), "(Intercept)"))
         }
       ))
       indices <- match(variables, colnames(x)[search_path$solution_terms])

R/divergence_minimizers.R
@@ -91,9 +91,14 @@ fit_glm_ridge_callback <- function(formula, data,
                                    projpred_var = matrix(nrow = nrow(data)),
                                    projpred_regul = 1e-4, ...) {
   # Preparations:
-  fr <- model.frame(delete.intercept(formula), data = data)
-  contrasts_arg <- get_contrasts_arg_list(formula, data = data)
-  x <- model.matrix(fr, data = data, contrasts.arg = contrasts_arg)
+  fr <- model.frame(formula, data = data)
+  # TODO: In the following model.matrix() call, allow user-specified contrasts
+  # to be passed to argument `contrasts.arg`. The `contrasts.arg` default
+  # (`NULL`) uses `options("contrasts")` internally, but it might be more
+  # convenient to let users specify contrasts directly. At that occasion,
+  # contrasts should also be tested thoroughly (not done until now).
+  x <- model.matrix(formula, data = fr)
+  x <- x[, colnames(x) != "(Intercept)", drop = FALSE]
   y <- model.response(fr)
   # Exclude arguments from `...` which cannot be passed to glm_ridge():
   dot_args <- list(...)
@@ -444,9 +449,12 @@ predict.subfit <- function(subfit, newdata = NULL) {
       return(subfit$x %*% rbind(alpha, beta))
     }
   } else {
-    contrasts_arg <- get_contrasts_arg_list(subfit$formula, newdata)
-    x <- model.matrix(delete.response(terms(subfit$formula)), newdata,
-                      contrasts.arg = contrasts_arg)
+    # TODO: In the following model.matrix() call, allow user-specified contrasts
+    # to be passed to argument `contrasts.arg`. The `contrasts.arg` default
+    # (`NULL`) uses `options("contrasts")` internally, but it might be more
+    # convenient to let users specify contrasts directly. At that occasion,
+    # contrasts should also be tested thoroughly (not done until now).
+    x <- model.matrix(delete.response(terms(subfit$formula)), data = newdata)
     if (is.null(beta)) {
       return(as.matrix(rep(alpha, NROW(x))))
     } else {

R/formula.R
@@ -837,24 +837,6 @@ delete.intercept <- function(formula) {
   return(update(formula, . ~ . - 1))
 }
 
-## construct contrasts.arg list argument for model.matrix based on the current
-## model's formula.
-## @param formula a formula object
-## @param data model's data
-## @return a named list with each factor and its contrasts
-get_contrasts_arg_list <- function(formula, data) {
-  ## extract model frame
-  ## check categorical variables
-  ## add contrasts for those
-  frame <- model.frame(delete.response(terms(formula)), data = data)
-  factors <- sapply(frame, is.factor)
-  contrasts_arg <- lapply(names(factors)[as.logical(factors)], function(v) {
-    stats::contrasts(frame[, v], contrasts = FALSE)
-  })
-  contrasts_arg <- setNames(contrasts_arg, names(factors)[as.logical(factors)])
-  return(contrasts_arg)
-}
-
 ## collapse a list of terms including contrasts
 ## @param formula model's formula
 ## @param path list of terms possibly including contrasts
@@ -864,12 +846,16 @@ collapse_contrasts_solution_path <- function(formula, path, data) {
   tt <- terms(formula)
   terms_ <- attr(tt, "term.labels")
   for (term in terms_) {
-    current_form <- as.formula(paste("~ 0 +", term))
-    contrasts_arg <- get_contrasts_arg_list(current_form, data)
-    if (length(contrasts_arg) == 0) {
+    # TODO: In the following model.matrix() call, allow user-specified contrasts
+    # to be passed to argument `contrasts.arg`. The `contrasts.arg` default
+    # (`NULL`) uses `options("contrasts")` internally, but it might be more
+    # convenient to let users specify contrasts directly. At that occasion,
+    # contrasts should also be tested thoroughly (not done until now).
+    x <- model.matrix(as.formula(paste("~ 1 +", term)), data = data)
+    if (length(attr(x, "contrasts")) == 0) {
       next
     }
-    x <- model.matrix(current_form, data, contrasts.arg = contrasts_arg)
+    x <- x[, colnames(x) != "(Intercept)", drop = FALSE]
     path <- Reduce(
       function(current, pattern) {
         pattern <- gsub("\\+", "\\\\+", pattern)

R/helper_formula.R
@@ -28,7 +28,7 @@ break_up_matrix_term <- function(formula, data) {
 
   term_labels <- attr(tt, "term.labels")
 
-  mm <- model.matrix(formula, data)
+  mm <- model.matrix(formula, data = data)
   assign <- attr(mm, "assign")
 
   new_data <- data

tests/testthat/helpers/getters.R
@@ -30,24 +30,5 @@ get_dat <- function(tstsetup, dat_crr = dat) {
 
 # A function to get the elements which may be supplied to argument `penalty`:
 get_penal_possbl <- function(formul_crr) {
-  trms_crr <- labels(terms(formul_crr))
-  if (length(trms_crr) > 0 && any(grepl("xca\\.", trms_crr))) {
-    contr_crr <- lapply(
-      setNames(nm = grep("xca\\.", trms_crr, value = TRUE)),
-      function(x_nm) {
-        # Note: As mentioned in issue #149, the reference level of a
-        # categorical predictor actually should not have its own coefficient.
-        # But to be able to run the tests for now, we also use `contrasts =
-        # FALSE` here:
-        contrasts(get(x_nm, envir = as.environment(dat)),
-                  contrasts = FALSE)
-      }
-    )
-  } else {
-    contr_crr <- NULL
-  }
-  mm_crr <- model.matrix(update(formul_crr, . ~ . + 0),
-                         data = dat,
-                         contrasts.arg = contr_crr)
-  return(colnames(mm_crr))
+  return(setdiff(colnames(model.matrix(formul_crr, data = dat)), "(Intercept)"))
 }

tests/testthat/helpers/testers.R
@@ -425,8 +425,8 @@ refmodel_tester <- function(
   return(invisible(TRUE))
 }
 
-# A helper function for testing the structure of a list of fits (each fit must
-# not necessarily be of class `"subfit"`) for the same single submodel.
+# A helper function for testing the structure of a list of fits for the same
+# single submodel.
 #
 # @param submodl_totest The `submodl` object (a list of fits for a single
 #   submodel, with one fit per projected draw) to test.
@@ -476,68 +476,21 @@ submodl_tester <- function(
   ))
 
   if (!has_grp && !has_add) {
-    sub_trms <- labels(terms(sub_formul[[1]]))
-    if (length(sub_trms) > 0) {
-      ncoefs <- sum(sapply(sub_trms, function(trm_i) {
-        ncol(model.matrix(
-          as.formula(paste("~ 0 +", trm_i)),
-          data = sub_data
-        ))
-      }))
-      ### As discussed in issue #149, the following might be more appropriate:
-      # ncoefs <- ncol(model.matrix(
-      #   as.formula(paste("~", paste(sub_trms, collapse = " + "))),
-      #   data = sub_data
-      # )) - 1L
-      ###
-    } else {
-      ncoefs <- 0L
-    }
-    formul_for_mm <- sub_formul[[1]]
-    sub_trms_for_mm <- labels(terms(formul_for_mm))
-    if (length(sub_trms_for_mm) > 0) {
-      if (any(grepl("xca\\.", sub_trms_for_mm))) {
-        sub_contr <- lapply(
-          setNames(nm = grep("xca\\.", sub_trms_for_mm, value = TRUE)),
-          function(x_nm) {
-            contrasts(get(x_nm, envir = as.environment(sub_data)),
-                      contrasts = FALSE)
-          }
-        )
-      } else {
-        sub_contr <- NULL
-      }
-    } else {
-      sub_contr <- NULL
-    }
-    sub_x_expected <- model.matrix(update(formul_for_mm, NULL ~ . + 0),
-                                   data = sub_data,
-                                   contrasts.arg = sub_contr)
+    sub_x_expected <- model.matrix(sub_formul[[1]], data = sub_data)
+    sub_x_expected <- sub_x_expected[
+      , colnames(sub_x_expected) != "(Intercept)", drop = FALSE
+    ]
+    ncoefs <- ncol(sub_x_expected)
     if (from_vsel_L1_search) {
-      attr(sub_x_expected, "assign") <- NULL
-      attr(sub_x_expected, "contrasts") <- NULL
       # Unfortunately, model.matrix() uses terms() and there seems to be no way
       # to set `keep.order = TRUE` in that internal terms() call. Thus, we have
       # to reorder the columns manually:
       if (length(solterms_vsel_L1_search) > 0) {
-        contr_list <- get_contrasts_arg_list(
-          as.formula(paste("~",
-                           paste(solterms_vsel_L1_search, collapse = " + "))),
-          data = sub_data
-        )
-        terms_contr <- names(contr_list)
         terms_contr_expd <- lapply(solterms_vsel_L1_search, function(term_crr) {
-          if (!term_crr %in% terms_contr) {
+          if (!is.factor(sub_data[[term_crr]])) {
             return(term_crr)
           } else {
-            if (sum(terms_contr == term_crr) != 1) {
-              stop("The following code is not general enough. It needs to be ",
-                   "adapted.")
-            }
-            return(paste0(
-              terms_contr[terms_contr == term_crr],
-              rownames(contr_list[[which(terms_contr == term_crr)]])
-            ))
+            return(paste0(term_crr, levels(sub_data[[term_crr]])[-1]))
           }
         })
         terms_contr_expd <- unlist(terms_contr_expd)
@@ -577,12 +530,12 @@ submodl_tester <- function(
                     info = info_str)
         expect_length(submodl_totest[[!!j]]$alpha, 1)
 
-        if (length(sub_trms) > 0 || !from_vsel_L1_search) {
+        if (ncoefs > 0 || !from_vsel_L1_search) {
           expect_true(is.matrix(submodl_totest[[!!j]]$beta), info = info_str)
           expect_true(is.numeric(submodl_totest[[!!j]]$beta), info = info_str)
           expect_identical(dim(submodl_totest[[!!j]]$beta), c(ncoefs, 1L),
                            info = info_str)
-        } else if (length(sub_trms) == 0) {
+        } else if (ncoefs == 0) {
           expect_null(submodl_totest[[!!j]]$beta, info = info_str)
         }
 

tests/testthat/setup.R
@@ -374,13 +374,10 @@ trms_common_spcl <- c("xco.1", "I(xco.1^2)",
                       "offset(offs_col)")
 
 # Solution terms for project()-ing from `"refmodel"`s:
-### Because of issue #149:
-# solterms_x <- c("xco.2", "xca.1")
-solterms_x <- c("xco.2", "xco.1")
-###
+solterms_x <- c("xco.2", "xca.1")
 solterms_z <- c("(1 | z.1)", "(xco.1 | z.1)")
 solterms_s <- c("s(s.1)") # , "s(s.2)"
-solterms_spcl <- c("xco.1", "I(xco.1^2)", "exp(xco.2)",
+solterms_spcl <- c("xca.1", "xco.1", "I(xco.1^2)", "exp(xco.2)",
                    "I(as.numeric(xco.3 > 0))",
                    "exp(xco.2):I(as.numeric(xco.3 > 0))")
 

tests/testthat/test_proj_pred.R
@@ -197,7 +197,7 @@ test_that("invalid `newdata` fails", {
     proj_linpred(prjs[[head(grep("\\.glm\\.gauss.*\\.solterms_x", names(prjs)),
                             1)]],
                  newdata = dat[, head(solterms_x, -1), drop = FALSE]),
-    "^object 'xco\\.1' not found$"
+    "^object '.*' not found$"
   )
 })
 
@@ -802,7 +802,7 @@ test_that("invalid `newdata` fails", {
                  newdata = dat[, head(solterms_x, -1), drop = FALSE],
                  .seed = seed2_tst,
                  solution_terms = solterms_x),
-    "^object 'xco\\.1' not found$"
+    "^object '.*' not found$"
   )
 })
 

commit 80ffadc8fc047a6992e22a887d9dcf50f3f3c19d

Fix #348 (L1 search in the presence of interaction terms; bug
introduced by commit fa57268b6abdd642ac4e17a3583888bff882d96e which fixed #149).

R/search.R
@@ -175,6 +175,7 @@ search_L1 <- function(p_ref, refmodel, nterms_max, penalty, opt) {
         }
       ))
       indices <- match(variables, colnames(x)[search_path$solution_terms])
+      indices <- indices[!is.na(indices)]
       beta <- search_path$beta[indices, max(indices) + 1, drop = FALSE]
       # Also reduce `x` (important for coef.subfit(), for example); note that
       # `x <- x[, variables, drop = FALSE]` should also be possible, but the