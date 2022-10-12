d88141040d9deb3d0e358bf27a664d947f6a70a2
fweber144 <fweber144@protonmail.com>
2022-10-12 14:26:34 +0200

    Explain the downside of `varsel()` in the docs.

--- R/varsel.R
+++ R/varsel.R
@@ -5,7 +5,9 @@
 #' *evaluation* part. The search part determines the solution path, i.e., the
 #' best submodel for each submodel size (number of predictor terms). The
 #' evaluation part determines the predictive performance of the submodels along
-#' the solution path.
+#' the solution path. In [varsel()], training and test data are the same, unless
+#' argument `d_test` is specified. Thus, if `d_test` is not specified, see
+#' [cv_varsel()] for a better alternative using cross-validation (CV).
 #'
 #' @param object An object of class `refmodel` (returned by [get_refmodel()] or
 #'   [init_refmodel()]) or an object that can be passed to argument `object` of
