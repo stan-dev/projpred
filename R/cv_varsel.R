0fed4ba0e8f56264dacbc927d91455d6ec18b7fc
fweber144 <fweber144@protonmail.com>
2022-10-21 10:47:43 +0200

    Generalize usage of `nterms_max` when the intercept should not
    be counted (not all occurrences of `nterms_max` checked, though).

--- R/cv_varsel.R
+++ R/cv_varsel.R
@@ -353,7 +353,7 @@ loo_varsel <- function(refmodel, method, nterms_max, ndraws,
   inds <- validset$inds
 
   ## initialize objects where to store the results
-  solution_terms_mat <- matrix(nrow = n, ncol = nterms_max - 1)
+  solution_terms_mat <- matrix(nrow = n, ncol = nterms_max - refmodel$intercept)
   loo_sub <- replicate(nterms_max, rep(NA, n), simplify = FALSE)
   mu_sub <- replicate(nterms_max, rep(NA, n), simplify = FALSE)
 
