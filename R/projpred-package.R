d0f5e550bbb327e0d4e2200543d218c85c6f0f31
fweber144 <fweber144@protonmail.com>
2022-11-05 06:49:42 +0100

    Adapt the general package documentation from `projpred-package.R` to
    the new wording from the `varsel()` and `cv_varsel()` descriptions.

--- R/projpred-package.R
+++ R/projpred-package.R
@@ -69,11 +69,12 @@
 #' \describe{
 #'   \item{[init_refmodel()], [get_refmodel()]}{For setting up a reference model
 #'   (only rarely needed explicitly).}
-#'   \item{[varsel()], [cv_varsel()]}{For variable selection, possibly with
-#'   cross-validation (CV).}
+#'   \item{[varsel()], [cv_varsel()]}{For running the *search* part and the
+#'   *evaluation* part for a projection predictive variable selection, possibly
+#'   with cross-validation (CV).}
 #'   \item{[summary.vsel()], [print.vsel()], [plot.vsel()],
 #'   [suggest_size.vsel()], [solution_terms.vsel()]}{For post-processing the
-#'   results from the variable selection.}
+#'   results from [varsel()] and [cv_varsel()].}
 #'   \item{[project()]}{For projecting the reference model onto submodel(s).
 #'   Typically, this follows the variable selection, but it can also be applied
 #'   directly (without a variable selection).}
