c7e4005908297f2b0537a03823fe6b8811b6f478
fweber144 <fweber144@protonmail.com>
2023-03-06 09:01:41 +0100

    Yet another minor readability and efficiency improvement.

--- R/formula.R
+++ R/formula.R
@@ -105,9 +105,9 @@ parse_additive_terms <- function(terms) {
   if (any(excluded)) {
     stop("te() and ti() terms are not supported, please use t2() instead.")
   }
-  smooth <- unname(unlist(sapply(smooth_terms, function(et) {
-    grep(make_function_regexp(et), terms, value = TRUE)
-  })))
+  smooth <- unlist(lapply(smooth_terms, function(st) {
+    grep(make_function_regexp(st), terms, value = TRUE)
+  }))
   if (any(grepl("\\(.+,.+=.+\\)", smooth))) {
     stop("In s() and t2() terms, arguments other than predictors are not ",
          "allowed.")
