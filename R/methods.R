99213f39e4f16bb89e629bad0d64bee77c3b0fec
fweber144 <fweber144@protonmail.com>
2023-04-08 20:12:54 +0200

    Resolve some imprecisenesses in the docs of `solution_terms()`.

--- R/methods.R
+++ R/methods.R
@@ -1880,13 +1880,14 @@ cv_ids <- function(n, K, out = c("foldwise", "indices"),
   return(cv)
 }
 
-#' Retrieve predictor solution path or predictor combination
+#' Retrieve the full-data solution path from a [varsel()] or [cv_varsel()] run
+#' or the predictor combination from a projection
 #'
-#' This function retrieves the "solution terms" from an `object`. For `vsel`
-#' objects (returned by [varsel()] or [cv_varsel()]), this is the predictor
-#' solution path of the variable selection. For `projection` objects (returned
-#' by [project()], possibly as elements of a `list`), this is the predictor
-#' combination onto which the projection was performed.
+#' This function retrieves the so-called *solution terms* from an `object`. For
+#' `vsel` objects (returned by [varsel()] or [cv_varsel()]), this is the
+#' full-data solution path from the search phase. For `projection` objects
+#' (returned by [project()], possibly as elements of a `list`), this is the
+#' predictor combination onto which the projection was performed.
 #'
 #' @param object The object from which to retrieve the solution terms. Possible
 #'   classes may be inferred from the names of the corresponding methods (see
