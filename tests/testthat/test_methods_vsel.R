ac98a49835f84725040886d1fb0011c990c10887
fweber144 <fweber144@protonmail.com>
2023-04-14 09:35:08 +0200

    Test the new message thrown by `print.vselsummary()`.

--- tests/testthat/test_methods_vsel.R
+++ tests/testthat/test_methods_vsel.R
@@ -119,10 +119,13 @@ context("print()")
 test_that("`x` of class \"vselsummary\" (based on varsel()) works", {
   skip_if_not(run_vs)
   for (tstsetup in names(smmrys_vs)) {
-    expect_output(
-      print_obj <- print(smmrys_vs[[tstsetup]]),
-      "Family:.*Link function:.*Formula:.*Observations:",
-      info = tstsetup
+    expect_message(
+      expect_output(
+        print_obj <- print(smmrys_vs[[tstsetup]]),
+        "Family:.*Link function:.*Formula:.*Observations:",
+        info = tstsetup
+      ),
+      NA
     )
     expect_identical(print_obj, smmrys_vs[[tstsetup]], info = tstsetup)
     if (run_snaps) {
@@ -141,10 +144,19 @@ test_that("`x` of class \"vselsummary\" (based on varsel()) works", {
 test_that("`x` of class \"vselsummary\" (based on cv_varsel())  works", {
   skip_if_not(run_cvvs)
   for (tstsetup in names(smmrys_cvvs)) {
-    expect_output(
-      print_obj <- print(smmrys_cvvs[[tstsetup]]),
-      "Family:.*Link function:.*Formula:.*Observations:",
-      info = tstsetup
+    args_crr <- args_cvvs[[args_smmry_cvvs[[tstsetup]]$tstsetup_vsel]]
+    if (isFALSE(args_crr$validate_search)) {
+      mssg_expected <- NA
+    } else {
+      mssg_expected <- "Column.*contains the full-data predictor ranking"
+    }
+    expect_message(
+      expect_output(
+        print_obj <- print(smmrys_cvvs[[tstsetup]]),
+        "Family:.*Link function:.*Formula:.*Observations:",
+        info = tstsetup
+      ),
+      mssg_expected
     )
     expect_identical(print_obj, smmrys_cvvs[[tstsetup]], info = tstsetup)
     if (run_snaps) {
