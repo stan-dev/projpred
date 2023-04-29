b98689ae1974ebb191ac46bb69ba06909e8663fe
fweber144 <fweber144@protonmail.com>
2023-04-29 21:03:02 +0200

    Fix #183: Rename `collapse_contrasts_solution_path()` to
    `collapse_ranked_predictors()` and also handle `poly()` terms within it.

--- R/search.R
+++ R/search.R
@@ -164,7 +164,7 @@ search_L1 <- function(p_ref, refmodel, nterms_max, penalty, opt) {
     p_ref, nlist(x, weights = refmodel$wobs), refmodel$family,
     intercept = TRUE, ncol(x), penalty, opt
   )
-  solution_terms <- collapse_contrasts_solution_path(
+  solution_terms <- collapse_ranked_predictors(
     refmodel$formula, colnames(x)[search_path$solution_terms], data = fr
   )
   outdmins <- lapply(0:length(solution_terms), function(nterms) {
