1e3727a14e4576c773d34243b66106f8ec4f5e62
fweber144 <fweber144@protonmail.com>
2023-05-28 19:22:29 +0200

    Docs: Enhancements for `varsel()`.

--- R/varsel.R
+++ R/varsel.R
@@ -1,10 +1,10 @@
 #' Run search and performance evaluation without cross-validation
 #'
 #' Run the *search* part and the *evaluation* part for a projection predictive
-#' variable selection. The search part determines the solution path, i.e., the
-#' best submodel for each submodel size (number of predictor terms). The
-#' evaluation part determines the predictive performance of the submodels along
-#' the solution path.
+#' variable selection. The search part determines the predictor ranking (also
+#' known as solution path), i.e., the best submodel for each submodel size
+#' (number of predictor terms). The evaluation part determines the predictive
+#' performance of the submodels along the predictor ranking.
 #'
 #' @param object An object of class `refmodel` (returned by [get_refmodel()] or
 #'   [init_refmodel()]) or an object that can be passed to argument `object` of
