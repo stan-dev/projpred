13a19583a3e93430e904f72dd9319debc89b61d6
fweber144 <fweber144@protonmail.com>
2023-11-15 11:54:40 +0100

    Tests: Use the global option to suppress warnings collected across all posterior draws.

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -904,6 +904,8 @@ options(projpred.additional_checks = TRUE)
 # Suppress the warning thrown if `cvrefbuilder` is `NULL` (here in the tests,
 # this should only be relevant for `datafit`s):
 options(projpred.warn_cvrefbuilder_NULL = FALSE)
+# Suppress warnings thrown while fitting the submodels:
+options(projpred.warn_submodel_fits = FALSE)
 # Set default number of significant digits to be printed:
 options(projpred.digits = getOption("digits"))
 
