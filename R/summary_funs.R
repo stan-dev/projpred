cac6dee94ce7fc9486d056b0f31e75c19160b3a0
fweber144 <fweber144@protonmail.com>
2025-01-06 15:28:30 +0100

    minor cleaning

--- R/summary_funs.R
+++ R/summary_funs.R
@@ -550,7 +550,7 @@ get_stat <- function(summaries, summaries_baseline = NULL,
     }
   }
 
-  if (stat %in% c("mse","rmse") && is.null(summaries_baseline)) {
+  if (stat %in% c("mse", "rmse") && is.null(summaries_baseline)) {
     # Compute mean and variance in log scale by matching the variance of a
     # log-normal approximation
     # https://en.wikipedia.org/wiki/Log-normal_distribution#Arithmetic_moments
